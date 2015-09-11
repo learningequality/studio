7c1cdd5326dd39a65b47e4a13164bd7239ea0976
Richard Tibbles <richard.tibbles@gmail.com>
2015-09-11 13:59:32 -0700

    More linting.

--- contentcuration/contentcuration/static/js/bundle_modules/base.js
+++ contentcuration/contentcuration/static/js/bundle_modules/base.js
@@ -23,8 +23,8 @@ function sameOrigin(url) {
     var sr_origin = '//' + host;
     var origin = protocol + sr_origin;
     // Allow absolute or scheme relative URLs to same origin
-    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+    return (url === origin || url.slice(0, origin.length + 1) === origin + '/') ||
+        (url === sr_origin || url.slice(0, sr_origin.length + 1) === sr_origin + '/') ||
         // or any other URL that isn't scheme relative or absolute i.e relative.
         !(/^(\/\/|http:|https:).*/.test(url));
 }
