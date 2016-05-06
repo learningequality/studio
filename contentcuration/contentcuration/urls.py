2a676e5b541cc6178296ae76e216a5c5e941d4aa
66eli77 <66eli77@gmail.com>
2016-05-06 10:18:19 -0700

    not in use anymore

--- contentcuration/contentcuration/urls.py
+++ contentcuration/contentcuration/urls.py
@@ -60,12 +60,6 @@ class TagViewSet(viewsets.ModelViewSet):
     queryset = ContentTag.objects.all()
     serializer_class = serializers.TagSerializer
 
-    def batch_add_tags(self, request, *args, **kwargs):
-        """
-        endpoint for api method
-        """
-        return api.batch_add_tags(request)
-
 class ExerciseViewSet(viewsets.ModelViewSet):
     queryset = Exercise.objects.all()
     serializer_class = serializers.ExerciseSerializer
@@ -97,7 +91,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
     url(r'^api/', include(bulkrouter.urls)),
-    url(r'^api/add_tags/', TagViewSet.as_view({'post': 'batch_add_tags'})),
     url(r'^api/copy_node/$', views.copy_node, name='copy_node'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'exercises/$', views.exercise_list, name='exercise_list'),
