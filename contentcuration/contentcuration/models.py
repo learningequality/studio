a1bf6ae81cdcaf77d6b99adaa23ac2f0214abf6e
66eli77 <66eli77@gmail.com>
2016-05-13 17:02:31 -0700

    directly implement the models instead of inheriting from kolibri

--- contentcuration/contentcuration/models.py
+++ contentcuration/contentcuration/models.py
@@ -11,8 +11,14 @@ from mptt.models import MPTTModel, TreeForeignKey
 from django.utils.translation import ugettext as _
 from kolibri.content.models import *
 
-class Channel(ChannelMetadata):
+class Channel(models.Model):
     """ Permissions come from association with organizations """
+    channel_id = models.UUIDField(primary_key=False, unique=True, default=uuid4, editable=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=400, blank=True)
+    author = models.CharField(max_length=400, blank=True)
+    theme = models.CharField(max_length=400, blank=True)
+    subscribed = models.BooleanField(default=False)
     editors = models.ManyToManyField(
         'auth.User',
         verbose_name=_("editors"),
@@ -43,7 +49,7 @@ class TopicTree(models.Model):
         help_text=_("For different versions of the tree in the same channel (trash, edit, workspace)"),
     )
     root_node = models.ForeignKey(
-        'Node',
+        'ContentMetadata',
         verbose_name=_("root node"),
         null=True,
         help_text=_(
@@ -61,17 +67,31 @@ class TopicTree(models.Model):
         verbose_name = _("Topic tree")
         verbose_name_plural = _("Topic trees")
 
-class ContentTag(AbstractContent):
-    tag_name = models.CharField(max_length=30, null=True, blank=True)
-    tag_type = models.CharField(max_length=30, null=True, blank=True)
+class ContentTag(models.Model):
+    tag_name = models.CharField(max_length=30, blank=True)
+    tag_type = models.CharField(max_length=30, blank=True)
     
     def __str__(self):
         return self.tag_name
 
-class Node(ContentMetadata):
+class ContentMetadata(MPTTModel, models.Model):
     """
     By default, all nodes have a title and can be used as a topic.
     """
+    content_id = models.UUIDField(primary_key=False, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=400, blank=True)
+    kind = models.ForeignKey('ContentKind', related_name='content_metadatas', blank=True, null=True)
+    slug = models.CharField(max_length=100)
+    total_file_size = models.IntegerField()
+    available = models.BooleanField(default=False)
+    license = models.ForeignKey('License')
+    prerequisite = models.ManyToManyField('self', related_name='is_prerequisite_of', through='PrerequisiteContentRelationship', symmetrical=False, blank=True)
+    is_related = models.ManyToManyField('self', related_name='relate_to', through='RelatedContentRelationship', symmetrical=False, blank=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+    tags = models.ManyToManyField(ContentTag, symmetrical=False, related_name='tagged_content', blank=True)
+    sort_order = models.FloatField(max_length=50, default=0, verbose_name=_("sort order"), help_text=_("Ascending, lowest number shown first"))
+    license_owner = models.CharField(max_length=200, blank=True, help_text=_("Organization of person who holds the essential rights"))
 
     created = models.DateTimeField(auto_now_add=True, verbose_name=_("created"))
     modified = models.DateTimeField(auto_now=True, verbose_name=_("modified"))
@@ -81,26 +101,11 @@ class Node(ContentMetadata):
         verbose_name=_("Published"),
         help_text=_("If published, students can access this item"),
     )
-
-    sort_order = models.FloatField(
-        max_length=50,
-        default=0,
-        verbose_name=_("sort order"),
-        help_text=_("Ascending, lowest number shown first"),
-    )
-
-    license_owner = models.CharField(
-        max_length=255,
-        blank=True,
-        null=True,
-        help_text=_("Organization of person who holds the essential rights"),
-    )
     original_filename = models.CharField(
         max_length=255,
         blank=True,
         null=True,
     )
-    tags = models.ManyToManyField(ContentTag, symmetrical=False, related_name='tagged_content', blank=True)
 
     @property
     def has_draft(self):
@@ -124,51 +129,120 @@ class Node(ContentMetadata):
         # Do not allow two nodes with the same name on the same level
         #unique_together = ('parent', 'title')
 
-# If we decide to subclass Content:
-#
-# class ContentVideo(Content):
-#     """
-#     Model for video data
-#     """
-#
-#     video_file = models.FileField(
-#         blank=True,
-#         null=True,
-#         upload_to='contents/video/thumbnails/',
-#         verbose_name=_("video file"),
-#         help_text=_("Upload video here"),
-#     )
-#
-#     thumbnail = models.ImageField(
-#         null=True,
-#         upload_to='contents/video/thumbnails/',
-#         help_text=_("Automatically created when new video is uploaded")
-#     )
-#
-#
-# class ContentPDF(Content):
-#     """
-#     Model for video data
-#     """
-#     pdf_file = models.FileField(
-#         blank=True,
-#         null=True,
-#         upload_to='contents/video/thumbnails/',
-#         verbose_name=_("video file"),
-#         help_text=_("Upload video here"),
-#     )
-#
-#
-# class Exercise(Content):
-#     """
-#     Model for Exercise data
-#     """
-class ContentLicense(License):
-    exists = models.BooleanField(
+class ContentKind(models.Model):
+    kind = models.CharField(primary_key=True, max_length=200, choices=content_kinds.choices)
+
+    def __str__(self):
+        return self.kind
+
+class FileFormat(models.Model):
+    extension = models.CharField(primary_key=True, max_length=40, choices=extensions.choices)
+
+    def __str__(self):
+        return self.extension
+
+class FormatPreset(models.Model):
+    id = models.CharField(primary_key=True, max_length=150, choices=presets.choices)
+    readable_name = models.CharField(max_length=400)
+    multi_language = models.BooleanField(default=False)
+    supplementary = models.BooleanField(default=False)
+    order = models.IntegerField()
+    kind = models.ForeignKey(ContentKind, related_name='format_presets')
+    allowed_formats = models.ManyToManyField(FileFormat, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    lang_code = models.CharField(primary_key=True, max_length=400)
+    lang_name = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.lang_name
+
+class File(models.Model):
+    """
+    The bottom layer of the contentDB schema, defines the basic building brick for content.
+    Things it can represent are, for example, mp4, avi, mov, html, css, jpeg, pdf, mp3...
+    """
+    checksum = models.CharField(max_length=400, blank=True)
+    available = models.BooleanField(default=False)
+    file_size = models.IntegerField(blank=True, null=True)
+    content_copy = models.FileField(upload_to=content_copy_name, storage=ContentCopyStorage(), max_length=500, blank=True)
+    contentmetadata = models.ForeignKey(ContentMetadata, related_name='files', blank=True, null=True)
+    file_format = models.ForeignKey(FileFormat, related_name='files', blank=True, null=True)
+    preset = models.ForeignKey(FormatPreset, related_name='files', blank=True, null=True)
+    lang = models.ForeignKey(Language, blank=True, null=True)
+
+    class Admin:
+        pass
+
+    def __str__(self):
+        return '{checksum}{extension}'.format(checksum=self.checksum, extension='.' + self.file_format.extension)
+
+class License(models.Model):
+    """
+    Normalize the license of ContentMetadata model
+    """
+    license_name = models.CharField(max_length=50)
+     exists = models.BooleanField(
         default=False,
         verbose_name=_("license exists"),
         help_text=_("Tells whether or not a content item is licensed to share"),
     )
+
+    def __str__(self):
+        return self.license_name
+
+class PrerequisiteContentRelationship(models.Model):
+    """
+    Predefine the prerequisite relationship between two ContentMetadata objects.
+    """
+    contentmetadata_1 = models.ForeignKey(ContentMetadata, related_name='%(app_label)s_%(class)s_1')
+    contentmetadata_2 = models.ForeignKey(ContentMetadata, related_name='%(app_label)s_%(class)s_2')
+
+    class Meta:
+        unique_together = ['contentmetadata_1', 'contentmetadata_2']
+
+    def clean(self, *args, **kwargs):
+        # self reference exception
+        if self.contentmetadata_1 == self.contentmetadata_2:
+            raise IntegrityError('Cannot self reference as prerequisite.')
+        # immediate cyclic exception
+        elif PrerequisiteContentRelationship.objects.using(self._state.db)\
+                .filter(contentmetadata_1=self.contentmetadata_2, contentmetadata_2=self.contentmetadata_1):
+            raise IntegrityError(
+                'Note: Prerequisite relationship is directional! %s and %s cannot be prerequisite of each other!'
+                % (self.contentmetadata_1, self.contentmetadata_2))
+        # distant cyclic exception
+        # elif <this is a nice to have exception, may implement in the future when the priority raises.>
+        #     raise Exception('Note: Prerequisite relationship is acyclic! %s and %s forms a closed loop!' % (self.contentmetadata_1, self.contentmetadata_2))
+        super(PrerequisiteContentRelationship, self).clean(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(PrerequisiteContentRelationship, self).save(*args, **kwargs)
+
+
+class RelatedContentRelationship(models.Model):
+    """
+    Predefine the related relationship between two ContentMetadata objects.
+    """
+    contentmetadata_1 = models.ForeignKey(ContentMetadata, related_name='%(app_label)s_%(class)s_1')
+    contentmetadata_2 = models.ForeignKey(ContentMetadata, related_name='%(app_label)s_%(class)s_2')
+
+    class Meta:
+        unique_together = ['contentmetadata_1', 'contentmetadata_2']
+
+    def save(self, *args, **kwargs):
+        # self reference exception
+        if self.contentmetadata_1 == self.contentmetadata_2:
+            raise IntegrityError('Cannot self reference as related.')
+        # handle immediate cyclic
+        elif RelatedContentRelationship.objects.using(self._state.db)\
+                .filter(contentmetadata_1=self.contentmetadata_2, contentmetadata_2=self.contentmetadata_1):
+            return  # silently cancel the save
+        super(RelatedContentRelationship, self).save(*args, **kwargs)
 
 class Exercise(models.Model):
 
