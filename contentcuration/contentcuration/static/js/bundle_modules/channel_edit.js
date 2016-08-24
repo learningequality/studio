6eb0727c67ae54c9622eae77066ead06823cddd5
Jordan Yoshihara <jayoshih@ucsd.edu>
2016-08-23 17:31:28 -0700

    Fixed publish styling

--- contentcuration/contentcuration/static/js/bundle_modules/channel_edit.js
+++ contentcuration/contentcuration/static/js/bundle_modules/channel_edit.js
@@ -5,6 +5,8 @@ var ChannelEditRouter = require("edit_channel/router");
 
 $(function() {
 	$("#channel-publish-button").on("click", publish_nodes);
+	$("#publish-id-help").on("click", show_publish_help);
+	$("#publish-id-copy").on("click", copy_publish_id);
 	if(window.channel){
 		window.current_channel = new Models.ChannelModel(window.channel);
 		window.current_channel.fetch({async:false});
@@ -13,6 +15,26 @@ $(function() {
 
 function publish_nodes(){
 	$("#channel-edit-content-wrapper").data("data").publish();
+}
+
+function show_publish_help(){
+	$("#publish-help-modal").modal("show");
+}
+
+function copy_publish_id(){
+	$("#publish-id-text").focus();
+	$("#publish-id-text").select();
+	try {
+    	document.execCommand("copy");
+    } catch(e) {
+        $("#publish-copy-success").text("Copy failed");
+    }
+
+    $("#publish-copy-success").css("display", "inline");
+    setTimeout(function(){
+    	$("#publish-copy-success").css("display", "none");
+    	$("#publish-copy-success").text("Copied!");
+    }, 2500);
 }
 
 module.exports = {
