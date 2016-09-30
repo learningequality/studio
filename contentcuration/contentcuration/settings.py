229414ddcbc82503ba89816bff334830ce6f5b3c
Jordan Yoshihara <jordanyoshihara@gmail.com>
2016-09-30 13:48:33 -0700

    Update settings.py

--- contentcuration/contentcuration/settings.py
+++ contentcuration/contentcuration/settings.py
@@ -106,6 +106,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'contentcuration',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
+        
+        # For dev purposes only
         'USER': 'learningequality',
         'PASSWORD': 'kolibri',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
