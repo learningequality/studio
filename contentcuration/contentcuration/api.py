8bc6ff2df3c18456801a3d7ecc16ff557ca4a33d
Aron Fyodor Asor <aronasorman@users.noreply.github.com>
2016-10-04 15:03:57 -0700

    Fix models module import

--- contentcuration/contentcuration/api.py
+++ contentcuration/contentcuration/api.py
@@ -11,7 +11,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from kolibri.content import models as KolibriContent
 from django.db import transaction
-import models
+import contentcuration.models as models
 
 def recurse(node, level=0):
     print ('\t' * level), node.id, node.lft, node.rght, node.title
