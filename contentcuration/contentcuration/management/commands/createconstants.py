9266b21d3d0b6fa626b32f7a6418d1ced3686f35
Jordan Yoshihara <jayoshih@ucsd.edu>
2016-10-07 14:18:28 -0700

    Added constants

---
+++ contentcuration/contentcuration/management/commands/createconstants.py
@@ -0,0 +1,405 @@
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from le_utils.constants import content_kinds,file_formats, format_presets, licenses, exercises
+from contentcuration import models
+import logging as logmodule
+logging = logmodule.getLogger(__name__)
+
+SITES = [
+    {
+        "model" : Site,
+        "pk" : "id",
+        "fields": {
+            "id": 1,
+            "name"  : "Kolibri Studio",
+            "domain" : "contentworkshop.learningequality.org",
+        },
+    },
+    {
+        "model" : Site,
+        "pk" : "id",
+        "fields": {
+            "id": 2,
+            "name"  : "Kolibri Studio (Debug Mode)",
+            "domain" : "127.0.0.1:8000",
+        },
+    },
+]
+
+LICENSES = [
+    {
+        "model": models.License,
+        "pk": "id",
+        "fields": {
+            "id": 1,
+            "license_name": licenses.CC_BY,
+            "exists": True,
+            "license_url": "https://creativecommons.org/licenses/by/4.0/",
+            "license_description": "The Attribution License lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.",
+        },
+    },
+    {
+        "model": models.License,
+        "pk": "id",
+        "fields": {
+            "id": 2,
+            "license_name": licenses.CC_BY_SA,
+            "exists": True,
+            "license_url": "https://creativecommons.org/licenses/by-sa/4.0",
+            "license_description": "The Attribution-ShareAlike License lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to \"copyleft\" free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.",
+        },
+    },
+    {
+        "model": models.License,
+        "pk": "id",
+        "fields": {
+            "id": 3,
+            "license_name": licenses.CC_BY_ND,
+            "exists": True,
+            "license_url": "https://creativecommons.org/licenses/by-nd/4.0/",
+            "license_description": "The Attribution-NoDerivs License allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.",
+        },
+    },
+    {
+        "model": models.License,
+        "pk": "id",
+        "fields": {
+            "id": 4,
+            "license_name": licenses.CC_BY_NC,
+            "exists": True,
+            "license_url": "https://creativecommons.org/licenses/by-nc/4.0",
+            "license_description": "The Attribution-NonCommercial License lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don't have to license their derivative works on the same terms.",
+        },
+    },
+    {
+        "model": models.License,
+        "pk": "id",
+        "fields": {
+            "id": 5,
+            "license_name": licenses.CC_BY_NC_SA,
+            "exists": True,
+            "license_url": "https://creativecommons.org/licenses/by-nc-sa/4.0",
+            "license_description": "The Attribution-NonCommercial-ShareAlike License lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.",
+        },
+    },
+    {
+        "model": models.License,
+        "pk": "id",
+        "fields": {
+            "id": 6,
+            "license_name": licenses.CC_BY_NC_ND,
+            "exists": True,
+            "license_url": "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+            "license_description": "The Attribution-NonCommercial-NoDerivs License is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can't change them in any way or use them commercially.",
+        },
+    },
+    {
+        "model": models.License,
+        "pk": "id",
+        "fields": {
+            "id": 7,
+            "license_name": licenses.ALL_RIGHTS_RESERVED,
+            "exists": True,
+            "license_url": "http://www.allrights-reserved.com/",
+            "license_description": "The All Rights Reserved License indicates that the copyright holder reserves, or holds for their own use, all the rights provided by copyright law under one specific copyright treaty.",
+        },
+    },
+    {
+        "model": models.License,
+        "pk": "id",
+        "fields": {
+            "id": 8,
+            "license_name": licenses.PUBLIC_DOMAIN,
+            "exists": True,
+            "license_url": "https://creativecommons.org/publicdomain/mark/1.0/",
+            "license_description": "Public Domain work has been identified as being free of known restrictions under copyright law, including all related and neighboring rights.",
+        },
+    },
+]
+
+FILE_FORMATS = [
+    {
+        "model": models.FileFormat,
+        "pk": "extension",
+        "fields": {
+            "extension": file_formats.MP4,
+            "mimetype" : file_formats.MP4_MIMETYPE,
+        },
+    },
+    {
+        "model": models.FileFormat,
+        "pk": "extension",
+        "fields": {
+            "extension": file_formats.VTT,
+            "mimetype" : file_formats.VTT_MIMETYPE,
+        },
+    },
+    {
+        "model": models.FileFormat,
+        "pk": "extension",
+        "fields": {
+            "extension": file_formats.SRT,
+            "mimetype" : file_formats.SRT_MIMETYPE,
+        },
+    },
+    {
+        "model": models.FileFormat,
+        "pk": "extension",
+        "fields": {
+            "extension": file_formats.PDF,
+            "mimetype" : file_formats.PDF_MIMETYPE,
+        },
+    },
+    {
+        "model": models.FileFormat,
+        "pk": "extension",
+        "fields": {
+            "extension": file_formats.MP3,
+            "mimetype" : file_formats.MP3_MIMETYPE,
+        },
+    },
+    {
+        "model": models.FileFormat,
+        "pk": "extension",
+        "fields": {
+            "extension": file_formats.JPG,
+            "mimetype" : file_formats.JPG_MIMETYPE,
+        },
+    },
+    {
+        "model": models.FileFormat,
+        "pk": "extension",
+        "fields": {
+            "extension": file_formats.JPEG,
+            "mimetype" : file_formats.JPG_MIMETYPE,
+        },
+    },
+    {
+        "model": models.FileFormat,
+        "pk": "extension",
+        "fields": {
+            "extension": file_formats.PNG,
+            "mimetype" : file_formats.PNG_MIMETYPE,
+        },
+    },
+    {
+        "model": models.FileFormat,
+        "pk": "extension",
+        "fields": {
+            "extension": file_formats.WAV,
+            "mimetype" : file_formats.WAV_MIMETYPE,
+        },
+    },
+    {
+        "model": models.FileFormat,
+        "pk": "extension",
+        "fields": {
+            "extension": file_formats.PERSEUS,
+            "mimetype" : file_formats.PERSEUS_MIMETYPE,
+        },
+    },
+]
+
+KINDS = [
+    {
+        "model": models.ContentKind,
+        "pk": "kind",
+        "fields": {
+            "kind": content_kinds.TOPIC,
+        },
+    },
+    {
+        "model": models.ContentKind,
+        "pk": "kind",
+        "fields": {
+            "kind": content_kinds.VIDEO,
+        }
+    },
+    {
+        "model": models.ContentKind,
+        "pk": "kind",
+        "fields": {
+            "kind": content_kinds.AUDIO,
+        },
+    },
+    {
+        "model": models.ContentKind,
+        "pk": "kind",
+        "fields": {
+            "kind": content_kinds.EXERCISE,
+        },
+    },
+    {
+        "model": models.ContentKind,
+        "pk": "kind",
+        "fields": {
+            "kind": content_kinds.DOCUMENT,
+        },
+    },
+]
+
+PRESETS = [
+    {
+        "model": models.FormatPreset,
+        "pk": "id",
+        "fields": {
+            "id" : format_presets.VIDEO_HIGH_RES,
+            "readable_name": format_presets.VIDEO_HIGH_RES_READABLE,
+            "multi_language" : False,
+            "supplementary" : False,
+            "thumbnail" : False,
+            "order" : 1,
+            "kind_id" : content_kinds.VIDEO,
+            "allowed_formats" : [file_formats.MP4],
+        },
+    },
+    {
+        "model": models.FormatPreset,
+        "pk": "id",
+        "fields": {
+            "id" : format_presets.VIDEO_LOW_RES,
+            "readable_name": format_presets.VIDEO_LOW_RES_READABLE,
+            "multi_language" : False,
+            "supplementary" : False,
+            "thumbnail" : False,
+            "order" : 2,
+            "kind_id" : content_kinds.VIDEO,
+            "allowed_formats" : [file_formats.MP4],
+        },
+    },
+    {
+        "model": models.FormatPreset,
+        "pk": "id",
+        "fields": {
+            "id" : format_presets.VIDEO_THUMBNAIL,
+            "readable_name": format_presets.VIDEO_THUMBNAIL_READABLE,
+            "multi_language" : False,
+            "supplementary" : True,
+            "thumbnail" : True,
+            "order" : 3,
+            "kind_id" : content_kinds.VIDEO,
+            "allowed_formats" : [file_formats.PNG, file_formats.JPG, file_formats.JPEG],
+        },
+    },
+    {
+        "model": models.FormatPreset,
+        "pk": "id",
+        "fields": {
+            "id" : format_presets.AUDIO,
+            "readable_name": format_presets.AUDIO_READABLE,
+            "multi_language" : False,
+            "supplementary" : False,
+            "thumbnail" : False,
+            "order" : 1,
+            "kind_id" : content_kinds.AUDIO,
+            "allowed_formats" : [file_formats.WAV, file_formats.MP3],
+        },
+    },
+    {
+        "model": models.FormatPreset,
+        "pk": "id",
+        "fields": {
+            "id" : format_presets.AUDIO_THUMBNAIL,
+            "readable_name": format_presets.AUDIO_THUMBNAIL_READABLE,
+            "multi_language" : False,
+            "supplementary" : True,
+            "thumbnail" : True,
+            "order" : 2,
+            "kind_id" : content_kinds.AUDIO,
+            "allowed_formats" : [file_formats.PNG, file_formats.JPG, file_formats.JPEG],
+        },
+    },
+    {
+        "model": models.FormatPreset,
+        "pk": "id",
+        "fields": {
+            "id" : format_presets.DOCUMENT,
+            "readable_name": format_presets.DOCUMENT_READABLE,
+            "multi_language" : False,
+            "supplementary" : False,
+            "thumbnail" : False,
+            "order" : 1,
+            "kind_id" : content_kinds.DOCUMENT,
+            "allowed_formats" : [file_formats.PDF],
+        },
+    },
+    {
+        "model": models.FormatPreset,
+        "pk": "id",
+        "fields": {
+            "id" : format_presets.DOCUMENT_THUMBNAIL,
+            "readable_name": format_presets.DOCUMENT_THUMBNAIL_READABLE,
+            "multi_language" : False,
+            "supplementary" : True,
+            "thumbnail" : True,
+            "order" : 2,
+            "kind_id" : content_kinds.DOCUMENT,
+            "allowed_formats" : [file_formats.PNG, file_formats.JPG, file_formats.JPEG]
+        }
+    },
+    {
+        "model": models.FormatPreset,
+        "pk": "id",
+        "fields": {
+            "id" : format_presets.EXERCISE,
+            "readable_name": format_presets.EXERCISE_READABLE,
+            "multi_language" : False,
+            "supplementary" : False,
+            "thumbnail" : False,
+            "order" : 1,
+            "kind_id" : content_kinds.EXERCISE,
+            "allowed_formats" : [file_formats.PERSEUS],
+        },
+    },
+    {
+        "model": models.FormatPreset,
+        "pk": "id",
+        "fields": {
+            "id" : format_presets.EXERCISE_THUMBNAIL,
+            "readable_name": format_presets.EXERCISE_THUMBNAIL_READABLE,
+            "multi_language" : False,
+            "supplementary" : True,
+            "thumbnail" : True,
+            "order" : 2,
+            "kind_id" : content_kinds.EXERCISE,
+            "allowed_formats" : [file_formats.PNG, file_formats.JPG, file_formats.JPEG],
+        },
+    },
+]
+
+
+CONSTANTS = [SITES, LICENSES, FILE_FORMATS, KINDS, PRESETS]
+
+class EarlyExit(BaseException):
+    def __init__(self, message, db_path):
+        self.message = message
+        self.db_path = db_path
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        try:
+            self.stdout.write("***** Creating Constants *****")
+            for constant_list in CONSTANTS:
+                current_model = ""
+                new_model_count = 0
+                for constant in constant_list:
+                    current_model=constant['model'].__name__
+                    obj, isNew = constant['model'].objects.update_or_create(**{constant['pk'] : constant['fields'][constant['pk']]})
+                    new_model_count += 1 if isNew else 0
+                    for attr, value in constant['fields'].items():
+                        setattr(obj, attr, value)
+                    obj.save()
+                self.stdout.write("{0}: {1} constants saved ({2} new)".format(str(current_model), len(constant_list), new_model_count))
+            self.stdout.write("************ DONE ************")
+
+        except EarlyExit as e:
+            logging.warning("Exited early due to {message}.".format(
+                message=e.message))
+            self.stdout.write("You can find your database in {path}".format(
+                path=e.db_path))
