6d0f02fd9082ef073eaf5397c338938589ffc0e3
Jordan Yoshihara <jordanyoshihara@gmail.com>
2016-10-17 10:32:34 -0700

    Fixed elif statement

--- contentcuration/contentcuration/management/commands/exportchannel.py
+++ contentcuration/contentcuration/management/commands/exportchannel.py
@@ -246,7 +246,7 @@ def write_assessment_item(assessment_item, zf):
         template = 'perseus/multiple_selection.json'
     elif assessment_item.type == exercises.SINGLE_SELECTION:
         template = 'perseus/multiple_selection.json'
-    if assessment_item.type == exercises.FREE_RESPONSE:
+    elif assessment_item.type == exercises.FREE_RESPONSE:
         template = 'perseus/input_question.json'
     elif assessment_item.type == exercises.INPUT_QUESTION:
         template = 'perseus/input_question.json'
