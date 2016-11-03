bdbe1b9f7745eff847d953a56dec657a6a6ace47
Jordan Yoshihara <jayoshih@ucsd.edu>
2016-11-03 14:07:57 -0700

    Removed extra newline

--- contentcuration/contentcuration/dev_settings.py
+++ contentcuration/contentcuration/dev_settings.py
@@ -9,5 +9,4 @@ ACCOUNT_ACTIVATION_DAYS=7
 # EMAIL_BACKEND = 'email_extras.backends.BrowsableEmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID =2
-
 logging.basicConfig(level='DEBUG')
