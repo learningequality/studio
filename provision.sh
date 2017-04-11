989e8432a6b4e384d7c94abd0158c32d7331011a
David <davidxcanas@gmail.com>
2017-04-10 17:10:42 -0700

    Running all package installations under sudo

--- provision.sh
+++ provision.sh
@@ -2,7 +2,6 @@
 set -eo pipefail
 set -x
 
-# run as root
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 apt-get install -y python python-pip python-dev postgresql-server-dev-all postgresql-contrib postgresql-client postgresql ffmpeg
 apt-get install -y nodejs
@@ -18,14 +17,11 @@ cd /vagrant/contentcuration
 
 devSettings="--settings=contentcuration.dev_settings"
 
-# run as user
-su ubuntu <<-UserSetup
-  pip install --user -r ../requirements_dev.txt
-  pip install --user -r ../requirements.txt
-  python manage.py collectstatic_js_reverse $devSettings
-  python manage.py collectstatic --noinput $devSettings
-  python manage.py loadconstants $devSettings
-  python manage.py migrate $devSettings
-UserSetup
+pip install -r ../requirements_dev.txt
+pip install -r ../requirements.txt
+python manage.py collectstatic_js_reverse $devSettings
+python manage.py collectstatic --noinput $devSettings
+python manage.py loadconstants $devSettings
+python manage.py migrate $devSettings
 
 # python manage.py calculateresources --init $devSettings
