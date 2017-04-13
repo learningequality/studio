2c0a6e307f06aebd17ab0b6d80e8034b5b47640f
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-04-12 23:34:55 -0700

    Fixed extraneous changes

--- contentcuration/contentcuration/models.py
+++ contentcuration/contentcuration/models.py
@@ -3,7 +3,6 @@ import os
 import uuid
 import hashlib
 import functools
-import collections
 import json
 from django.conf import settings
 from django.contrib import admin
@@ -327,35 +326,6 @@ class License(models.Model):
 
     def __str__(self):
         return self.license_name
-
-
-class memoized(object):
-    '''Decorator. Caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-    '''
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-    def __call__(self, *args):
-        if not isinstance(args, collections.Hashable):
-         # uncacheable. a list, for instance.
-         # better to not cache than blow up.
-         return self.func(*args)
-
-        if args in self.cache:
-            return self.cache[args]
-        else:
-            value = self.func(*args)
-            self.cache[args] = value
-        return value
-    def __repr__(self):
-        '''Return the function's docstring.'''
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        '''Support instance methods.'''
-        return functools.partial(self.__call__, obj)
 
 class ContentNode(MPTTModel, models.Model):
     """
