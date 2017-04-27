2c744581ee5efb188a22d6195c8f8a09c442356b
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-04-27 13:02:52 -0700

    Added check for duplicate node ids in topic level

--- contentcuration/contentcuration/view/internal_views.py
+++ contentcuration/contentcuration/view/internal_views.py
@@ -238,21 +238,27 @@ def convert_data_to_nodes(content_data, parent_node):
     try:
         root_mapping = {}
         sort_order = 1
+        parent = ContentNode.objects.get(pk=parent_node)
+        existing_node_ids = parent.children.values_list('node_id', flat=True) if parent else []
+
         with transaction.atomic():
             for node_data in content_data:
-                # Create the node
-                new_node = create_node(node_data, parent_node, sort_order)
+                # Check if node id is already in the tree to avoid duplicates
+                if node_data['node_id'] not in existing_node_ids:
+                    # Create the node
+                    new_node = create_node(node_data, parent_node, sort_order)
 
-                # Create files associated with node
-                map_files_to_node(new_node, node_data['files'])
+                    # Create files associated with node
+                    map_files_to_node(new_node, node_data['files'])
 
-                # Create questions associated with node
-                create_exercises(new_node, node_data['questions'])
-                sort_order += 1
+                    # Create questions associated with node
+                    create_exercises(new_node, node_data['questions'])
+                    sort_order += 1
 
-                # Track mapping between newly created node and node id
-                root_mapping.update({node_data['node_id'] : new_node.pk})
+                    # Track mapping between newly created node and node id
+                    root_mapping.update({node_data['node_id'] : new_node.pk})
             return root_mapping
+
     except KeyError as e:
         raise ObjectDoesNotExist("Error creating node: {0}".format(e.message))
 
