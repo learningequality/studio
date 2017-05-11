cdcb1e10c5bc2777d956de491659623828393df7
Aron Fyodor Asor <aronasorman@gmail.com>
2017-05-11 12:21:24 -0700

    Re-add google-cloud-python-debugger, for great debugging.

--- contentcuration/contentcuration/wsgi.py
+++ contentcuration/contentcuration/wsgi.py
@@ -8,21 +8,25 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import subprocess
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "contentcuration.settings")
 
 # Attach Python Cloud Debugger
-# try:
-#     import googleclouddebugger
-#     if os.getenv("RUN_CLOUD_DEBUGGER"):
-#         googleclouddebugger.enable(
-#             enable_service_account_auth=True,
-#             project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
-#             project_number=os.getenv('GOOGLE_CLOUD_PROJECT_NUMBER'),
-#             service_account_json_file=os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-# except ImportError:
-#     pass
+try:
+    import googleclouddebugger
+
+    if os.getenv("RUN_CLOUD_DEBUGGER"):
+        googleclouddebugger.AttachDebugger(
+            version=subprocess.check_output(['git', 'rev-parse', 'HEAD']),
+            project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+            project_number=os.getenv('GOOGLE_CLOUD_PROJECT_NUMBER'),
+            enable_service_account_auth=True,
+            service_account_json_file=os.getenv("GOOGLE_APPLICATION_CREDENTIALS"),
+        )
+except ImportError:
+    pass
 
 application = get_wsgi_application()
