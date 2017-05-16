3c9d07f4617dc7f27c90082d340ca249b1cedc2e
Aron Fyodor Asor <aronasorman@gmail.com>
2017-05-16 12:15:52 -0700

    ignore morango migrations on the default server

--- contentcuration/contentcuration/router.py
+++ contentcuration/contentcuration/router.py
@@ -1,9 +1,7 @@
-import logging
 from kolibri.content.models import ContentDatabaseModel
 
 
 class Router(object):
-
     def db_for_read(self, model, **hints):
         db = None
         if issubclass(model, ContentDatabaseModel):
@@ -32,8 +30,8 @@ class Router(object):
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if "kolibri" in app_label and db == "default":
-            return False        # don't run on main DB
+            return False  # don't run on main DB
+        elif "morango" in app_label and db == "default":
+            return False  # don't run morango migrations on the default db either!
         else:
-            return None         # we have no opinion here. Best practice!
-
-
+            return None  # we have no opinion here. Best practice!
