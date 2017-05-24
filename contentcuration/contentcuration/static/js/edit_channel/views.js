71bc4ef670b27a815ee8bd71acebe62f70cb77cb
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-05-24 10:01:00 -0700

    Fixed deleting channels and reloading

--- contentcuration/contentcuration/static/js/edit_channel/views.js
+++ contentcuration/contentcuration/static/js/edit_channel/views.js
@@ -41,7 +41,9 @@ var BaseView = Backbone.View.extend({
 		collection.forEach(function(entry){
       		$.merge(list_to_reload, entry.get("ancestors"));
 		});
-		list_to_reload.push(window.current_channel.get("main_tree").id)
+		if(window.current_channel.id){
+			list_to_reload.push(window.current_channel.get("main_tree").id)
+		}
 		this.retrieve_nodes($.unique(list_to_reload), true).then(function(fetched){
 			fetched.forEach(function(model){
 				var object = window.workspace_manager.get(model.get("id"));
