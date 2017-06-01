0690fe3a48a1563e2f14656ea9b271de40122277
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-06-01 10:19:37 -0700

    Added rounding for formating sizes

--- contentcuration/contentcuration/static/js/edit_channel/utils/string_helper.js
+++ contentcuration/contentcuration/static/js/edit_channel/utils/string_helper.js
@@ -6,13 +6,13 @@ function format_size(text){
   var isnegative = value < 0;
   value = Math.abs(value)
   if(value > 999999999)
-    return (isnegative ? "-" : "") + parseInt(value/1000000000) + "GB";
+    return (isnegative ? "-" : "") + Math.round(parseFloat(value/1000000000)) + "GB";
   else if(value > 999999)
-    return (isnegative ? "-" : "") + parseInt(value/1000000) + "MB";
+    return (isnegative ? "-" : "") + Math.round(parseFloat(value/1000000)) + "MB";
   else if(value > 999)
-    return (isnegative ? "-" : "") + parseInt(value/1000) + "KB";
+    return (isnegative ? "-" : "") + Math.round(parseFloat(value/1000)) + "KB";
   else
-    return (isnegative ? "-" : "") + parseInt(value) + "B";
+    return (isnegative ? "-" : "") + Math.round(parseFloat(value)) + "B";
 }
 
 function escape_str(text){
