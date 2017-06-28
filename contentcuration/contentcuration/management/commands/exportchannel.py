4df2dfa0047a6ae7e464487f26a26e7d30836fa9
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-06-28 16:21:04 -0700

    Started exporting prerequisites

--- contentcuration/contentcuration/management/commands/exportchannel.py
+++ contentcuration/contentcuration/management/commands/exportchannel.py
@@ -64,7 +64,8 @@ class Command(BaseCommand):
                 map_content_nodes(channel.main_tree,)
                 save_export_database(channel_id)
                 increment_channel_version(channel)
-                mark_all_nodes_as_changed(channel)
+                map_prerequisites(channel.main_tree)
+                # mark_all_nodes_as_changed(channel)
                 # use SQLite backup API to put DB into archives folder.
                 # Then we can use the empty db name to have SQLite use a temporary DB (https://www.sqlite.org/inmemorydb.html)
 
@@ -360,6 +361,17 @@ def process_image_strings(content):
             })
             content = content.replace(match.group(1), img_match.group(1))
     return content, image_list
+
+def map_prerequisites(root_node):
+    print "\n\n\n\n\n"
+    for n in ccmodels.PrerequisiteContentRelationship.objects.filter(prerequisite__tree_id=root_node.tree_id):
+        prerequisite_node = kolibrimodels.ContentNode.objects.get(pk=n.prerequisite.node_id)
+        target_node = kolibrimodels.ContentNode.objects.get(pk=n.target_node.node_id)
+
+        print "PREREQUISITE", prerequisite_node.title
+        print "TARGET", target_node.title
+        print "\n\n"
+    print "\n\n\n\n\n"
 
 def map_channel_to_kolibri_channel(channel):
     logging.debug("Generating the channel metadata.")
