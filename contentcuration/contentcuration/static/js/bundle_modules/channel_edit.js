7903d61acbae7564a2785c65863e71bb822199e2
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-06-28 16:45:08 -0700

    Added static modal for publish success modal

--- contentcuration/contentcuration/static/js/bundle_modules/channel_edit.js
+++ contentcuration/contentcuration/static/js/bundle_modules/channel_edit.js
@@ -6,7 +6,6 @@ var ChannelEditRouter = require("edit_channel/router");
 $(function() {
 	$("#channel-publish-button").on("click", publish_nodes);
 	$("#channel-activate-button").on("click", approve_channel);
-	$("#publish-id-help").on("click", show_publish_help);
 	$("#publish-id-copy").on("click", function(){ copy_publish_id($("#publish_id_text")[0]); });
 	$("#modal-copy-btn").on("click", function(){ copy_publish_id($("#modal-copy-text")[0]); });
 	if(window.channel){
@@ -28,24 +27,17 @@ function approve_channel(){
 	window.workspace_manager.get_main_view().activate_channel();
 }
 
-function show_publish_help(){
-	$("#publish-help-modal").modal("show");
-}
-
 function copy_publish_id(text_element){
 	text_element.focus();
 	text_element.select();
 	try {
     	document.execCommand("copy");
     	$("#publish-id-copy").removeClass("glyphicon-copy").addClass("glyphicon-ok");
-    	$("#modal-copy-btn").text("Copied!");
     } catch(e) {
         $("#publish-id-copy").removeClass("glyphicon-copy").addClass("glyphicon-remove");
-        $("#modal-copy-btn").text("Copy Failed");
     }
     setTimeout(function(){
     	$("#publish-id-copy").removeClass("glyphicon-ok").removeClass("glyphicon-remove").addClass("glyphicon-copy");
-    	$("#modal-copy-btn").text("COPY");
     }, 2500);
 }
 
