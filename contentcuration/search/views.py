f487719f6fbed4c1032b23e38f1335bb06d214e7
Jonathan Boiser <jboiser@gmail.com>
2017-06-28 16:22:34 -0700

    refactor

--- contentcuration/search/views.py
+++ contentcuration/search/views.py
@@ -5,29 +5,32 @@ from rest_framework.response import Response
 from . import serializers
 
 
-@api_view(['GET'])
-def search_items(request):
-    """
-    Keyword search of items (i.e. non-topics)
-    """
+def get_accessible_contentnodes(request):
     exclude_channel = request.query_params.get('exclude_channel', '')
 
     # Get tree_ids for channels accessible to the user
     tree_ids = cc_models.Channel.objects \
         .select_related('main_tree') \
         .filter(Q(deleted=False) & (Q(public=True) | Q(editors=request.user) | Q(viewers=request.user))) \
-        .exclude(pk=exclude_channel) \
-        .values_list('main_tree__tree_id', flat=True)
 
-    queryset = cc_models.ContentNode.objects \
-        .filter(tree_id__in=tree_ids) \
-        .exclude(kind='topic')
+    if exclude_channel is not None:
+        tree_ids = tree_ids.exclude(pk=exclude_channel)
 
+    tree_ids = tree_ids.values_list('main_tree__tree_id', flat=True)
+
+    return cc_models.ContentNode.objects \
+        .filter(tree_id__in=tree_ids)
+
+
+@api_view(['GET'])
+def search_items(request):
+    """
+    Keyword search of items (i.e. non-topics)
+    """
+    queryset = get_accessible_contentnodes(request).exclude(kind='topic')
     search_query = request.query_params.get('q', None)
-
     if search_query is not None:
         queryset = queryset.filter(title__icontains=search_query)
-
     serializer = serializers.ContentSearchResultSerializer(queryset, many=True)
     return Response(serializer.data)
 
@@ -37,10 +40,9 @@ def search_topics(request):
     """
     Keyword search of topics
     """
-    queryset = cc_models.ContentNode.objects.filter(kind='topic')
+    queryset = get_accessible_contentnodes(request).filter(kind='topic')
     search_query = request.query_params.get('q', None)
     if search_query is not None:
         queryset = queryset.filter(title__icontains=search_query)
-
     serializer = serializers.ContentSearchResultSerializer(queryset, many=True)
     return Response(serializer.data)
