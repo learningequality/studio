d595f1ff1e3c5ad40974a2c6d4b6a90845c8e718
Jonathan Boiser <jboiser@gmail.com>
2017-06-30 12:58:19 -0700

    Fix various errors in test_createchannel

--- contentcuration/contentcuration/tests/test_createchannel.py
+++ contentcuration/contentcuration/tests/test_createchannel.py
@@ -12,6 +12,7 @@ from django.core.urlresolvers import reverse_lazy
 
 pytestmark = pytest.mark.django_db
 
+
 @pytest.yield_fixture
 def fileobj_temp():
     randomfilebytes = ":)"
@@ -22,46 +23,57 @@ def fileobj_temp():
         f.seek(0)
         yield f
 
+
 @pytest.fixture
 def thumbnail(url, fileobj_temp):
     return base64.b64encode(fileobj_temp.read())
+
 
 @pytest.fixture
 def topic():
     return mixer.blend('contentcuration.ContentKind', kind='topic')
 
+
 @pytest.fixture
 def video():
     return mixer.blend('contentcuration.ContentKind', kind='video')
+
 
 @pytest.fixture
 def preset_video(video):
     return mixer.blend('contentcuration.FormatPreset', id='mp4', kind=video)
 
+
 @pytest.fixture
 def fileformat_mp4():
     return mixer.blend('contentcuration.FileFormat', extension='mp4', mimetype='application/video')
+
 
 @pytest.yield_fixture
 def fileobj_video(fileobj_temp, preset_video, fileformat_mp4):
     db_file_obj = mixer.blend('contentcuration.File', file_format=fileformat_mp4, preset=preset_video, file_on_disk=fileobj_temp.name)
     yield db_file_obj
 
+
 @pytest.fixture
 def license():
     return mixer.blend('contentcuration.License', license_name="License to Kill")
+
 
 @pytest.fixture
 def audio():
     return mixer.blend('contentcuration.ContentKind', kind='audio')
 
+
 @pytest.fixture
 def preset_audio(audio):
     return mixer.blend('contentcuration.FormatPreset', id='mp3', kind=audio)
 
+
 @pytest.fixture
 def fileformat_mp3():
     return mixer.blend('contentcuration.FileFormat', extension='mp3', mimetype='application/audio')
+
 
 @pytest.yield_fixture
 def fileobj_audio(fileobj_temp, preset_audio, fileformat_mp3):
@@ -73,13 +85,16 @@ def fileobj_audio(fileobj_temp, preset_audio, fileformat_mp3):
 def exercise():
     return mixer.blend('contentcuration.ContentKind', kind='exercise')
 
+
 @pytest.fixture
 def preset_exercise(exercise):
     return mixer.blend('contentcuration.FormatPreset', id='perseus', kind=exercise)
 
+
 @pytest.fixture
 def fileformat_perseus():
     return mixer.blend('contentcuration.FileFormat', extension='perseus', mimetype='application/perseus')
+
 
 @pytest.yield_fixture
 def fileobj_exercise(fileobj_temp, preset_exercise, fileformat_perseus):
@@ -91,18 +106,22 @@ def fileobj_exercise(fileobj_temp, preset_exercise, fileformat_perseus):
 def document():
     return mixer.blend('contentcuration.ContentKind', kind='document')
 
+
 @pytest.fixture
 def preset_document(document):
     return mixer.blend('contentcuration.FormatPreset', id='pdf', kind=document)
+
 
 @pytest.fixture
 def fileformat_pdf():
     return mixer.blend('contentcuration.FileFormat', extension='pdf', mimetype='application/pdf')
 
+
 @pytest.yield_fixture
 def fileobj_document(fileobj_temp, preset_document, fileformat_pdf):
     db_file_obj = mixer.blend('contentcuration.File', file_format=fileformat_pdf, preset=preset_document, file_on_disk=fileobj_temp.name)
     yield db_file_obj
+
 
 @pytest.fixture
 def channel_metadata(thumbnail):
@@ -114,6 +133,7 @@ def channel_metadata(thumbnail):
         "language": "EN",
         "thumbnail": thumbnail,
     }
+
 
 @pytest.fixture
 def topic_tree_data(fileobj_document, fileobj_video, fileobj_exercise, fileobj_audio, license, topic):
@@ -142,7 +162,7 @@ def topic_tree_data(fileobj_document, fileobj_video, fileobj_exercise, fileobj_a
                         {
                             "title": "01 - The Critique of Pure Reason",
                             "id": "facefacefacefacefacefacefaceface",
-                            "related":"deaddeaddeaddeaddeaddeaddeaddead",
+                            "related": "deaddeaddeaddeaddeaddeaddeaddead",
                             "files": [fileobj_video.checksum + '.' + fileobj_video.file_format.extension],
                             "author": "Immanuel Kant",
                             "license": license.license_name,
@@ -193,6 +213,7 @@ def topic_tree_data(fileobj_document, fileobj_video, fileobj_exercise, fileobj_a
         },
     ]
 
+
 @pytest.fixture
 def topic_tree_flat(fileobj_document, fileobj_video, fileobj_exercise, fileobj_audio, license, topic):
     return [
@@ -228,7 +249,7 @@ def topic_tree_flat(fileobj_document, fileobj_video, fileobj_exercise, fileobj_a
             "title": "01 - The Critique of Pure Reason",
             "id": "facefacefacefacefacefacefaceface",
             "parent_check": "fadefadefadefadefadefadefadefade",
-            "related":"deaddeaddeaddeaddeaddeaddeaddead",
+            "related": "deaddeaddeaddeaddeaddeaddeaddead",
             "files": [fileobj_video.checksum + '.' + fileobj_video.file_format.extension],
             "description": "",
             "author": "Immanuel Kant",
@@ -284,25 +305,31 @@ def topic_tree_flat(fileobj_document, fileobj_video, fileobj_exercise, fileobj_a
         }
     ]
 
+
 @pytest.fixture
 def url():
     return "http://127.0.0.1:8000"
+
 
 @pytest.fixture
 def fileobj_id1():
     return 'notarealid.pdf'
 
+
 @pytest.fixture
 def fileobj_id2():
     return 'notarealid.mp3'
+
 
 @pytest.fixture
 def fileobj_id3():
     return 'notarealid.mp4'
 
+
 @pytest.fixture
 def fileobj_id4():
     return 'notarealid.perseus'
+
 
 @pytest.fixture
 def file_list(fileobj_video, fileobj_audio, fileobj_document, fileobj_exercise, fileobj_id1, fileobj_id2, fileobj_id3, fileobj_id4):
@@ -317,6 +344,7 @@ def file_list(fileobj_video, fileobj_audio, fileobj_document, fileobj_exercise, 
         fileobj_id4,
     ]
 
+
 @pytest.fixture
 def file_diff(fileobj_id1, fileobj_id2, fileobj_id3, fileobj_id4):
     return [
@@ -326,9 +354,11 @@ def file_diff(fileobj_id1, fileobj_id2, fileobj_id3, fileobj_id4):
         fileobj_id4,
     ]
 
+
 @pytest.fixture
 def filename():
     return "Filename"
+
 
 @pytest.fixture
 def source_url():
@@ -340,23 +370,25 @@ def api_file_upload_response(url, fileobj_temp, fileformat_mp4, filename, source
     name = fileobj_temp.name + "." + fileformat_mp4.extension
     contenttype = fileformat_mp4.mimetype
     file_upload_url = url + str(reverse_lazy('api_file_upload'))
-    payload={
-        'filename':filename,
-        'source_url':source_url,
+    payload = {
+        'filename': filename,
+        'source_url': source_url,
         'content_type': contenttype,
         'name': name,
         'file': fileobj_temp.read(),
     }
     return Client().post(file_upload_url, data=json.dumps(payload), content_type='text/json')
 
+
 @pytest.fixture
 def api_create_channel_response(url, channel_metadata, topic_tree_data):
     create_channel_url = url + str(reverse_lazy('api_create_channel'))
-    payload={
+    payload = {
         'channel_data': channel_metadata,
-        'content_data':topic_tree_data,
+        'content_data': topic_tree_data,
     }
     return Client().post(create_channel_url, data=json.dumps(payload), content_type='text/json')
+
 
 @pytest.fixture
 def staging_tree_root(api_create_channel_response):
@@ -364,14 +396,19 @@ def staging_tree_root(api_create_channel_response):
     channel = models.Channel.objects.get(pk=channel_id)
     return channel.staging_tree
 
+
 """ FILE ENDPOINT TESTS """
+
+
 def test_api_file_upload_status(api_file_upload_response):
     assert api_file_upload_response.status_code == requests.codes.ok
+
 
 def test_api_file_upload_data(api_file_upload_response, filename, source_url):
     response = json.loads(api_file_upload_response.content)['new_file']
     file_hash = response['hash'].split('.')[0]
     assert models.File.objects.filter(pk=response['file_id'], checksum=file_hash, contentnode=None, original_filename=filename, source_url=source_url).exists()
+
 
 def test_file_diff(file_list, file_diff):
     returned_list = get_file_diff(file_list)
@@ -379,8 +416,11 @@ def test_file_diff(file_list, file_diff):
 
 
 """ TOPIC TREE CREATION TESTS """
+
+
 def test_channel_create_success(api_create_channel_response):
     assert api_create_channel_response.status_code == requests.codes.ok
+
 
 def test_channel_create_channel_created(api_create_channel_response, channel_metadata):
     channel_id = json.loads(api_create_channel_response.content)['new_channel']
@@ -389,15 +429,18 @@ def test_channel_create_channel_created(api_create_channel_response, channel_met
     thumbnail_check = channel_metadata['thumbnail']
     assert models.Channel.objects.filter(pk=channel_id, name=name_check, description=description_check, thumbnail=thumbnail_check).exists()
 
+
 def test_channel_create_staging_tree_created(staging_tree_root, topic):
     assert staging_tree_root is not None and staging_tree_root.kind == topic
+
 
 def test_channel_create_tree_created(api_create_channel_response, topic_tree_flat, staging_tree_root):
     for n in topic_tree_flat:
         node = models.ContentNode.objects.get(node_id=n['id'])
-        assert node.title==n['title'] and node.description==n['description'] and node.author == n['author'] and node.kind.pk == n['kind']
-        assert node.license.license_name==n['license'] if node.license else n['license'] is None
-        assert node.parent.node_id==n['parent_check'] or node.parent == staging_tree_root
+        assert node.title == n['title'] and node.description == n['description'] and node.author == n['author'] and node.kind.pk == n['kind']
+        assert node.license.license_name == n['license'] if node.license else n['license'] is None
+        assert node.parent.node_id == n['parent_check'] or node.parent == staging_tree_root
+
 
 def test_channel_create_files_created(api_create_channel_response, topic_tree_flat):
     for n in topic_tree_flat:
@@ -405,14 +448,16 @@ def test_channel_create_files_created(api_create_channel_response, topic_tree_fl
             node = models.ContentNode.objects.get(node_id=n['id'])
             assert len(n['files']) == node.files.all().count()
             for file_obj in node.files.all():
-                assert str(file_obj.checksum+'.'+file_obj.file_format.extension) in n['files']
+                assert str(file_obj.checksum + '.' + file_obj.file_format.extension) in n['files']
+
 
 def test_channel_create_main_tree_set(api_create_channel_response, staging_tree_root):
     channel_id = json.loads(api_create_channel_response.content)['new_channel']
-    channel =  models.Channel.objects.get(pk=channel_id)
+    channel = models.Channel.objects.get(pk=channel_id)
     assert channel.main_tree == staging_tree_root
+
 
 def test_channel_create_version_incremented(api_create_channel_response):
     channel_id = json.loads(api_create_channel_response.content)['new_channel']
-    channel =  models.Channel.objects.get(pk=channel_id)
+    channel = models.Channel.objects.get(pk=channel_id)
     assert channel.version == 1
