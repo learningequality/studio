d7461642894f944d11efd9e080798a8bcda3f114
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-06-30 10:36:42 -0700

    Fixed styling issues

--- contentcuration/contentcuration/static/js/edit_channel/channel_settings/views.js
+++ contentcuration/contentcuration/static/js/edit_channel/channel_settings/views.js
@@ -14,7 +14,7 @@ var SettingsModalView = BaseViews.BaseModalView.extend({
         this.render(this.close, {});
         this.settings_view = new SettingsView({
             el: this.$(".modal-body"),
-            modal : this,
+            modal: this,
             model: this.model,
             onsave: options.onsave
         });
@@ -25,7 +25,7 @@ var SettingsModalView = BaseViews.BaseModalView.extend({
 var SettingsView = BaseViews.BaseListEditableItemView.extend({
     template: require("./hbtemplates/settings_dialog.handlebars"),
     initialize: function(options) {
-        _.bindAll(this, 'set_thumbnail', 'reset_thumbnail', 'remove_thumbnail');
+        _.bindAll(this, "set_thumbnail", "reset_thumbnail", "remove_thumbnail");
         this.modal = options.modal;
         this.onsave = options.onsave;
         this.thumbnail_url = this.model.get("thumbnail_url");
@@ -33,26 +33,26 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
         this.render();
     },
     events: {
-      "click #settings_submit" : "submit_changes",
-      "change .input_listener" : "register_changes",
-      "keyup .input_listener" : "register_changes"
+      "click #settings_submit": "submit_changes",
+      "change .input_listener": "register_changes",
+      "keyup .input_listener": "register_changes"
     },
     render: function() {
         this.$el.html(this.template({
             channel: this.model.toJSON(),
             licenses: window.licenses.toJSON(),
-            preferences: this.model.get('preferences')
+            preferences: this.model.get("preferences")
         }));
-        $("#license_select").val(this.get_license_id(this.model.get('preferences').license));
-        $("#mastery_model_select").val(this.model.get('preferences').mastery_model);
-        $("#custom_license_description").css('display', (this.get_license_name()==="Special Permissions")? "block" : "none");
-        $("#mastery_custom_criterion").css('visibility', ($("#mastery_model_select").val()==="m_of_n")? "visible" : "hidden");
+        $("#license_select").val(this.get_license_id(this.model.get("preferences").license));
+        $("#mastery_model_select").val(this.model.get("preferences").mastery_model);
+        $("#custom_license_description").css("display", (this.get_license_name()==="Special Permissions")? "block" : "none");
+        $("#mastery_custom_criterion").css("visibility", ($("#mastery_model_select").val()==="m_of_n")? "visible" : "hidden");
         this.image_upload = new FileViews.ThumbnailUploadView({
             model: this.model,
             el: this.$("#channel_thumbnail"),
-            preset_id: 'channel_thumbnail',
+            preset_id: "channel_thumbnail",
             upload_url: window.Urls.thumbnail_upload(),
-            acceptedFiles: 'image/jpeg,image/jpeg,image/png',
+            acceptedFiles: "image/jpeg,image/jpeg,image/png",
             image_url: this.thumbnail_url,
             default_url: "/static/img/kolibri_placeholder.png",
             onsuccess: this.set_thumbnail,
@@ -67,32 +67,32 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
         return window.licenses.findWhere({license_name: license_name}).id;
     },
     get_license_name: function(){
-        return window.licenses.get($("#license_select").val()).get('license_name');
+        return window.licenses.get($("#license_select").val()).get("license_name");
     },
     submit_changes:function(){
-        var preferences = this.model.get('preferences');
-        preferences["license"] = this.get_license_name();
-        preferences["author"] = $("#author_field").val().trim();
-        preferences["copyright_holder"] = $("#input_copyright_holder").val().trim();
-        preferences["license_description"] = $("#custom_license_description").val().trim();
-        preferences["mastery_model"] = $("#mastery_model_select").val();
-        preferences["m_value"] = $("#m_value").val();
-        preferences["n_value"] = $("#n_value").val();
-        preferences["auto_derive_video_thumbnail"] = $("#auto_video_thumbnail").is(":checked");
-        preferences["auto_derive_audio_thumbnail"] = $("#auto_audio_thumbnail").is(":checked");
-        preferences["auto_derive_document_thumbnail"] = $("#auto_document_thumbnail").is(":checked");
-        preferences["auto_derive_html5_thumbnail"] = $("#auto_html5_thumbnail").is(":checked");
+        var preferences = this.model.get("preferences");
+        preferences.license = this.get_license_name();
+        preferences.author = $("#author_field").val().trim();
+        preferences.copyright_holder = $("#input_copyright_holder").val().trim();
+        preferences.license_description = $("#custom_license_description").val().trim();
+        preferences.mastery_model = $("#mastery_model_select").val();
+        preferences.m_value = $("#m_value").val();
+        preferences.n_value = $("#n_value").val();
+        preferences.auto_derive_video_thumbnail = $("#auto_video_thumbnail").is(":checked");
+        preferences.auto_derive_audio_thumbnail = $("#auto_audio_thumbnail").is(":checked");
+        preferences.auto_derive_document_thumbnail = $("#auto_document_thumbnail").is(":checked");
+        preferences.auto_derive_html5_thumbnail = $("#auto_html5_thumbnail").is(":checked");
         var self = this;
-        $("#settings_submit").html("Saving...");
-        $("#settings_submit").attr('disabled', 'disabled');
-        $("#settings_submit").addClass('disabled');
+        $("#settings_submit").html("Saving...")
+                            .attr("disabled", "disabled")
+                            .addClass("disabled");
         this.save({
             "name": $("#input_title").val().trim(),
             "description": $("#input_description").val(),
-            "thumbnail": this.model.get('thumbnail'),
+            "thumbnail": this.model.get("thumbnail"),
             "preferences": JSON.stringify(preferences)
         }).then(function(data){
-            self.onsave(data)
+            self.onsave(data);
             $("#settings_submit").html("SAVED!");
             setTimeout(function(){
                 $("#settings_submit").html("No Changes Found");
@@ -100,13 +100,15 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
         });
     },
     register_changes:function(){
-        $("#custom_license_description").css('display', (this.get_license_name()==="Special Permissions")? "block" : "none");
-        $("#mastery_custom_criterion").css('visibility', ($("#mastery_model_select").val()==="m_of_n")? "visible" : "hidden");
-        if(Number($("#m_value").val()) > Number($("#n_value").val())) $("#n_value").val($("#m_value").val());
+        $("#custom_license_description").css("display", (this.get_license_name()==="Special Permissions")? "block" : "none");
+        $("#mastery_custom_criterion").css("visibility", ($("#mastery_model_select").val()==="m_of_n")? "visible" : "hidden");
+        if(Number($("#m_value").val()) > Number($("#n_value").val())) {
+            $("#n_value").val($("#m_value").val());
+        }
         $("#settings_submit").html("SAVE CHANGES");
 
         var isvalid = $("#input_title").val().trim() !== "";
-        $("#channel_error").css('display', (isvalid)? "none" : "inline-block");
+        $("#channel_error").css("display", (isvalid)? "none" : "inline-block");
         if(isvalid){
             $("#settings_submit").attr("disabled", false);
             $("#settings_submit").removeClass("disabled");
@@ -120,11 +122,11 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
         this.register_changes();
     },
     remove_thumbnail:function(){
-        this.model.set('thumbnail', "/static/img/kolibri_placeholder.png");
+        this.model.set("thumbnail", "/static/img/kolibri_placeholder.png");
         this.register_changes();
     },
     set_thumbnail:function(thumbnail, formatted_name, path){
-        this.model.set('thumbnail', formatted_name);
+        this.model.set("thumbnail", formatted_name);
         this.register_changes();
     },
     set_editing: function(edit_mode_on){
