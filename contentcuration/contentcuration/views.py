2fcf5f619ac664b6d7b0629f818b4404631803d5
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-07-12 12:20:30 -0700

    Make endpoint public

--- contentcuration/contentcuration/views.py
+++ contentcuration/contentcuration/views.py
@@ -14,7 +14,7 @@ from contentcuration.api import check_supported_browsers, add_editor_to_channel,
 from contentcuration.models import VIEW_ACCESS, Language, Channel, License, FileFormat, FormatPreset, ContentKind, ContentNode, Invitation
 from contentcuration.serializers import LanguageSerializer, RootNodeSerializer, ChannelListSerializer, ChannelSerializer, LicenseSerializer, FileFormatSerializer, FormatPresetSerializer, ContentKindSerializer, CurrentUserSerializer, UserChannelListSerializer, InvitationSerializer
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
 
@@ -244,7 +244,9 @@ def get_staged_diff_endpoint(request):
     if request.method == 'POST':
         return HttpResponse(json.dumps(get_staged_diff(json.loads(request.body)['channel_id'])))
 
+
 @api_view(['GET'])
+@permission_classes((AllowAny,))
 def get_channel_name_by_id(request):
     """ Endpoint: /public/channel/?id=<channel_id> """
     channel_id = request.query_params.get('id')
