81ba4047d94d9abcf9d226f47c8dcc323f9acbdb
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-07-20 18:53:14 -0700

    Finished channel settings translations

--- contentcuration/contentcuration/static/js/edit_channel/channel_settings/views.js
+++ contentcuration/contentcuration/static/js/edit_channel/channel_settings/views.js
@@ -5,9 +5,40 @@ var Models = require("edit_channel/models");
 var FileViews = require("edit_channel/file_upload/views");
 require("channel_settings.less");
 
+var NAMESPACE = "channel_settings";
+var MESSAGES = {
+    "header": "CHANNEL SETTINGS",
+    "saving": "Saving...",
+    "saved": "SAVED!",
+    "no_changes": "No Changes Found",
+    "save_changes": "SAVE CHANGES",
+    "close": "CLOSE",
+    "channel_name": "Channel Name",
+    "channel_name_error": "Channel name cannot be blank.",
+    "channel_name_placeholder": "Enter channel name...",
+    "channel_description": "Channel Description",
+    "description_placeholder": "Enter channel description...",
+    "content_defaults": "Content Defaults",
+    "content_defaults_prompt": "Set defaults for new content items",
+    "author": "Author",
+    "author_placeholder": "Enter author name...",
+    "license": "License",
+    "license_description_placeholder": "Enter license description...",
+    "copyright_holder": "Copyright Holder",
+    "copyright_holder_placeholder": "Enter copyright holder name...",
+    "exercise_criteria": "Exercise Mastery Criteria",
+    "of": "of",
+    "auto_thumbnail": "Automatically generate thumbnails for...",
+    "videos": "Videos",
+    "audio": "Audio",
+    "html": "HTML Apps",
+    "documents": "Documents",
+}
+
 var SettingsModalView = BaseViews.BaseModalView.extend({
     template: require("./hbtemplates/settings_modal.handlebars"),
-
+    name: NAMESPACE,
+    messages: MESSAGES,
     initialize: function(options) {
         this.parent_view = options.parent_view;
         this.modal = true;
@@ -24,6 +55,8 @@ var SettingsModalView = BaseViews.BaseModalView.extend({
 
 var SettingsView = BaseViews.BaseListEditableItemView.extend({
     template: require("./hbtemplates/settings_dialog.handlebars"),
+    name: NAMESPACE,
+    messages: MESSAGES,
     initialize: function(options) {
         _.bindAll(this, "set_thumbnail", "reset_thumbnail", "remove_thumbnail");
         this.modal = options.modal;
@@ -42,6 +75,8 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
             channel: this.model.toJSON(),
             licenses: window.licenses.toJSON(),
             preferences: this.model.get("preferences")
+        },  {
+            data: this.get_intl_data()
         }));
         $("#license_select").val(this.get_license_id(this.model.get("preferences").license));
         $("#mastery_model_select").val(this.model.get("preferences").mastery_model);
@@ -83,7 +118,7 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
         preferences.auto_derive_document_thumbnail = $("#auto_document_thumbnail").is(":checked");
         preferences.auto_derive_html5_thumbnail = $("#auto_html5_thumbnail").is(":checked");
         var self = this;
-        $("#settings_submit").html("Saving...")
+        $("#settings_submit").html(this.get_translation("saving"))
                             .attr("disabled", "disabled")
                             .addClass("disabled");
         this.save({
@@ -93,9 +128,9 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
             "preferences": JSON.stringify(preferences)
         }).then(function(data){
             self.onsave(data);
-            $("#settings_submit").html("SAVED!");
+            $("#settings_submit").html(self.get_translation("saved"));
             setTimeout(function(){
-                $("#settings_submit").html("No Changes Found");
+                $("#settings_submit").html(self.get_translation("no_changes"));
             }, 2000);
         });
     },
@@ -105,7 +140,7 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
         if(Number($("#m_value").val()) > Number($("#n_value").val())) {
             $("#n_value").val($("#m_value").val());
         }
-        $("#settings_submit").html("SAVE CHANGES");
+        $("#settings_submit").html(this.get_translation("save_changes"));
 
         var isvalid = $("#input_title").val().trim() !== "";
         $("#channel_error").css("display", (isvalid)? "none" : "inline-block");
--- contentcuration/contentcuration/static/js/edit_channel/utils/string_helper.js
+++ contentcuration/contentcuration/static/js/edit_channel/utils/string_helper.js
@@ -1,4 +1,5 @@
 var globalMessageStore = require("utils/translations");
+var _ = require("underscore");
 
 function format_size(text){
   if (!text) {
@@ -29,12 +30,20 @@ var CONSTANT_TRANSLATIONS = {
   "document": "Document",
   "exercise": "Exercise",
   "html5": "HTML5 App",
-  "do_all": "Do All",
+  "do_all": "100% Correct",
   "num_correct_in_a_row_10": "10 in a row",
   "num_correct_in_a_row_2": "2 in a row",
   "num_correct_in_a_row_3": "3 in a row",
   "num_correct_in_a_row_5": "5 in a row",
-  "m_of_n": "M out of N",
+  "m_of_n": "M of N...",
+  "do_all_description": "Learner must answer all questions in the exercise correctly (not recommended for long exercises)",
+  "num_correct_in_a_row_10_description": "Learner must answer ten questions in a row correctly",
+  "num_correct_in_a_row_2_description": "Learner must answer two questions in a row correctly",
+  "num_correct_in_a_row_3_description": "Learner must answer three questions in a row correctly",
+  "num_correct_in_a_row_5_description": "Learner must answer five questions in a row correctly",
+  "m_of_n_description": "Learner must answer M questions correctly from the last N questions answered " +
+                        "(e.g. 3 out of 5 means learners need to answer 3 questions correctly out of the " +
+                        "5 most recently answered questions)",
   "input_question": "Input Question",
   "multiple_selection": "Multiple Selection",
   "single_selection": "Single Selection",
@@ -73,10 +82,39 @@ var CONSTANT_TRANSLATIONS = {
   "CC BY-NC-ND": "CC BY-NC-ND",
   "All Rights Reserved": "All Rights Reserved",
   "Public Domain": "Public Domain",
-  "Special Permissions": "Special Permissions"
+  "Special Permissions": "Special Permissions",
+  "CC BY_description": "The Attribution License lets others distribute, remix, tweak, " +
+                      "and build upon your work, even commercially, as long as they credit " +
+                      "you for the original creation. This is the most accommodating of licenses " +
+                      "offered. Recommended for maximum dissemination and use of licensed materials.",
+  "CC BY-SA_description": "The Attribution-ShareAlike License lets others remix, tweak, and build " +
+                      "upon your work even for commercial purposes, as long as they credit you and " +
+                      "license their new creations under the identical terms. This license is often " +
+                      "compared to \"copyleft\" free and open source software licenses. All new works based " +
+                      "on yours will carry the same license, so any derivatives will also allow commercial use. " +
+                      "This is the license used by Wikipedia, and is recommended for materials that would benefit " +
+                      "from incorporating content from Wikipedia and similarly licensed projects.",
+  "CC BY-ND_description": "The Attribution-NoDerivs License allows for redistribution, commercial and " +
+                      "non-commercial, as long as it is passed along unchanged and in whole, with credit to you.",
+  "CC BY-NC_description": "The Attribution-NonCommercial License lets others remix, tweak, and build upon your " +
+                      "work non-commercially, and although their new works must also acknowledge you and be " +
+                      "non-commercial, they don't have to license their derivative works on the same terms.",
+  "CC BY-NC-SA_description": "The Attribution-NonCommercial-ShareAlike License lets others remix, tweak, and build " +
+                      "upon your work non-commercially, as long as they credit you and license their new creations " +
+                      "under the identical terms.",
+  "CC BY-NC-ND_description": "The Attribution-NonCommercial-NoDerivs License is the most restrictive of our six main " +
+                      "licenses, only allowing others to download your works and share them with others as long as they " +
+                      "credit you, but they can't change them in any way or use them commercially.",
+  "All Rights Reserved_description": "The All Rights Reserved License indicates that the copyright holder reserves, or " +
+                      "holds for their own use, all the rights provided by copyright law under one specific copyright treaty.",
+  "Public Domain_description": "Public Domain work has been identified as being free of known restrictions under copyright " +
+                      "law, including all related and neighboring rights.",
+  "Special Permissions_description": "Special Permissions is a custom license to use when the current licenses do not apply to " +
+                      "the content. The owner of this license is responsible for creating a description of what this license entails.",
 }
 
 function translate(constant_id){
+  console.log(constant_id)
   var messages = _.extend(CONSTANT_TRANSLATIONS, globalMessageStore["constants"] || {});
   return messages[constant_id];
 }
