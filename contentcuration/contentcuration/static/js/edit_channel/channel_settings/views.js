a9cf54a9f076f167602003e16eea28ef2577e944
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-10-03 12:32:16 -0700

    Fixed channel settings thumbnail bug

--- contentcuration/contentcuration/static/js/edit_channel/channel_settings/views.js
+++ contentcuration/contentcuration/static/js/edit_channel/channel_settings/views.js
@@ -47,7 +47,7 @@ var SettingsModalView = BaseViews.BaseModalView.extend({
             model: this.model,
             onsave: options.onsave
         });
-        this.$("#channel_settings_modal").on("shown.bs.modal", this.settings_view.init_focus);
+        this.$("#channel_settings_modal").on("shown.bs.modal", this.settings_view.create_initial);
     }
 });
 
@@ -57,7 +57,7 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
     name: NAMESPACE,
     $trs: MESSAGES,
     initialize: function(options) {
-        _.bindAll(this, "set_thumbnail", "reset_thumbnail", "remove_thumbnail", "init_focus");
+        _.bindAll(this, "set_thumbnail", "reset_thumbnail", "remove_thumbnail", "init_focus", "create_initial");
         this.modal = options.modal;
         this.onsave = options.onsave;
         this.thumbnail_url = this.model.get("thumbnail_url");
@@ -84,6 +84,8 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
         $("#custom_license_description").css("display", (this.get_license_name()==="Special Permissions")? "block" : "none");
         $("#mastery_custom_criterion").css("visibility", ($("#mastery_model_select").val()==="m_of_n")? "visible" : "hidden");
         $("#select_language").val(this.model.get("language") || 0);
+    },
+    create_initial: function() {
         this.image_upload = new Images.ThumbnailUploadView({
             model: this.model,
             el: this.$("#channel_thumbnail"),
@@ -100,6 +102,7 @@ var SettingsView = BaseViews.BaseListEditableItemView.extend({
             allow_edit: true,
             is_channel: true
         });
+        this.init_focus();
     },
     init_focus: function(){
         this.set_indices();
