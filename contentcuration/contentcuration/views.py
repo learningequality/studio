8e9ebc1c7d570243280425bd3d67a86734682aa0
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-10-09 12:26:37 -0700

    Separated health/stealth endpoint

--- contentcuration/contentcuration/views.py
+++ contentcuration/contentcuration/views.py
@@ -29,8 +29,10 @@ def base(request):
 
 
 def health(request):
-    return HttpResponse("500")
+    return HttpResponse(Channel.objects.first().name)
 
+def stealth(request):
+    return HttpResponse("500")
 
 def unsupported_browser(request):
     return render(request, 'unsupported_browser.html')
