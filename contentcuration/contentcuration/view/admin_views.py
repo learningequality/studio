ad1e1f462f7ee5dbdf3ef66896cca8aec0237a5c
Jordan Yoshihara <jordanyoshihara@gmail.com>
2018-02-26 11:30:04 -0800

    Added requirement

--- contentcuration/contentcuration/view/admin_views.py
+++ contentcuration/contentcuration/view/admin_views.py
@@ -25,7 +25,6 @@ from contentcuration.api import check_supported_browsers
 from contentcuration.models import Channel, User, Invitation, ContentNode
 from contentcuration.utils.messages import get_messages
 from contentcuration.serializers import AdminChannelListSerializer, AdminUserListSerializer, CurrentUserSerializer
-from reportlab.pdfgen import canvas
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
