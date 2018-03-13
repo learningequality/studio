ccefb023a0e12711763917ab202617e775dd7d90
Aron Fyodor Asor <aronasorman@gmail.com>
2018-03-12 17:00:42 -0700

    make celery endpoint specification more complicated
    because redis now has a password

--- contentcuration/contentcuration/production_settings.py
+++ contentcuration/contentcuration/production_settings.py
@@ -29,9 +29,16 @@ DATABASES = {
 }
 
 # celery settings
-BROKER_URL = os.getenv("CELERY_BROKER_URL") or BROKER_URL
-CELERY_RESULT_BACKEND = (os.getenv("CELERY_RESULT_BACKEND_URL")
-                         or CELERY_RESULT_BACKEND)
+BROKER_URL = "redis://:{password}@{endpoint}:/{db}".format(
+    password=os.getenv("CELERY_REDIS_PASSWORD"),
+    endpoint=os.getenv("CELERY_BROKER_ENDPOINT"),
+    db=os.getenv("CELERY_REDIS_DB")
+) or BROKER_URL
+CELERY_RESULT_BACKEND = "redis://:{password}@{endpoint}:/{db}".format(
+    password=os.getenv("CELERY_REDIS_PASSWORD"),
+    endpoint=os.getenv("CELERY_RESULT_BACKEND_ENDPOINT"),
+    db=os.getenv("CELERY_REDIS_DB")
+) or CELERY_RESULT_BACKEND
 CELERY_TIMEZONE = os.getenv("CELERY_TIMEZONE") or CELERY_TIMEZONE
 
 # email settings
