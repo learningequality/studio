616b63c88783e64f0479f6ca344aee302182138b
Aron Fyodor Asor <aronasorman@gmail.com>
2018-03-20 16:39:25 -0700

    allow any hosts

--- contentcuration/contentcuration/dev_settings.py
+++ contentcuration/contentcuration/dev_settings.py
@@ -3,7 +3,7 @@ from .settings import *
 import logging
 
 DEBUG = True
-ALLOWED_HOSTS = ["studio.local", "192.168.31.9", "127.0.0.1"]
+ALLOWED_HOSTS = ["*", "192.168.99.102"]
 
 ACCOUNT_ACTIVATION_DAYS = 7
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
