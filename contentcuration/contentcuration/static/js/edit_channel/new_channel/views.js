aead41800a518074599c4e55504c149f0c6b660b
David Hu <davidhu91@gmail.com>
2018-04-06 17:12:52 -0700

    Support ctrl+click on windows

--- contentcuration/contentcuration/static/js/edit_channel/new_channel/views.js
+++ contentcuration/contentcuration/static/js/edit_channel/new_channel/views.js
@@ -330,7 +330,7 @@ var ChannelListItem = BaseViews.BaseListEditableItemView.extend({
 	open_channel:function(event){
 		if(this.$('.channel-container-wrapper').hasClass('highlight')){
 			var open_url = '/channels/' + this.model.get("id") + ((this.can_edit)? '/edit' : '/view');
-			if (event.metaKey) {
+			if (event.metaKey || event.ctrlKey) {
 				window.open(open_url, '_blank');
 			} else {
 				window.location.href = open_url;
