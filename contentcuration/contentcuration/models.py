92c2eae24969168857d3b5fae5a5c303fbe7b328
Jordan Yoshihara <jordanyoshihara@gmail.com>
2018-04-13 12:20:37 -0700

    Fix name error

--- contentcuration/contentcuration/models.py
+++ contentcuration/contentcuration/models.py
@@ -436,7 +436,7 @@ class Channel(models.Model):
 
             # Delete db if channel has been deleted and mark as unpublished
             if not original_channel.deleted and self.deleted:
-                channel.pending_editors.all().delete()
+                self.pending_editors.all().delete()
                 channel_db_url = os.path.join(settings.DB_ROOT, self.id) + ".sqlite3"
                 if os.path.isfile(channel_db_url):
                     os.unlink(channel_db_url)
