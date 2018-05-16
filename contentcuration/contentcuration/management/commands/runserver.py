b5ab2c376eb0572889e5afaadce7e6363855a574
Aron Fyodor Asor <aronasorman@gmail.com>
2018-05-16 12:03:07 -0700

    launch ptvsd on the dev server

--- contentcuration/contentcuration/management/commands/runserver.py
+++ contentcuration/contentcuration/management/commands/runserver.py
@@ -1,15 +1,18 @@
 from __future__ import print_function
 
 import atexit
+import logging
 import os
 import subprocess
 from threading import Thread
 
-from contentcuration.utils.minio_utils import (ensure_storage_bucket_public,
-                                               start_minio)
+from django.conf import settings
 from django.contrib.staticfiles.management.commands.runserver import \
     Command as RunserverCommand
 from django.core.management.base import CommandError
+
+from contentcuration.utils.minio_utils import (ensure_storage_bucket_public,
+                                               start_minio)
 
 
 class Command(RunserverCommand):
@@ -23,10 +26,18 @@ class Command(RunserverCommand):
     def handle(self, *args, **options):
         return super(Command, self).handle(*args, **options)
 
-    def start_minio(self):
-        self.stdout.write("Starting minio")
+    def setup_remote_debugging(self):
+        """
+        If you have Visual Studio Code running, you can connect to port 3000
+        and set up breakpoints through your editor.
+        """
+        # Don't attempt to set up remote debugging if we're not in
+        # debug mode
+        if not settings.DEBUG:
+            return
 
-        self.minio_process = subprocess.Popen(
-            ["run_minio.py"],
-            stdin=subprocess.PIPE,
-        )
+        try:
+            import ptvsd
+        except ImportError:
+            logging.info("ptvsd not installed; not setting up remote debugger")
+            return
