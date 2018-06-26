34b876e534229ef780c695b182e1fd4a7e14e7dc
Aron Fyodor Asor <191955+aronasorman@users.noreply.github.com>
2018-06-26 11:56:56 -0700

    delete old ksonnet code
    Moving to helm now!

--- k8s/images/nginx/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Run yasha (a cli jinja templating engine) to generate the real nginx.conf file
-sigil -f /etc/nginx/nginx.conf.jinja2 aws_s3_bucket_name=$AWS_BUCKET_NAME aws_s3_endpoint_url=$AWS_S3_ENDPOINT_URL > /etc/nginx/nginx.conf
-
-nginx -c /etc/nginx/nginx.conf
