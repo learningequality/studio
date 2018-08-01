1d235333574321b9a651c32992d72429dd3680c6
kollivier <kevino@theolliviers.com>
2018-07-31 17:43:34 -0700

    Make sure we don't use sort_order 0 when creating the user's clipboard root node.

--- contentcuration/contentcuration/models.py
+++ contentcuration/contentcuration/models.py
@@ -216,7 +216,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
         if not self.clipboard_tree:
             self.clipboard_tree = ContentNode.objects.create(title=self.email + " clipboard", kind_id="topic",
-                                                             sort_order=0)
+                                                             sort_order=get_next_sort_order())
             self.clipboard_tree.save()
             changed = True
 
