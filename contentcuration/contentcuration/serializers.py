91658ee3ad343ccfd01b4b321ffeecda518004b2
Micah Fitch <fitchmicah@gmail.com>
2018-08-28 21:45:34 -0400

    file uploading works... yay!

--- contentcuration/contentcuration/serializers.py
+++ contentcuration/contentcuration/serializers.py
@@ -77,53 +77,20 @@ class FileListSerializer(serializers.ListSerializer):
         user = self.context['request'].user
         import ipdb
         with transaction.atomic():
-            for item in validated_data:
-                ipdb.set_trace()
-                # item.update({
-                #     # 'preset_id': item['preset']['id'],
-                #     'language_id': item.get('language')['id'] if item.get('language') else None
-                # })
-
-                # # User should not be able to change files without a display
-                # if item['preset']['display']:
-                #     if 'id' in item:
-                #         update_files[item['id']] = item
-                #     else:
-                #         # create new nodes
-                #         ret.append(File.objects.create(**item))
-                # item.pop('preset', None)
-                # item.pop('language', None)
-
-        files_to_delete = []
-        nodes_to_parse = []
-        current_files = [f['id'] for f in validated_data]
-
         # Get files that have the same contentnode, preset, and language as the files that are now attached to this node
-        for file_obj in validated_data:
-            file_obj = File.objects.get(pk=file_obj['id'])
-            file_obj = file_obj.contentnode.files.exclude(pk=file_obj.pk)\
-                        .filter(preset_id=file_obj.preset_id, language_id=file_obj.language_id)\
-                        .delete()
-            # delete_queryset = File.objects.filter(
-            #     Q(contentnode=file_obj['contentnode']) &  # Get files that are associated with this node
-            #     (Q(preset_id=file_obj['preset_id']) | Q(
-            #         preset=None)) &  # Look at files that have the same preset as this file
-            #     Q(language_id=file_obj.get('language_id')) &  # Look at files with the same language as this file
-            #     ~Q(id=file_obj['id'])  # Remove the file if it's not this file
-            # )
-            # files_to_delete += [f for f in delete_queryset.all()]
-        #     if file_obj['contentnode'] not in nodes_to_parse:
-        #         nodes_to_parse.append(file_obj['contentnode'])
+            for item in validated_data:
 
-        # # Delete removed files
-        # for node in nodes_to_parse:
-        #     previous_files = node.files.all()
-        #     for f in previous_files:
-        #         if f.id not in current_files:
-        #             files_to_delete.append(f)
+                file_obj = File.objects.get(pk=item['id'])
+                files_to_replace = item['contentnode'].files.exclude(pk=file_obj.pk)\
+                            .filter(preset_id=file_obj.preset_id, language_id=file_obj.language_id)
+                files_to_replace.delete()
 
-        # for to_delete in files_to_delete:
-        #     to_delete.delete()
+                if file_obj.preset and file_obj.preset.display:
+                    if file_obj.pk:
+                        update_files[file_obj.pk] = item
+                    else:
+                        # create new nodes
+                        ret.append(File.objects.create(**item))
 
         if update_files:
             with transaction.atomic():
