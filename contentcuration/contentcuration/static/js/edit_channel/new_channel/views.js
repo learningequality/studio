08c085e35bf3893636e93df74a096baefdacbb17
kollivier <kevino@theolliviers.com>
2018-10-27 08:11:24 -0700

    Sanity check that lists are populated before calling methods on them. Also remove error handlers, as the API calls aren't erroring out, just slow.

--- contentcuration/contentcuration/static/js/edit_channel/new_channel/views.js
+++ contentcuration/contentcuration/static/js/edit_channel/new_channel/views.js
@@ -74,56 +74,28 @@ var ChannelListPage  = BaseViews.BaseView.extend({
 				el: self.$("#channel_list"),
 				collection: channels
 			});
-		}).catch(function(e) {
-			  if (Raven && Raven.catchException) {
-			    Raven.catchException(e);
-			  } else {
-			    console.log("Error retrieving current channel list:");
-			    console.log(e.statusText);
-			  }
-			});
+		});
 		State.current_user.get_bookmarked_channels().then(function(channels){
 			self.starred_channel_list = new StarredChannelList({
 				container: self,
 				el: self.$("#starred_list"),
 				collection: channels
 			});
-		}).catch(function(e) {
-			  if (Raven && Raven.catchException) {
-			    Raven.catchException(e);
-			  } else {
-			    console.log("Error retrieving bookmarked channel list:");
-			    console.log(e.statusText);
-			  }
-			});
+		});
 		State.current_user.get_public_channels().then(function(channels){
 			self.public_channel_list = new PublicChannelList({
 				container: self,
 				el: self.$("#public_list"),
 				collection: channels
 			});
-		}).catch(function(e) {
-			  if (Raven && Raven.catchException) {
-			    Raven.catchException(e);
-			  } else {
-			    console.log("Error retrieving public channel list:");
-			    console.log(e.statusText);
-			  }
-			});
+		});
 		State.current_user.get_view_only_channels().then(function(channels){
 			self.viewonly_channel_list = new ViewOnlyChannelList({
 				container: self,
 				el: self.$("#viewonly_list"),
 				collection: channels
 			});
-		}).catch(function(e) {
-			  if (Raven && Raven.catchException) {
-			    Raven.catchException(e);
-			  } else {
-			    console.log("Error retrieving view-only channel list:");
-			    console.log(e.statusText);
-			  }
-			});
+		});
 	},
 	events: {
 		'click .new_channel_button' : 'new_channel',
@@ -163,10 +135,22 @@ var ChannelListPage  = BaseViews.BaseView.extend({
 	},
 	set_active_channel(channel) {
 		this.$el.removeClass("active_channel");
-		this.starred_channel_list.set_active_channel(channel);
-		this.current_channel_list.set_active_channel(channel);
-		this.public_channel_list.set_active_channel(channel);
-		this.viewonly_channel_list.set_active_channel(channel);
+		// This function gets called by open_channel, but since the API calls to populate the channels list can be slow,
+		// these lists may be undefined when this gets called. For now, just check that the list is undefined to prevent
+		// this from throwing errors.
+		// TODO: The real fix is to make the channel list code more performant so that this doesn't happen.
+		if (this.starred_channel_list) {
+		  this.starred_channel_list.set_active_channel(channel);
+		}
+		if (this.current_channel_list) {
+		  this.current_channel_list.set_active_channel(channel);
+		}
+		if (this.public_channel_list) {
+		  this.public_channel_list.set_active_channel(channel);
+		}
+		if (this.viewonly_channel_list) {
+		  this.viewonly_channel_list.set_active_channel(channel);
+		}
 	},
 	set_all_models: function(channel){
 		this.starred_channel_list.set_model(channel);
