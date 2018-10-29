a857995a57b9681c946b9c98f7741ad5d1fcdc35
Kevin Ollivier <kevino@theolliviers.com>
2018-10-29 09:03:50 -0700

    Get detailed Sentry error reports for AJAX call errors. (#1068)

--- contentcuration/contentcuration/static/js/bundle_modules/base.js
+++ contentcuration/contentcuration/static/js/bundle_modules/base.js
@@ -16,6 +16,30 @@ if(!global.Promise) {
 
 
 $(function() {
+  $(document).ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
+    let message = thrownError || jqXHR.statusText;
+    // Put the URL in the main message for timeouts so we can see which timeouts are most frequent.
+    if (jqXHR.status === 504) {
+      message = "Request Timed Out: " + ajaxSettings.url;
+    }
+    const extraData = {
+              type: ajaxSettings.type,
+              url: ajaxSettings.url,
+              data: ajaxSettings.data,
+              status: jqXHR.status,
+              error: thrownError || jqXHR.statusText,
+              response: jqXHR.responseText.substring(0, 100)
+    };
+
+    console.log("AJAX Request Error: " + message);
+    console.log("Error data: " + JSON.stringify(extraData));
+    if (Raven && Raven.captureMessage) {
+      Raven.captureMessage(message, {
+          extra: extraData
+      });
+    }
+  });
+
   // TODO revaluate need. From their repo:
   // Note: As of late 2015 most mobile browsers - notably Chrome and Safari - no longer have a 300ms touch delay, so fastclick offers no benefit on newer browsers, and risks introducing bugs into your application. Consider carefully whether you really need to use it.
   fastclick.attach(document.body);
