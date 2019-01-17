fbb9c2bc6f29b059da09764b563441ae687aee47
Lingyi Wang <lywang07@gmail.com>
2019-01-16 17:45:25 -0800

    Add a progress tracker to the async task command

--- contentcuration/contentcuration/utils/asynccommand.py
+++ contentcuration/contentcuration/utils/asynccommand.py
@@ -1,16 +1,36 @@
 from abc import abstractmethod
+from collections import namedtuple
 
 from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
+
+Progress = namedtuple(
+    'Progress',
+    [
+        'progress',
+        'total',
+        'fraction',
+    ]
+)
 
 
 class TaskCommand(BaseCommand):
-    def start_progress(self, *args, **options):
-        # TODO: needs implementation
-        pass
+    def handle(self, *args, **options):
+        self.progresstracker = None
+        return self.handle_async(*args, **options)
 
-    def update_progress(self, *args, **options):
-        # TODO: needs implementation
-        pass
+    def start_progress(self, total):
+        self.progresstracker = Progress(progress=0, total=total, fraction=0)
+
+    def update_progress(self, increment):
+        tracker = self.progresstracker
+        progress = tracker.progress + increment
+        if progress > tracker.total:
+            raise CommandError("Progress reaches over 100%.")
+
+        fraction = 1.0 * progress / tracker.total
+        updated_tracker = tracker._replace(progress=progress, fraction=fraction)
+        self.progresstracker = updated_tracker
 
     @abstractmethod
     def handle_async(self, *args, **options):
