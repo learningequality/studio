26b6afb59a1bb47bdd6bc84298b3b0d82e635572
Micah Fitch <micah@learningequality.org>
2019-02-05 15:09:39 -0600

    more specific error message when primary modal is already open

--- contentcuration/contentcuration/static/js/edit_channel/vuexModules/primaryModal.js
+++ contentcuration/contentcuration/static/js/edit_channel/vuexModules/primaryModal.js
@@ -1,8 +1,8 @@
-const ERROR_PRIMARY_MODAL_ALREADY_OPEN = 'The primary modal is already open.'
+const ERROR_PRIMARY_MODAL_ALREADY_OPEN = (modalName) => `The primary modal is in use by: ${modalName} modal.`
 
 const primaryModalModule = {
     state: {
-      primaryModalInUse: false,
+      primaryModalInUse: null,
     },
     getters: {
       primaryModalInUse(state) {
@@ -10,19 +10,19 @@ const primaryModalModule = {
       },
     },
     mutations: {
-      OPEN_PRIMARY_MODAL: (state) => { state.primaryModalInUse = true },
-      CLOSE_PRIMARY_MODAL: (state) => { state.primaryModalInUse = false },
+      OPEN_PRIMARY_MODAL: (state, payload) => { state.primaryModalInUse = payload },
+      CLOSE_PRIMARY_MODAL: (state) => { state.primaryModalInUse = null },
     },
     actions: {
       usePrimaryModal({state, commit}, buildModalView){
         return new Promise((resolve, reject) => {
           if (!state.primaryModalInUse) {
             let modalView = buildModalView()
-            commit('OPEN_PRIMARY_MODAL')
+            commit('OPEN_PRIMARY_MODAL', modalView)
             modalView.listenTo(modalView, 'removed', () => commit('CLOSE_PRIMARY_MODAL'))
             resolve(modalView)
           } else {
-            throw new Error(ERROR_PRIMARY_MODAL_ALREADY_OPEN)
+            throw new Error(ERROR_PRIMARY_MODAL_ALREADY_OPEN(state.primaryModalInUse.name))
           }
         })
       },
