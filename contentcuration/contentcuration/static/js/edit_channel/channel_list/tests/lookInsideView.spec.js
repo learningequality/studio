e958ef37ae842e908907fbf0f354d90586bc2f2b
Micah Fitch <micah@learningequality.org>
2019-03-22 17:58:05 -0500

    collapse redundant import lines into one line

--- contentcuration/contentcuration/static/js/edit_channel/channel_list/tests/lookInsideView.spec.js
+++ contentcuration/contentcuration/static/js/edit_channel/channel_list/tests/lookInsideView.spec.js
@@ -1,7 +1,6 @@
 import { shallowMount } from '@vue/test-utils';
 import LookInsideView from './../views/LookInsideView.vue';
-import { localStore } from './data.js';
-import { mockFunctions } from './data.js';
+import { localStore, mockFunctions } from './data.js';
 
 function makeWrapper() {
   return shallowMount(LookInsideView, {
