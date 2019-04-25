2dd9efe5fd9ab7e9eb7d5130801575b4bec688ec
Jordan Yoshihara <jordanyoshihara@gmail.com>
2019-04-25 10:08:16 -0700

    Updated comment

--- contentcuration/contentcuration/static/js/edit_channel/publish/vuex/store.js
+++ contentcuration/contentcuration/static/js/edit_channel/publish/vuex/store.js
@@ -9,15 +9,29 @@ var store = new Vuex.Store({
       namespaced: true,
       state: {
         channel: null,
-        tempTaskID: null, // Depending on task logic, set using mutation
-        // on publishChannel action success callback
-        // or update getter logic to get task info
+
+        /* TODO: Update with actual task logic
+
+          I have created two different ways to handle this:
+          1. Store the task on this store, in which case you can
+            rename this variable to publishingTaskID or something.
+            This will be set under the mutation SET_TASK.
+            I'm not sure how vue-y this is though since it can be
+            assumed that the task will be available elsewhere on
+            the store, so that leads to...
+          2. Just use the taskID getter to return the correct task id,
+            effectively getting rid of this variable and the SET_TASK
+            mutation.
+
+          Since I don't have all the context on the task module, I'll
+          leave it up to you to determine the best course of action
+        */
+        tempTaskID: null,
       },
       actions: actions,
       mutations: mutations,
       getters: {
         taskID(state) {
-          // TODO: Replace with task logic
           return state.tempTaskID;
         },
       },
