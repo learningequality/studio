442ec1fc5243bc009c7f8e9b1eda21d3876c3b98
Micah Fitch <micah@learningequality.org>
2019-04-30 15:51:19 -0700

    check currently instantiated celery workers against expected set of workers

--- deploy/probers/worker_probe.py
+++ deploy/probers/worker_probe.py
@@ -5,19 +5,26 @@ import sys
 sys.path.insert(1, '../../contentcuration')
 from contentcuration import celery_app
 
+EXPECTED_WORKERS = {'celery'}
+
 class WorkerProbe(BaseProbe):
 
     metric = "worker_ping_latency_msec"
 
     def do_probe(self):
         workers = celery_app.control.inspect().ping()
-        # import pdb; pdb.set_trace()
+        workers_present = set()
+        get_worker_hostname = lambda worker: worker.split('@')[0]
         if not workers:
             raise Exception('No workers are running! Yikes!')
         else:
             for worker, info in workers.items():
                 if info[u'ok'] != 'pong':
-                    raise Exception('Worker %s responded with an unexpected status `%s`!  Yikes!' % (worker, info[u'ok']))
+                    raise Exception('Worker %s responded with an unexpected status `%s`!  Huh?' % (worker, info[u'ok']))
+                else:
+                    workers_present.add(get_worker_hostname(worker))
+            if workers_present != EXPECTED_WORKERS:
+                raise Exception('The set instantiated workers did not match what was expected!')
 
 if __name__ == "__main__":
     WorkerProbe().run()
