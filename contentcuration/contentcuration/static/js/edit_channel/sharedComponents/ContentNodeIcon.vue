5a13ba7bb535d132b713fd29d3e7faee6a70b373
Jordan Yoshihara <jordanyoshihara@gmail.com>
2019-05-07 16:03:59 -0700

    Added shared components

---
+++ contentcuration/contentcuration/static/js/edit_channel/sharedComponents/ContentNodeIcon.vue
@@ -0,0 +1,58 @@
+<template>
+  <VIcon>{{ icon }}</VIcon>
+</template>
+
+<script>
+
+  import _ from 'underscore';
+  import Constants from 'edit_channel/constants';
+
+  const kinds = _.pluck(Constants.ContentKinds, 'kind');
+
+  export default {
+    name: 'ContentNodeIcon',
+    props: {
+      kind: {
+        type: String,
+        required: true,
+        validator: value => {
+          return _.contains(kinds, value);
+        },
+      },
+    },
+    computed: {
+      icon() {
+        switch (this.kind) {
+          case 'topic':
+            return 'folder';
+          case 'video':
+            return 'theaters';
+          case 'audio':
+            return 'headset';
+          case 'image':
+            return 'image';
+          case 'exercise':
+            return 'star';
+          case 'document':
+            return 'description';
+          case 'html5':
+            return 'widgets';
+          default:
+            return 'error';
+        }
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="less" scoped>
+
+  @import '../../../less/global-variables.less';
+
+  .v-icon {
+    color: @gray-500;
+  }
+
+</style>
---
+++ contentcuration/contentcuration/static/js/edit_channel/sharedComponents/HelpTooltip.vue
@@ -0,0 +1,54 @@
+<template>
+  <VTooltip
+    :top="position === 'top'"
+    :bottom="position === 'bottom'"
+    :left="position === 'left'"
+    :right="position === 'right'"
+    maxWidth="150px"
+  >
+    <VIcon slot="activator" color="primary">
+      {{ icon }}
+    </VIcon>
+    <span>{{ text }}</span>
+  </VTooltip>
+</template>
+
+<script>
+
+  export default {
+    name: 'HelpTooltip',
+    props: {
+      text: {
+        type: String,
+        required: true,
+      },
+      icon: {
+        type: String,
+        default: 'help_outline',
+      },
+      position: {
+        type: String,
+        default: 'top',
+        validator: function(value) {
+          return _.contains(['top', 'bottom', 'left', 'right'], value);
+        },
+      },
+    },
+  };
+
+</script>
+
+<style lang="less">
+
+  @import '../../../less/global-variables.less';
+
+  .v-tooltip .v-icon {
+    cursor: pointer;
+  }
+
+  .v-tooltip__content {
+    font-family: @font-family;
+    text-align: center;
+  }
+
+</style>
