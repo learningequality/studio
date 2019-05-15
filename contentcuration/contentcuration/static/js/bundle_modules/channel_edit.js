13540efadeba7018f1d863541467fde645df8489
Jonathan Boiser <jboiser@gmail.com>
2019-05-15 15:46:34 -0700

    Separate channel_list into it’s own bundle

--- contentcuration/contentcuration/static/js/bundle_modules/channel_edit.js
+++ contentcuration/contentcuration/static/js/bundle_modules/channel_edit.js
@@ -1,60 +1,8 @@
 // include all logic in "base" entrypoint
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import ChannelEditIndex from './ChannelEditIndex.vue';
-import ChannelListPage from 'edit_channel/channel_list/views/ChannelListPage.vue';
-
-Vue.use(VueRouter);
-
+const Backbone = require('backbone');
 const WorkspaceManager = require('../edit_channel/utils/workspace_manager');
-const store = require('edit_channel/channel_list/vuex/store');
 require('./base');
 
-const router = new VueRouter({
-  routes: [
-    {
-      name: 'ChannelList',
-      path: '/',
-      component: ChannelListPage,
-      props: {
-        activeList: 'EDITABLE',
-      },
-    },
-    {
-      name: 'ChannelList/Starred',
-      path: '/starred',
-      component: ChannelListPage,
-      props: {
-        activeList: 'STARRED',
-      },
-    },
-    {
-      name: 'ChannelList/ViewOnly',
-      path: '/view_only',
-      component: ChannelListPage,
-      props: {
-        activeList: 'VIEW_ONLY',
-      },
-    },
-    {
-      name: 'ChannelList/Public',
-      path: '/public',
-      component: ChannelListPage,
-      props: {
-        activeList: 'PUBLIC',
-      },
-    },
-    {
-      name: 'ChannelList/Collections',
-      path: '/collections',
-      component: ChannelListPage,
-      props: {
-        activeList: 'CHANNEL_SETS',
-      },
-    },
-  ],
-});
-// var Backbone = require('backbone');
 
 const State = require('edit_channel/state');
 // Require the router object so that it is ready for Backbone history start call.
@@ -69,12 +17,10 @@ $(function() {
     State.current_channel.fetch({ async: false });
   }
   $('#channel-edit-content-wrapper').on('click', close_popups);
-
-  new Vue({
-    el: '#channel-container',
-    store,
-    router,
-    ...ChannelEditIndex,
+  Backbone.history.start({
+    pushState: true,
+    // set in Django template
+    root: window.url,
   });
 });
 
