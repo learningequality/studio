8bd691df718fac83951a00eed66e96f4fa70d3b2
Jordan Yoshihara <jordanyoshihara@gmail.com>
2019-05-20 16:37:53 -0700

    Exclude garbage nodes from query

--- contentcuration/contentcuration/views/base.py
+++ contentcuration/contentcuration/views/base.py
@@ -507,7 +507,8 @@ class SandboxView(TemplateView):
     def get_context_data(self, **kwargs):
         kwargs = super(SandboxView, self).get_context_data(**kwargs)
         kinds = ['topic', 'exercise', 'html5', 'document', 'video', 'audio']
-        nodes = list(ContentNode.objects.filter(kind_id=k).values('title', 'kind_id', 'pk').first() for k in kinds)
+        tree_ids = Channel.objects.filter(deleted=False).values_list('main_tree__tree_id', flat=True)
+        nodes = list(ContentNode.objects.filter(kind_id=k, tree_id__in=tree_ids).values('title', 'kind_id', 'pk').first() for k in kinds)
         nodes = [{'id': n['pk'], 'kind': n['kind_id'], 'title': n['title']} for n in nodes]
         kwargs.update({"nodes": json.dumps(nodes),
                        "channel": Channel.objects.first().pk})
