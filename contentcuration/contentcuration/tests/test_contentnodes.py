b9aea1fde1934503b4faf5601a42d128fcd16af3
kollivier <kevino@theolliviers.com>
2019-05-23 11:45:59 -0700

    Fix linting issues.

--- contentcuration/contentcuration/tests/test_contentnodes.py
+++ contentcuration/contentcuration/tests/test_contentnodes.py
@@ -35,7 +35,9 @@ def _check_nodes(parent, title=None, original_channel_id=None, source_channel_id
             assert node.title == title
         assert node.parent == parent
         if original_channel_id:
-            assert node.original_channel_id == original_channel_id, "Node {} with title {} has an incorrect original_channel_id.".format(node.pk, node.title)
+            assert node.original_channel_id == original_channel_id,\
+                "Node {} with title {} has an incorrect original_channel_id.".\
+                format(node.pk, node.title)
         if channel:
             assert node.get_channel() == channel
         if source_channel_id:
@@ -52,21 +54,28 @@ class NodeGettersTestCase(BaseTestCase):
         self.thumbnail_data = "allyourbase64arebelongtous"
 
     def test_get_node_thumbnail_default(self):
-        new_node = ContentNode.objects.create(title="Heyo!", parent=self.channel.main_tree, kind=self.topic)
+        new_node = ContentNode.objects.create(title="Heyo!",
+                                              parent=self.channel.main_tree,
+                                              kind=self.topic)
 
-        default_thumbnail = "/".join([settings.STATIC_URL.rstrip("/"), "img", "{}_placeholder.png".format(new_node.kind_id)])
+        default_thumbnail = "/".join([settings.STATIC_URL.rstrip("/"), "img",
+                                      "{}_placeholder.png".format(new_node.kind_id)])
         thumbnail = new_node.get_thumbnail()
         assert thumbnail == default_thumbnail
 
     def test_get_node_thumbnail_base64(self):
-        new_node = ContentNode.objects.create(title="Heyo!", parent=self.channel.main_tree, kind=self.topic)
+        new_node = ContentNode.objects.create(title="Heyo!",
+                                              parent=self.channel.main_tree,
+                                              kind=self.topic)
 
         new_node.thumbnail_encoding = '{"base64": "%s"}' % self.thumbnail_data
 
         assert new_node.get_thumbnail() == self.thumbnail_data
 
     def test_get_node_thumbnail_file(self):
-        new_node = ContentNode.objects.create(title="Heyo!", parent=self.channel.main_tree, kind=self.topic)
+        new_node = ContentNode.objects.create(title="Heyo!",
+                                              parent=self.channel.main_tree,
+                                              kind=self.topic)
         thumbnail_file = create_thumbnail_from_base64(testdata.base64encoding())
         thumbnail_file.contentnode = new_node
 
@@ -85,7 +94,6 @@ class NodeGettersTestCase(BaseTestCase):
         assert details['kind_count'] > 0
 
 
-
 class NodeOperationsTestCase(BaseTestCase):
 
     def setUp(self):
@@ -95,8 +103,8 @@ class NodeOperationsTestCase(BaseTestCase):
 
     def test_duplicate_nodes(self):
         """
-        Ensures that when we copy nodes, the new channel gets marked as changed but the old channel doesn't,
-        and that the nodes point to the new channel.
+        Ensures that when we copy nodes, the new channel gets marked as changed
+        but the old channel doesn't, and that the nodes point to the new channel.
         """
         num_nodes = 10
         title = "Dolly"
@@ -111,7 +119,8 @@ class NodeOperationsTestCase(BaseTestCase):
         assert self.channel.main_tree.get_channel() == self.channel
 
         assert self.channel.main_tree.parent is None
-        _check_nodes(self.channel.main_tree, title, original_channel_id=self.channel.id, source_channel_id=self.channel.id, channel=self.channel)
+        _check_nodes(self.channel.main_tree, title, original_channel_id=self.channel.id,
+                     source_channel_id=self.channel.id, channel=self.channel)
 
         new_channel = testdata.channel()
 
@@ -128,7 +137,8 @@ class NodeOperationsTestCase(BaseTestCase):
 
         new_tree = nodes.duplicate_node_bulk(self.channel.main_tree, parent=new_channel.main_tree)
 
-        _check_nodes(new_tree, title, original_channel_id=self.channel.id, source_channel_id=self.channel.id, channel=new_channel)
+        _check_nodes(new_tree, title, original_channel_id=self.channel.id,
+                     source_channel_id=self.channel.id, channel=new_channel)
         new_channel.main_tree.refresh_from_db()
         assert new_channel.main_tree.changed is True
 
@@ -143,17 +153,17 @@ class NodeOperationsTestCase(BaseTestCase):
         title = "Dolly"
         num_nodes = 10
         topic, _created = ContentKind.objects.get_or_create(kind="Topic")
-        new_node = ContentNode.objects.create(title="Heyo!", parent=self.channel.main_tree, kind=topic)
+        new_node = ContentNode.objects.create(title="Heyo!",
+                                              parent=self.channel.main_tree, kind=topic)
         self.channel.save()
-        # assert self.channel.main_tree.get_root() == self.channel.main_tree
-        # assert self.channel.main_tree.get_channel() == self.channel
         _create_nodes(num_nodes, title, parent=new_node)
 
         assert self.channel.main_tree.changed is True
         assert self.channel.main_tree.get_channel() == self.channel
 
         assert self.channel.main_tree.parent is None
-        _check_nodes(new_node, title, original_channel_id=self.channel.id, source_channel_id=self.channel.id, channel=self.channel)
+        _check_nodes(new_node, title, original_channel_id=self.channel.id,
+                     source_channel_id=self.channel.id, channel=self.channel)
 
         channels = [
             self.channel,
@@ -176,7 +186,8 @@ class NodeOperationsTestCase(BaseTestCase):
             prev_channel.main_tree.refresh_from_db()
             assert prev_channel.main_tree.changed is False
 
-            # simulate a clean, right-after-publish state to ensure only new channel is marked as change
+            # simulate a clean, right-after-publish state to ensure only new channel
+            # is marked as change
             channel.main_tree.changed = False
             channel.main_tree.save()
             channel.main_tree.refresh_from_db()
@@ -185,7 +196,8 @@ class NodeOperationsTestCase(BaseTestCase):
             # make sure we always copy the copy we made in the previous go around :)
             copy_node_root = nodes.duplicate_node_bulk(copy_node_root, parent=channel.main_tree)
 
-            _check_nodes(copy_node_root, original_channel_id=self.channel.id, source_channel_id=prev_channel.id, channel=channel)
+            _check_nodes(copy_node_root, original_channel_id=self.channel.id,
+                         source_channel_id=prev_channel.id, channel=channel)
             channel.main_tree.refresh_from_db()
             assert channel.main_tree.changed is True
             assert channel.main_tree.get_descendants().filter(changed=True).exists()
@@ -195,8 +207,9 @@ class NodeOperationsTestCase(BaseTestCase):
 
     def test_move_nodes(self):
         """
-        Ensures that moving nodes properly removes them from the original parent and adds them to the new one,
-        and marks the new and old parents as changed, and that the node channel info gets updated as well.
+        Ensures that moving nodes properly removes them from the original parent
+        and adds them to the new one, and marks the new and old parents as changed,
+        and that the node channel info gets updated as well.
         """
         title = "A Node on the Move"
         topic, _created = ContentKind.objects.get_or_create(kind="Topic")
@@ -208,7 +221,8 @@ class NodeOperationsTestCase(BaseTestCase):
         assert self.channel.main_tree.changed is True
         assert self.channel.main_tree.parent is None
 
-        _check_nodes(self.channel.main_tree, title, original_channel_id=self.channel.id, source_channel_id=self.channel.id, channel=self.channel)
+        _check_nodes(self.channel.main_tree, title, original_channel_id=self.channel.id,
+                     source_channel_id=self.channel.id, channel=self.channel)
 
         new_channel = testdata.channel()
         new_channel.editors.add(self.user)
@@ -222,7 +236,8 @@ class NodeOperationsTestCase(BaseTestCase):
 
         assert self.channel.main_tree.pk not in [node['id'] for node in nodes]
 
-        # simulate a clean, right-after-publish state for both trees to ensure they are marked changed after this
+        # simulate a clean, right-after-publish state for both trees to ensure they are marked
+        # changed after this
         self.channel.main_tree.changed = False
         self.channel.main_tree.save()
         new_channel.main_tree.changed = False
@@ -236,7 +251,8 @@ class NodeOperationsTestCase(BaseTestCase):
         new_channel.main_tree.refresh_from_db()
 
         # these can get out of sync if we don't do a rebuild
-        assert self.channel.main_tree.get_descendants().count() == self.channel.main_tree.get_descendant_count()
+        assert self.channel.main_tree.get_descendants().count() ==\
+            self.channel.main_tree.get_descendant_count()
 
         assert self.channel.main_tree != new_channel.main_tree
         assert self.channel.main_tree.changed is True
@@ -255,8 +271,10 @@ class NodeOperationsTestCase(BaseTestCase):
         assert not self.channel.main_tree.get_descendants().filter(changed=True).exists()
         assert new_channel.main_tree.get_descendants().filter(changed=True).exists()
 
-        # TODO: Should a newly created node that was moved still have the channel it was moved from as its origin/source
-        _check_nodes(new_channel.main_tree, title=title, original_channel_id=self.channel.id, source_channel_id=self.channel.id, channel=new_channel)
+        # TODO: Should a newly created node that was moved still have the channel it was moved
+        # from as its origin/source
+        _check_nodes(new_channel.main_tree, title=title, original_channel_id=self.channel.id,
+                     source_channel_id=self.channel.id, channel=new_channel)
 
 
 class NodeOperationsAPITestCase(BaseAPITestCase):
@@ -275,7 +293,8 @@ class NodeOperationsAPITestCase(BaseAPITestCase):
         assert self.channel.main_tree.changed is True
         assert self.channel.main_tree.parent is None
 
-        _check_nodes(self.channel.main_tree, title, original_channel_id=self.channel.id, source_channel_id=self.channel.id, channel=self.channel)
+        _check_nodes(self.channel.main_tree, title, original_channel_id=self.channel.id,
+                     source_channel_id=self.channel.id, channel=self.channel)
 
         # simulate a clean, right-after-publish state to ensure it is marked as change
         self.channel.main_tree.changed = False
@@ -295,15 +314,15 @@ class NodeOperationsAPITestCase(BaseAPITestCase):
         for node in self.channel.main_tree.get_children():
             delete_data['nodes'].append(node.pk)
 
-        request = self.create_post_request(reverse_lazy('delete_nodes'), data=json.dumps(delete_data), content_type='application/json')
+        request = self.create_post_request(reverse_lazy('delete_nodes'),
+                                           data=json.dumps(delete_data),
+                                           content_type='application/json')
         nodes.delete_nodes(request)
 
         self.channel.main_tree.refresh_from_db()
         assert self.channel.main_tree.get_descendants().count() == 0
         assert not self.channel.main_tree.get_descendants().filter(changed=True).exists()
         assert self.channel.main_tree.changed is True
-
-
 
 
 class SyncNodesOperationTestCase(BaseTestCase):
@@ -317,11 +336,11 @@ class SyncNodesOperationTestCase(BaseTestCase):
     def test_sync_after_no_changes(self):
         orig_video, cloned_video = self._setup_original_and_deriative_nodes()
         sync_node(cloned_video, self.new_channel.id,
-                         sync_attributes=True,
-                         sync_tags=True,
-                         sync_files=True,
-                         sync_assessment_items=True,
-                         sync_sort_order=True)
+                  sync_attributes=True,
+                  sync_tags=True,
+                  sync_files=True,
+                  sync_assessment_items=True,
+                  sync_sort_order=True)
         self._assert_same_files(orig_video, cloned_video)
 
     def test_sync_with_subs(self):
@@ -330,13 +349,12 @@ class SyncNodesOperationTestCase(BaseTestCase):
         self._add_subs_to_video_node(orig_video, 'es')
         self._add_subs_to_video_node(orig_video, 'en')
         sync_node(cloned_video, self.new_channel.id,
-                         sync_attributes=True,
-                         sync_tags=True,
-                         sync_files=True,
-                         sync_assessment_items=True,
-                         sync_sort_order=True)
+                  sync_attributes=True,
+                  sync_tags=True,
+                  sync_files=True,
+                  sync_assessment_items=True,
+                  sync_sort_order=True)
         self._assert_same_files(orig_video, cloned_video)
-
 
     def test_resync_after_more_subs_added(self):
         orig_video, cloned_video = self._setup_original_and_deriative_nodes()
@@ -344,21 +362,20 @@ class SyncNodesOperationTestCase(BaseTestCase):
         self._add_subs_to_video_node(orig_video, 'es')
         self._add_subs_to_video_node(orig_video, 'en')
         sync_node(cloned_video, self.new_channel.id,
-                         sync_attributes=True,
-                         sync_tags=True,
-                         sync_files=True,
-                         sync_assessment_items=True,
-                         sync_sort_order=True)
+                  sync_attributes=True,
+                  sync_tags=True,
+                  sync_files=True,
+                  sync_assessment_items=True,
+                  sync_sort_order=True)
         self._add_subs_to_video_node(orig_video, 'ar')
         self._add_subs_to_video_node(orig_video, 'zul')
         sync_node(cloned_video, self.new_channel.id,
-                         sync_attributes=True,
-                         sync_tags=True,
-                         sync_files=True,
-                         sync_assessment_items=True,
-                         sync_sort_order=True)
+                  sync_attributes=True,
+                  sync_tags=True,
+                  sync_files=True,
+                  sync_assessment_items=True,
+                  sync_sort_order=True)
         self._assert_same_files(orig_video, cloned_video)
-
 
     def _create_video_node(self, title, parent, withsubs=False):
         data = dict(
@@ -404,9 +421,9 @@ class SyncNodesOperationTestCase(BaseTestCase):
         self.new_channel.save()
         self.new_channel.main_tree = self._create_empty_tree()
         self.new_channel.main_tree.save()
-        new_tree = nodes.duplicate_node_bulk(self.channel.main_tree, parent=self.new_channel.main_tree)
+        new_tree = nodes.duplicate_node_bulk(self.channel.main_tree,
+                                             parent=self.new_channel.main_tree)
         self.new_channel.main_tree = new_tree
-        # self.new_channel.main_tree.save()   #  InvalidMove: A node may not be made a child of any of its descendants.
         self.new_channel.main_tree.refresh_from_db()
 
         # Return video nodes we need for this test
