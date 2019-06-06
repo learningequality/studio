11e185860f52757c013f64221927f0b605805cd1
Michaela Robosova <robosova.michaela@seznam.cz>
2019-06-06 12:02:25 +0200

    Disable html hint class value check
    Vuetify helper classes contain double dashes,
    e.g. grey--text.

--- .htmlhintrc.js
+++ .htmlhintrc.js
@@ -1,3 +1,4 @@
 const htmlHintConfig = require('kolibri-tools/.htmlhintrc');
+htmlHintConfig['id-class-value'] = false;
 htmlHintConfig['--vue-component-conventions'] = false;
 module.exports = htmlHintConfig;
