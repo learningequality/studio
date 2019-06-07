0fa4695d7ecc9ececa2dd5df671c10fbf8b565a1
Michaela Robosova <robosova.michaela@seznam.cz>
2019-06-07 10:39:19 +0200

    Install Vuetify globally for Jest tests

--- jest_config/setup.js
+++ jest_config/setup.js
@@ -1,9 +1,17 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+Vue.use(Vuetify);
+
 const csrf = global.document.createElement('input');
 csrf.name = 'csrfmiddlewaretoken';
 csrf.value = 'csrfmiddlewaretoken';
 global.document.body.append(csrf);
-global.window.Urls = new Proxy({}, {
-  get(obj, prop) {
-    return () => prop;
+global.window.Urls = new Proxy(
+  {},
+  {
+    get(obj, prop) {
+      return () => prop;
+    },
   }
-});
+);
