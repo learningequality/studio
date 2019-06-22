43f90398e6bc58fa424f07e757a164a2d62f6980
Jordan Yoshihara <jordanyoshihara@gmail.com>
2019-06-21 17:48:07 -0700

    Use public channel for testing

--- contentcuration/contentcuration/views/base.py
+++ contentcuration/contentcuration/views/base.py
@@ -512,7 +512,7 @@ class SandboxView(TemplateView):
         nodes = list(ContentNode.objects.filter(kind_id=k, tree_id__in=tree_ids).exclude(parent=None).first() for k in kinds)
         nodes = ContentNodeSerializer(nodes, many=True)
         kwargs.update({"nodes": JSONRenderer().render(nodes.data),
-                       "channel": self.request.user.editable_channels.filter(deleted=False).first().pk,
+                       "channel": Channel.objects.filter(deleted=False, public=True).first().pk,
                        "current_user": JSONRenderer().render(CurrentUserSerializer(self.request.user).data)
                        })
         return kwargs
