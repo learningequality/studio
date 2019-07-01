2a223f2643b7d2dfa82d4fdc711ca1b52aebb33c
Michaela Robosova <robosova.michaela@seznam.cz>
2019-07-01 10:48:57 +0200

    Update toolbar for new use cases

--- contentcuration/contentcuration/static/js/edit_channel/uploader/components/AssessmentItemToolbar/AssessmentItemToolbar.spec.js
+++ contentcuration/contentcuration/static/js/edit_channel/uploader/components/AssessmentItemToolbar/AssessmentItemToolbar.spec.js
@@ -20,17 +20,48 @@ describe('AssessmentItemToolbar', () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  it("doesn't render edit icon if displayEdit is false", () => {
+  it('uses a correct item label in menu if specified', () => {
     wrapper = mount(AssessmentItemToolbar, {
       propsData: {
-        displayEdit: false,
+        itemLabel: 'question',
+      },
+    });
+
+    expect(wrapper.find('[data-test="toolbarMenu"]').html()).toContain('Add question above');
+    expect(wrapper.find('[data-test="toolbarMenu"]').html()).toContain('Add question below');
+  });
+
+  it("doesn't render edit icon if displayEditIcon is false", () => {
+    wrapper = mount(AssessmentItemToolbar, {
+      propsData: {
+        displayEditIcon: false,
       },
     });
 
     expect(wrapper.html()).not.toContain('edit');
   });
 
-  it('emits click action with correct payload on arrow up click', () => {
+  it("doesn't render delete icon if displayDeleteIcon is false", () => {
+    wrapper = mount(AssessmentItemToolbar, {
+      propsData: {
+        displayDeleteIcon: false,
+      },
+    });
+
+    expect(wrapper.html()).not.toContain('delete');
+  });
+
+  it("doesn't render menu if displayMenu is false", () => {
+    wrapper = mount(AssessmentItemToolbar, {
+      propsData: {
+        displayMenu: false,
+      },
+    });
+
+    expect(wrapper.contains('[data-test="toolbarMenu"]')).toBe(false);
+  });
+
+  it('emits click action with correct payload on arrow up icon click', () => {
     wrapper.find('[data-test=toolbarIconArrowUp]').trigger('click');
 
     expect(wrapper.emitted().click).toBeTruthy();
@@ -38,7 +69,7 @@ describe('AssessmentItemToolbar', () => {
     expect(wrapper.emitted().click[0][0]).toEqual(AssessmentItemToolbarActions.MOVE_ITEM_UP);
   });
 
-  it('emits click action with correct payload on arrow down click', () => {
+  it('emits click action with correct payload on arrow down icon click', () => {
     wrapper.find('[data-test=toolbarIconArrowDown]').trigger('click');
 
     expect(wrapper.emitted().click).toBeTruthy();
@@ -46,7 +77,7 @@ describe('AssessmentItemToolbar', () => {
     expect(wrapper.emitted().click[0][0]).toEqual(AssessmentItemToolbarActions.MOVE_ITEM_DOWN);
   });
 
-  it('emits click action with correct payload on edit click', () => {
+  it('emits click action with correct payload on edit icon click', () => {
     wrapper.find('[data-test=toolbarIconEdit]').trigger('click');
 
     expect(wrapper.emitted().click).toBeTruthy();
@@ -54,7 +85,15 @@ describe('AssessmentItemToolbar', () => {
     expect(wrapper.emitted().click[0][0]).toEqual(AssessmentItemToolbarActions.EDIT_ITEM);
   });
 
-  it('emits click action with correct payload on "Add question above" click', () => {
+  it('emits click action with correct payload on delete icon click', () => {
+    wrapper.find('[data-test=toolbarIconDelete]').trigger('click');
+
+    expect(wrapper.emitted().click).toBeTruthy();
+    expect(wrapper.emitted().click.length).toBe(1);
+    expect(wrapper.emitted().click[0][0]).toEqual(AssessmentItemToolbarActions.DELETE_ITEM);
+  });
+
+  it('emits click action with correct payload on menu "Add item above" click', () => {
     wrapper.find('[data-test=toolbarMenuAddItemAbove]').trigger('click');
 
     expect(wrapper.emitted().click).toBeTruthy();
@@ -62,7 +101,7 @@ describe('AssessmentItemToolbar', () => {
     expect(wrapper.emitted().click[0][0]).toEqual(AssessmentItemToolbarActions.ADD_ITEM_ABOVE);
   });
 
-  it('emits click action with correct payload on "Add question below" click', () => {
+  it('emits click action with correct payload on menu "Add item below" click', () => {
     wrapper.find('[data-test=toolbarMenuAddItemBelow]').trigger('click');
 
     expect(wrapper.emitted().click).toBeTruthy();
@@ -70,7 +109,7 @@ describe('AssessmentItemToolbar', () => {
     expect(wrapper.emitted().click[0][0]).toEqual(AssessmentItemToolbarActions.ADD_ITEM_BELOW);
   });
 
-  it('emits click action with correct payload on "Delete" click', () => {
+  it('emits click action with correct payload on menu "Delete" click', () => {
     wrapper.find('[data-test=toolbarMenuDeleteItem]').trigger('click');
 
     expect(wrapper.emitted().click).toBeTruthy();
