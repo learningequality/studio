29fbf1f98ac1d21a6e9dcd3a824ef2d964a9d730
Jordan Yoshihara <jordanyoshihara@gmail.com>
2019-07-02 14:56:54 -0700

    Added section headers

--- contentcuration/contentcuration/static/js/edit_channel/uploader/views/DetailsTabView.vue
+++ contentcuration/contentcuration/static/js/edit_channel/uploader/views/DetailsTabView.vue
@@ -1,28 +1,10 @@
 <template>
   <div class="details-edit-view">
     <VForm ref="form" v-model="valid" :lazyValidation="newContent" :disabled="viewOnly">
-      <!-- Language and import link -->
-      <VLayout grid alignTop class="language-section" wrap>
-        <VFlex v-if="oneSelected && isImported" lg4 md7 sm12>
-          <VBtn color="primary" flat class="import-link" :href="importUrl" target="_blank">
-            {{ $tr('importedFromButtonText', {channel: 'Sample Channel'}) }}
-          </VBtn>
-        </VFlex>
-        <VSpacer />
-        <VFlex lg4 md5 sm12>
-          <LanguageDropdown
-            :hint="languageHint"
-            :language="changes.language.value"
-            :placeholder="getPlaceholder('language')"
-            :readonly="viewOnly"
-            @changed="setLanguage"
-          />
-        </VFlex>
-
+      <VLayout grid wrap>
         <!-- Title -->
-        <VFlex xs12>
+        <VFlex v-if="oneSelected" xs12>
           <VTextField
-            v-if="oneSelected"
             :value="changes.title.value"
             :counter="(viewOnly)? null : 200"
             maxlength="200"
@@ -31,180 +13,228 @@
             autofocus
             required
             :readonly="viewOnly"
+            class="title-input"
             @change="setTitle"
           />
         </VFlex>
 
-        <!-- Visibility -->
-        <VFlex sm12 md4>
-          <VisibilityDropdown
-            :role="changes.role_visibility.value"
-            :placeholder="getPlaceholder('role_visibility')"
-            :required="!changes.role_visibility.varied"
+        <!-- Description -->
+        <VFlex v-if="oneSelected" xs12>
+          <VTextarea
+            :value="changes.description.value"
+            :label="$tr('descriptionLabel')"
+            :counter="!viewOnly && 400"
+            autoGrow
             :readonly="viewOnly"
-            @changed="setVisibility"
+            @change="setDescription"
           />
         </VFlex>
-        <VSpacer />
 
-        <!-- Mastery -->
-        <VFlex sm12 md6>
-          <MasteryDropdown
-            v-if="allExercises"
-            :masteryModel="changes.extra_fields.mastery_model.value"
-            :placeholder="getExtraFieldPlaceholder('mastery_model')"
-            :required="!changes.extra_fields.mastery_model.varied"
-            :mValue="changes.extra_fields.m.value"
-            :mPlaceholder="getExtraFieldPlaceholder('m')"
-            :mRequired="!changes.extra_fields.m.varied"
-            :nValue="changes.extra_fields.n.value"
-            :nPlaceholder="getExtraFieldPlaceholder('n')"
-            :nRequired="!changes.extra_fields.n.varied"
+        <!-- Tags -->
+        <VFlex xs12>
+          <VCombobox
+            ref="tagbox"
+            :value="changes.tags"
+            :items="tags"
+            :searchInput.sync="tagText"
             :readonly="viewOnly"
-            @changed="setExtraFields"
-          />
-          <v-checkbox
-            v-if="allExercises"
-            :label="$tr('randomizeQuestionLabel')"
-            :inputValue="changes.extra_fields.randomize.value"
-            :indeterminate="changes.extra_fields.randomize.varied"
-            color="primary"
-            :readonly="viewOnly"
-            @change="setQuestionOrderRandomization"
-          />
+            chips
+            :label="$tr('tagsLabel')"
+            multiple
+            deletableChips
+            hideSelected
+            maxlength="30"
+            autoSelectFirst
+            @change="handleTags"
+          >
+            <template v-slot:no-data>
+              <VListTile v-if="tagText && tagText.trim()">
+                <VListTileContent>
+                  <VListTileTitle>
+                    {{ $tr('noTagsFoundText', {text: tagText.trim()}) }}
+                  </VListTileTitle>
+                </VListTileContent>
+              </VListTile>
+            </template>
+          </VCombobox>
         </VFlex>
       </VLayout>
 
-      <!-- Authoring Section -->
-      <div v-if="allResources">
-        <VDivider />
-        <VLayout row class="auth-section align-top" wrap>
-          <VFlex v-if="disableAuthEdits" xs12 class="imports-detected-text">
-            {{ $tr('detectedImportText') }}
-          </VFlex>
-
-          <!-- Author -->
-          <VFlex md4 sm12 class="field-with-tooltip">
-            <VCombobox
-              :value="changes.author.value"
-              :items="authors"
-              :label="$tr('authorLabel')"
-              :readonly="viewOnly || disableAuthEdits"
-              maxlength="200"
-              autoSelectFirst
-              :placeholder="getPlaceholder('author')"
-              @change="setAuthor"
-            />
-            <HelpTooltip :text="$tr('authorToolTip')" />
-          </VFlex>
-
-          <!-- Provider -->
-          <VFlex md4 sm12 class="field-with-tooltip">
-            <VCombobox
-              :value="changes.provider.value"
-              :items="providers"
-              :label="$tr('providerLabel')"
-              :readonly="viewOnly || disableAuthEdits"
-              maxlength="200"
-              :placeholder="getPlaceholder('provider')"
-              autoSelectFirst
-              @change="setProvider"
-            />
-            <HelpTooltip :text="$tr('providerToolTip')" />
-          </VFlex>
-
-          <!-- Aggregator -->
-          <VFlex md4 sm12 class="field-with-tooltip">
-            <VCombobox
-              :value="changes.aggregator.value"
-              :items="aggregators"
-              :label="$tr('aggregatorLabel')"
-              :readonly="viewOnly || disableAuthEdits"
-              maxlength="200"
-              autoSelectFirst
-              :placeholder="getPlaceholder('aggregator')"
-              @change="setAggregator"
-            />
-            <HelpTooltip :text="$tr('aggregatorToolTip')" />
-          </VFlex>
-
-          <!-- License -->
-          <VFlex md5 sm12>
-            <LicenseDropdown
-              :selectedID="changes.license.value"
-              :licenseDescription="changes.license_description.value"
-              :required="!changes.license.varied"
-              :readonly="viewOnly || disableAuthEdits"
-              :descriptionRequired="!changes.license_description.varied"
-              :placeholder="getPlaceholder('license')"
-              :descriptionPlaceholder="getPlaceholder('license_description')"
-              @licensechanged="setLicense"
-              @descriptionchanged="setLicenseDescription"
-            />
-          </VFlex>
-          <VSpacer />
-
-          <!-- Copyright Holder -->
-          <VFlex md5 sm12>
-            <VCombobox
-              v-if="copyrightHolderRequired"
-              :value="changes.copyright_holder.value"
-              :items="copyrightHolders"
-              :label="$tr('copyrightHolderLabel')"
-              maxlength="200"
-              :required="!changes.copyright_holder.varied"
-              :rules="rules.copyrightHolder"
-              :placeholder="getPlaceholder('copyright_holder')"
-              autoSelectFirst
-              :readonly="viewOnly || disableAuthEdits"
-              @input="setCopyrightHolder"
-            />
-          </VFlex>
-        </VLayout>
-        <VDivider />
-      </div>
-
-      <!-- Description -->
-      <VFlex xs12>
-        <VTextarea
-          :value="changes.description.value"
-          :placeholder="getPlaceholder('description')"
-          :label="$tr('descriptionLabel')"
-          :counter="!viewOnly && 400"
-          autoGrow
-          :readonly="viewOnly"
-          @change="setDescription"
-        />
-      </VFlex>
-
-      <!-- Tags -->
-      <VFlex xs12>
-        <VCombobox
-          ref="tagbox"
-          :value="changes.tags"
-          :items="tags"
-          :searchInput.sync="tagText"
-          :readonly="viewOnly"
-          chips
-          :label="$tr('tagsLabel')"
-          multiple
-          deletableChips
-          hideSelected
-          maxlength="30"
-          autoSelectFirst
-          @change="handleTags"
-        >
-          <template v-slot:no-data>
-            <VListTile v-if="tagText && tagText.trim()">
-              <VListTileContent>
-                <VListTileTitle>
-                  {{ $tr('noTagsFoundText', {text: tagText.trim()}) }}
-                </VListTileTitle>
-              </VListTileContent>
-            </VListTile>
+      <v-expansion-panel v-model="panel" expand>
+        <v-expansion-panel-content key="audience">
+          <template v-slot:header>
+            <div>
+              <v-icon>people</v-icon> {{ $tr('audienceHeader') }}
+            </div>
           </template>
-        </VCombobox>
-      </VFlex>
+          <v-card>
+            <v-layout row wrap>
+              <!-- Language -->
+              <VFlex lg4 md5 sm12>
+                <LanguageDropdown
+                  :hint="languageHint"
+                  :language="changes.language.value"
+                  :placeholder="getPlaceholder('language')"
+                  :readonly="viewOnly"
+                  @changed="setLanguage"
+                />
+              </VFlex>
+              <VSpacer />
+
+              <!-- Visibility -->
+              <VFlex lg4 md5 sm12>
+                <VisibilityDropdown
+                  :role="changes.role_visibility.value"
+                  :placeholder="getPlaceholder('role_visibility')"
+                  :required="!changes.role_visibility.varied"
+                  :readonly="viewOnly"
+                  @changed="setVisibility"
+                />
+              </VFlex>
+            </v-layout>
+          </v-card>
+        </v-expansion-panel-content>
+        <v-expansion-panel-content key="assessments">
+          <template v-slot:header>
+            <div>
+              <v-icon>star</v-icon> {{ $tr('assessmentHeader') }}
+            </div>
+          </template>
+          <v-card>
+            <v-layout row wrap>
+              <!-- Mastery -->
+              <VFlex sm12 md5>
+                <MasteryDropdown
+                  v-if="allExercises"
+                  :masteryModel="changes.extra_fields.mastery_model.value"
+                  :placeholder="getExtraFieldPlaceholder('mastery_model')"
+                  :required="!changes.extra_fields.mastery_model.varied"
+                  :mValue="changes.extra_fields.m.value"
+                  :mPlaceholder="getExtraFieldPlaceholder('m')"
+                  :mRequired="!changes.extra_fields.m.varied"
+                  :nValue="changes.extra_fields.n.value"
+                  :nPlaceholder="getExtraFieldPlaceholder('n')"
+                  :nRequired="!changes.extra_fields.n.varied"
+                  :readonly="viewOnly"
+                  @changed="setExtraFields"
+                />
+              </VFlex>
+              <v-spacer />
+              <VFlex sm12 md5>
+                <v-checkbox
+                  v-if="allExercises"
+                  :label="$tr('randomizeQuestionLabel')"
+                  :inputValue="changes.extra_fields.randomize.value"
+                  :indeterminate="changes.extra_fields.randomize.varied"
+                  color="primary"
+                  :readonly="viewOnly"
+                  @change="setQuestionOrderRandomization"
+                />
+              </VFlex>
+            </v-layout>
+          </v-card>
+        </v-expansion-panel-content>
+        <v-expansion-panel-content v-if="allResources" key="source">
+          <template v-slot:header>
+            <div>
+              <v-icon>copyright</v-icon>
+              {{ $tr('sourceHeader') }}
+              <span v-if="disableAuthEdits">
+                {{ $tr('detectedImportText') }}
+              </span>
+            </div>
+          </template>
+          <v-card>
+            <v-layout row wrap class="auth-section">
+              <VFlex v-if="oneSelected && isImported" xs12>
+                <VBtn color="primary" flat class="import-link" :href="importUrl" target="_blank">
+                  {{ $tr('importedFromButtonText', {channel: 'Sample Channel'}) }}
+                </VBtn>
+              </VFlex>
+
+              <!-- Author -->
+              <VFlex md4 sm12 class="field-with-tooltip">
+                <VCombobox
+                  :value="changes.author.value"
+                  :items="authors"
+                  :label="$tr('authorLabel')"
+                  :readonly="viewOnly || disableAuthEdits"
+                  maxlength="200"
+                  autoSelectFirst
+                  :placeholder="getPlaceholder('author')"
+                  @change="setAuthor"
+                />
+                <HelpTooltip :text="$tr('authorToolTip')" />
+              </VFlex>
+              <VSpacer />
+
+              <!-- Provider -->
+              <VFlex md4 sm12 class="field-with-tooltip">
+                <VCombobox
+                  :value="changes.provider.value"
+                  :items="providers"
+                  :label="$tr('providerLabel')"
+                  :readonly="viewOnly || disableAuthEdits"
+                  maxlength="200"
+                  :placeholder="getPlaceholder('provider')"
+                  autoSelectFirst
+                  @change="setProvider"
+                />
+                <HelpTooltip :text="$tr('providerToolTip')" />
+              </VFlex>
+              <VSpacer />
+
+              <!-- Aggregator -->
+              <VFlex md4 sm12 class="field-with-tooltip">
+                <VCombobox
+                  :value="changes.aggregator.value"
+                  :items="aggregators"
+                  :label="$tr('aggregatorLabel')"
+                  :readonly="viewOnly || disableAuthEdits"
+                  maxlength="200"
+                  autoSelectFirst
+                  :placeholder="getPlaceholder('aggregator')"
+                  @change="setAggregator"
+                />
+                <HelpTooltip :text="$tr('aggregatorToolTip')" />
+              </VFlex>
+
+              <!-- License -->
+              <VFlex md5 sm12>
+                <LicenseDropdown
+                  :selectedID="changes.license.value"
+                  :licenseDescription="changes.license_description.value"
+                  :required="!changes.license.varied"
+                  :readonly="viewOnly || disableAuthEdits"
+                  :descriptionRequired="!changes.license_description.varied"
+                  :placeholder="getPlaceholder('license')"
+                  :descriptionPlaceholder="getPlaceholder('license_description')"
+                  @licensechanged="setLicense"
+                  @descriptionchanged="setLicenseDescription"
+                />
+              </VFlex>
+              <VSpacer />
+
+              <!-- Copyright Holder -->
+              <VFlex md5 sm12>
+                <VCombobox
+                  v-if="copyrightHolderRequired"
+                  :value="changes.copyright_holder.value"
+                  :items="copyrightHolders"
+                  :label="$tr('copyrightHolderLabel')"
+                  maxlength="200"
+                  :required="!changes.copyright_holder.varied"
+                  :rules="rules.copyrightHolder"
+                  :placeholder="getPlaceholder('copyright_holder')"
+                  autoSelectFirst
+                  :readonly="viewOnly || disableAuthEdits"
+                  @input="setCopyrightHolder"
+                />
+              </VFlex>
+            </v-layout>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
     </VForm>
   </div>
 </template>
@@ -228,7 +258,7 @@
       languageHelpText: 'Leave blank to default to topic language',
       languageChannelHelpText: 'Leave blank to default to channel language',
       importedFromButtonText: 'Imported from {channel}',
-      detectedImportText: 'READ-ONLY: content has been imported with view-only permission',
+      detectedImportText: '[Read-only: content has been imported with view-only permission]',
       authorLabel: 'Author',
       authorToolTip: 'Person or organization who created this content',
       providerLabel: 'Provider',
@@ -243,6 +273,9 @@
       variedFieldPlaceholder: '---',
       noTagsFoundText: 'No results matching "{text}". Press \'enter\'to create a new tag',
       randomizeQuestionLabel: 'Randomize question order for learners',
+      audienceHeader: 'Audience',
+      assessmentHeader: 'Assessment Options',
+      sourceHeader: 'Source',
     },
     components: {
       LanguageDropdown,
@@ -261,6 +294,7 @@
       return {
         tagText: null,
         valid: true,
+        panel: ['audience', 'assessments', 'source'],
         rules: {
           title: [v => !!v || this.$tr('titleValidationMessage')],
           copyrightHolder: [
@@ -394,6 +428,65 @@
     padding: 30px;
     margin: 0 auto;
 
+    .v-expansion-panel {
+      margin-top: 50px;
+      box-shadow: none !important;
+      /deep/ .v-expansion-panel__container {
+        border: 0;
+        /deep/ .v-expansion-panel__header {
+          min-height: 35px;
+          padding: 3px 15px;
+          font-size: 11pt;
+          font-weight: bold;
+          color: white;
+          /deep/ .v-icon,
+          .v-icon {
+            margin-right: 10px;
+            font-size: 14pt;
+            color: white !important;
+            vertical-align: bottom;
+          }
+        }
+        /deep/ .v-expansion-panel__body {
+          padding: 15px 10px 50px;
+          .layout {
+            width: 100%;
+            margin-left: 0;
+            &.auth-section {
+              .v-input {
+                margin-bottom: 20px;
+              }
+              .v-autocomplete {
+                /deep/ .v-input__append-inner {
+                  visibility: hidden;
+                }
+              }
+              .field-with-tooltip {
+                .v-autocomplete {
+                  display: inline-block;
+                  width: calc(100% - 70px);
+                }
+                /deep/ .v-icon {
+                  margin-left: 5px;
+                  font-size: 14pt;
+                }
+              }
+            }
+          }
+        }
+
+        &:nth-child(1) .v-expansion-panel__header {
+          background-color: @video-color;
+        }
+        &:nth-child(2) .v-expansion-panel__header {
+          background-color: @exercise-color;
+        }
+        &:nth-child(3) .v-expansion-panel__header {
+          background-color: @html-color;
+        }
+      }
+    }
+
     .import-link {
       .action-text;
 
@@ -409,9 +502,6 @@
       margin-top: 30px;
       .flex {
         text-align: center;
-      }
-      .v-input {
-        margin-bottom: 20px;
       }
       /deep/ .v-input--is-readonly {
         /deep/ label {
@@ -430,34 +520,6 @@
         }
         /deep/ .v-chip {
           color: black;
-        }
-      }
-
-      .auth-section {
-        width: 100%;
-        margin: 0 auto;
-
-        .imports-detected-text {
-          padding: 5px;
-          margin-bottom: 15px;
-          font-weight: bold;
-          color: @gray-700;
-          background-color: @gray-200;
-        }
-        .v-autocomplete {
-          /deep/ .v-input__append-inner {
-            visibility: hidden;
-          }
-        }
-        .field-with-tooltip {
-          .v-autocomplete {
-            display: inline-block;
-            width: calc(100% - 75px);
-          }
-          /deep/ .v-icon {
-            margin-left: 5px;
-            font-size: 14pt;
-          }
         }
       }
     }
