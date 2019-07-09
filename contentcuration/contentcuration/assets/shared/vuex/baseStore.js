d3c34efd76e7ef6ed7e16ec6db6317013d16240d
Richard Tibbles <richard@learningequality.org>
2019-07-08 17:07:56 -0700

    Add shared base state. Add simple client wrapper.

---
+++ contentcuration/contentcuration/assets/shared/vuex/baseStore.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import session from './session';
+
+Vue.use(Vuex);
+
+export default function storeFactory({state, actions, getters, mutations, modules, plugins} = {
+  state: {},
+  actions: {},
+  getters: {},
+  mutations: {},
+  modules: {},
+  plugins: [],
+}) {
+  return new Vuex.Store({
+    state,
+    actions,
+    getters,
+    mutations,
+    plugins,
+    modules: {
+      session,
+      ...modules,
+    },
+  });
+}
---
+++ contentcuration/contentcuration/assets/shared/vuex/session/index.js
@@ -0,0 +1,26 @@
+import Constants from 'edit_channel/constants/index';
+
+export default {
+  state: {
+    currentUser: {
+      first_name: 'Guest',
+      ...window.user,
+    },
+    preferences: window.user_preferences === 'string' ? JSON.parse(window.user_preferences) : window.user_preferences
+  },
+  currentLanguage: Constants.Languages.find(
+    l => l.id && l.id.toLowerCase() === (window.languageCode || 'en')
+  ),
+  mutations: {
+    SET_CURRENT_USER(state, currentUser) {
+      state.currentUser = {
+        ...currentUser,
+      };
+    },
+  },
+  getters: {
+    currentUserId(state) {
+      return state.currentUser.id
+    },
+  },
+}
