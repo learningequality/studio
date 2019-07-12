e37ecba27572d397c38a6637991c2d5193d33a11
Lingyi Wang <lywang07@gmail.com>
2019-07-12 10:09:31 -0700

    use set-string flag for the int to avoid it being converted to float64

--- k8s/helm-deploy.sh
+++ k8s/helm-deploy.sh
@@ -42,4 +42,4 @@ helm upgrade --install $RELEASENAME . \
      --set studioApp.gDrive.keyJson=$(base64 $GDRIVE_SERVICE_ACCOUNT_JSON  --wrap=0) \
      --set sentry.dsnKey=$(echo "$SENTRY_DSN_KEY" | base64 --wrap=0) \
      --set studioProber.newrelicKey=$PROBER_NEWRELIC_KEY \
-     --set studioProber.newrelicAccountId=$PROBER_NEWRELIC_ACCOUNT_ID
+     --set-string studioProber.newrelicAccountId=$PROBER_NEWRELIC_ACCOUNT_ID  # use set-string to resolve the issue https://github.com/helm/helm/issues/1707
