485ca416e50cfb4db98bdf63c47449ff06a0651a
Richard Tibbles <richard@learningequality.org>
2019-10-10 22:23:07 -0700

    Setup base app initialization for common app logic.

---
+++ contentcuration/contentcuration/frontend/shared/app.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Vuetify from 'vuetify';
+import { theme, icons } from 'shared/vuetify';
+
+import 'shared/i18n/setup';
+
+import 'vuetify/dist/vuetify.min.css';
+import 'shared/styles/main.less';
+
+Vue.use(VueRouter);
+Vue.use(Vuetify, {
+  rtl: window.isRTL,
+  theme: theme(),
+  icons: icons(),
+});
+
+export let rootVue;
+
+export default function startApp({
+  store,
+  router,
+  index,
+}) {
+  rootVue = new Vue({
+    el: 'app',
+    store,
+    router,
+    ...index,
+  });
+}
