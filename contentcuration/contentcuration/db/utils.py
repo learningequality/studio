9e1339a6cf0db397b022a3724362f84dc95f3af8
Blaine Jester <blainesworld@gmail.com>
2019-10-11 17:04:20 -0700

    Add example to temporary model function

--- contentcuration/contentcuration/db/utils.py
+++ contentcuration/contentcuration/db/utils.py
@@ -14,6 +14,43 @@ def temporary_model(model, using=DEFAULT_DB_ALIAS, atomic=True):
     Creates a temporary database table, using a temporary model class, such that the implementer
     is able to use Django ORM tooling to interact with the short-lived instance of the table.
 
+    An example:
+
+        class TempThing(TemporaryModel):
+            id = AutoField(primary_key=True)
+            thing_id = IntegerField()
+            thing_name = CharField()
+            objects = CustomManager()
+
+
+        with temporary_model(TempThing):
+            # TempThing table now exists
+            TempThing.objects.bulk_create([
+                TempThing(thing_id=thing.get('id', None), thing_name=thing_name['name'])
+                for thing in unsettled_things
+            ])
+
+            # Example: cross reference large list, get new
+            existing = RealThing.objects.filter(name=OuterRef('thing_name'))
+            new_things = TempThing.objects.exclude_by(Exists(existing))
+
+            # Example: cross reference large list, get matching
+            existing = RealThing.objects.filter(name=OuterRef('thing_name'))
+            old_things = TempThing.objects.filter_by(Exists(existing))
+
+            # Example: insert into another
+            existing = RealThing.objects.filter(name=OuterRef('thing_name'))
+            new_things = TempThing.objects.exclude_by(Exists(existing))
+            RealThing.objects.create_from(new_things, name='thing_name')
+
+            # Example: update another table
+            update_from = TempThing.objects.filter(thing_id__isnull=False)
+            RealThing.objects.update_from(Join(update_from, id='thing_id'), name='thing_name')
+
+        # outside of `temporary_model` context, table no longer exists
+        # end of example
+
+
     :param model: The temporary model used to create the TEMP TABLE
     :type model: contentcuration.db.models.base.TemporaryModel
     :param using: The database connection alias
