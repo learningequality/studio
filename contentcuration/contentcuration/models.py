cf3528c4c67ff3fa13f25b1d391ebd1cc191937c
kollivier <kevino@theolliviers.com>
2019-11-15 10:31:25 -0800

    Remove unneeded field.

--- contentcuration/contentcuration/models.py
+++ contentcuration/contentcuration/models.py
@@ -374,11 +374,12 @@ class MPTTTreeIDManager(models.Model):
     Because MPTT uses plain integers for tree IDs and does not use an auto-incrementing field for them,
     the same ID can sometimes be assigned to two trees if two channel create ops happen concurrently.
 
+    As we are using this table only for the ID generation, it does not need any fields.
+
     We resolve this by creating a dummy table and using its ID as the tree index to take advantage of the db's
     concurrency-friendly way of generating sequential integer IDs. There is a custom migration that ensures
     that the number of records (and thus id) matches the max tree ID number when this table gets added.
     """
-    current = models.BooleanField(default=False)
 
 
 def file_on_disk_name(instance, filename):
