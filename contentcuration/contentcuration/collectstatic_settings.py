818bfd0ce7a4c441a0e55e589d6217c1a0da69d1
Aron Fyodor Asor <191955+aronasorman@users.noreply.github.com>
2020-01-29 18:31:56 +0000

    remove django_prometheus from collectstatic settings

--- contentcuration/contentcuration/collectstatic_settings.py
+++ contentcuration/contentcuration/collectstatic_settings.py
@@ -4,3 +4,8 @@
 from .settings import *
 
 CACHES['default']['BACKEND'] = "django_prometheus.cache.backends.locmem.LocMemCache"
+
+DATABASES["default"]["ENGINE"] = "django.db.backends.postgresql_psycopg2"
+
+# Remove django_prometeheus from INSTALLED_APPS
+INSTALLED_APPS = tuple(app for app in INSTALLED_APPS if app != "django_prometheus")
