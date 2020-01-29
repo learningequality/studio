50805776766566069561754f45065635bc836bdf
Jacob Pierce <w.jacobpierce@gmail.com>
2020-01-29 15:39:17 -0800

    revert sandbox

--- contentcuration/contentcuration/frontend/shared/views/Sandbox.vue
+++ contentcuration/contentcuration/frontend/shared/views/Sandbox.vue
@@ -1,30 +1,19 @@
 <template>
 
   <div>
-    <a href="javascript:void(0);" @click="$store.dispatch('showSnackbar', { duration: duration, text: 'hi!' })">Auto dismiss</a>
-    <label>Enter duration in ms (0 if you want it to live forever)</label>
-    <br>
-    <input v-model="duration" type="text" style="border: 1px solid black;">
+    Insert component here for testing!
     <TemplateComponent />
-    <GlobalSnackbar />
   </div>
 
 </template>
 <script>
 
   import TemplateComponent from '../../channelEdit/views/template/Template';
-  import GlobalSnackbar from './GlobalSnackbar';
 
   export default {
     name: 'Sandbox',
     components: {
       TemplateComponent,
-      GlobalSnackbar,
-    },
-    data() {
-      return {
-        duration: 1000,
-      };
     },
   };
 
