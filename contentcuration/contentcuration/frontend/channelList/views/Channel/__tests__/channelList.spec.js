15918098d7f3a327de6da5fded792c68d0384eee
Richard Tibbles <richard@learningequality.org>
2020-02-06 13:27:25 -0800

    Try to cleanup some async test left overs. Force exit anyway.

--- contentcuration/contentcuration/frontend/channelList/views/Channel/__tests__/channelList.spec.js
+++ contentcuration/contentcuration/frontend/channelList/views/Channel/__tests__/channelList.spec.js
@@ -1,36 +1,33 @@
 import { mount } from '@vue/test-utils';
-import flushPromises from 'flush-promises';
 import store from '../../../store';
 import router from '../../../router';
-import { RouterNames, ListTypes } from '../../../constants';
+import { ListTypes } from '../../../constants';
 import ChannelList from '../ChannelList.vue';
 
-function makeWrapper(listType) {
+function makeWrapper(listType, newChannelStub) {
   return mount(ChannelList, {
     router,
     store,
     propsData: {
-      listType: listType || ListTypes.EDITABLE,
+      listType: listType,
     },
+    methods: {
+      newChannel: newChannelStub,
+    }
   });
 }
 
 describe('channelList', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = makeWrapper();
-  });
   it('should only show the new channel button on edit mode', () => {
+    const wrapper = makeWrapper(ListTypes.EDITABLE);
     expect(wrapper.find('[data-test="add-channel"]').exists()).toBe(true);
     let viewWrapper = makeWrapper(ListTypes.VIEW_ONLY);
     expect(viewWrapper.find('[data-test="add-channel"]').exists()).toBe(false);
   });
-  it('should create a new channel when new channel button is clicked', async () => {
-    const newId = 'test';
-    wrapper.vm.createChannel = jest.fn(() => Promise.resolve(newId));
+  it('should create a new channel when new channel button is clicked', () => {
+    const newChannelStub = jest.fn();
+    const wrapper = makeWrapper(ListTypes.EDITABLE, newChannelStub);
     wrapper.find('[data-test="add-channel"]').trigger('click');
-    expect(wrapper.vm.createChannel).toHaveBeenCalled();
-    await flushPromises();
-    expect(wrapper.vm.$route.name).toEqual(RouterNames.CHANNEL_EDIT);
+    expect(newChannelStub).toHaveBeenCalled();
   });
 });
