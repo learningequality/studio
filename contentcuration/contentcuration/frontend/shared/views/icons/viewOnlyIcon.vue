1bc1ce73e463a91f0fd23792b549d8acee5ebeae
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-03-06 17:19:04 -0800

    removed svg size limit

--- contentcuration/contentcuration/frontend/shared/views/icons/viewOnlyIcon.vue
+++ contentcuration/contentcuration/frontend/shared/views/icons/viewOnlyIcon.vue
@@ -4,8 +4,8 @@
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     version="1.1"
-    width="24"
-    height="24"
+    width="auto"
+    height="auto"
     viewBox="0 0 24 24"
   >
     <!-- eslint-disable-next-line max-len -->
