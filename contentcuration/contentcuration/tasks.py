14282e1e6c117d51a4babc72ca351f970353161a
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-03-17 16:56:46 -0700

    Moved export code to separate utils file

--- contentcuration/contentcuration/tasks.py
+++ contentcuration/contentcuration/tasks.py
@@ -7,7 +7,6 @@ from celery.decorators import task
 from celery.utils.log import get_task_logger
 from django.conf import settings
 from django.contrib.sites.models import Site
-from django.core.files.storage import default_storage
 from django.core.mail import EmailMessage
 from django.db import transaction
 from django.template.loader import render_to_string
@@ -18,10 +17,9 @@ from contentcuration.models import ContentNode
 from contentcuration.models import Task
 from contentcuration.models import User
 from contentcuration.serializers import ContentNodeSerializer
+from contentcuration.utils.channels import export_public_channel_info
 from contentcuration.utils.csv_writer import write_channel_csv_file
 from contentcuration.utils.csv_writer import write_user_csv
-from contentcuration.utils.export_writer import ChannelDetailsCSVWriter
-from contentcuration.utils.export_writer import ChannelDetailsPDFWriter
 from contentcuration.utils.nodes import duplicate_node_bulk
 from contentcuration.utils.nodes import duplicate_node_inline
 from contentcuration.utils.nodes import move_nodes
@@ -153,24 +151,9 @@ def getnodedetails_task(node_id):
 
 @task(name='exportpublicchannelsinfo_task')
 def exportpublicchannelsinfo_task(user_id, export_type="pdf", site_id=1):
-    filename = "Kolibri Studio content library.{}".format(export_type)
     user = User.objects.get(pk=user_id)
     site = Site.objects.get(id=site_id)
-    channel_ids = Channel.objects.filter(public=True).values_list('pk', flat=True)
-    if export_type == 'csv':
-        filepath = ChannelDetailsCSVWriter(channel_ids, site=site, filename=filename).write()
-        mimetype = "text/csv"
-    else:
-        filepath = ChannelDetailsPDFWriter(channel_ids, site=site, condensed=True, filename=filename).write()
-        mimetype = "application/pdf"
-
-    subject = "Your {} has finished generating".format(export_type)
-    message = render_to_string('export/public_channels_email.txt', {'export_type': export_type, 'user': user})
-
-    with default_storage.open(filepath) as fobj:
-        email = EmailMessage(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
-        email.attach(filename, fobj.read(), mimetype)
-        email.send()
+    export_public_channel_info(user, export_type=export_type, site=site)
 
 
 type_mapping = {
