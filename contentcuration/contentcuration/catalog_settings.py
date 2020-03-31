c8777bf13651e947e44e954fa873a82d0c936a1b
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-03-30 19:30:08 -0700

    Removed other cache settings

--- contentcuration/contentcuration/catalog_settings.py
+++ contentcuration/contentcuration/catalog_settings.py
@@ -1,12 +1,5 @@
 # flake8: noqa
-from .dev_settings import *
 from .settings import *  # noqa
 
 LIBRARY_MODE = True
 SITE_READ_ONLY = True
-CACHES['default']['LOCATION'] = 'readonly_cache'
-
-MIDDLEWARE += (
-    'django.middleware.cache.UpdateCacheMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
-)
