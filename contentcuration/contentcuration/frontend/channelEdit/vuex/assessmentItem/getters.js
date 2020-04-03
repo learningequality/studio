e6f31a275f469432470678a9bc57eff23c75972c
Michaela Robosova <michaela@learningequality.com>
2020-04-03 13:49:49 +0200

    Assessment items getters tests + docstrings

--- contentcuration/contentcuration/frontend/channelEdit/vuex/assessmentItem/getters.js
+++ contentcuration/contentcuration/frontend/channelEdit/vuex/assessmentItem/getters.js
@@ -1,5 +1,8 @@
 import { validateAssessmentItem } from '../../utils';
 
+/**
+ * Get assessment items of a node.
+ */
 export function getAssessmentItems(state) {
   return function(contentNodeId) {
     if (!state.assessmentItemsMap[contentNodeId]) {
@@ -11,24 +14,36 @@ export function getAssessmentItems(state) {
   };
 }
 
+/**
+ * Get total number of assessment items of a node.
+ */
 export function getAssessmentItemsCount(state) {
   return function(contentNodeId) {
     return getAssessmentItems(state)(contentNodeId).length;
   };
 }
 
+/**
+ * Get an array of nested arrays containing error codes for corresponding assessment items.
+ */
 export function getAssessmentItemsErrors(state) {
   return function(contentNodeId) {
     return getAssessmentItems(state)(contentNodeId).map(validateAssessmentItem);
   };
 }
 
+/**
+ * Get total number of invalid assessment items of a node.
+ */
 export function getInvalidAssessmentItemsCount(state) {
   return function(contentNodeId) {
     return getAssessmentItemsErrors(state)(contentNodeId).filter(arr => arr.length).length;
   };
 }
 
+/**
+ * Are all assessment items of a node valid?
+ */
 export function getAssessmentItemsAreValid(state) {
   return function(contentNodeId) {
     return getInvalidAssessmentItemsCount(state)(contentNodeId) === 0;
