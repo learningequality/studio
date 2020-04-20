5e29c9305adcc9bf9a27a16c49a08f4939e067b2
Richard Tibbles <richard@learningequality.org>
2020-04-20 16:28:37 -0700

    Make file upload endpoint py3 compatible.

--- contentcuration/contentcuration/views/files.py
+++ contentcuration/contentcuration/views/files.py
@@ -14,7 +14,6 @@ from django.http import HttpResponse
 from django.http import HttpResponseBadRequest
 from django.http import HttpResponseForbidden
 from django.views.decorators.http import require_http_methods
-from le_utils.constants import content_kinds
 from le_utils.constants import exercises
 from le_utils.constants import file_formats
 from le_utils.constants import format_presets
@@ -22,7 +21,6 @@ from le_utils.constants.languages import getlang_by_alpha2
 from pressurecooker.subtitles import build_subtitle_converter
 from pressurecooker.subtitles import InvalidSubtitleFormatError
 from pressurecooker.subtitles import LANGUAGE_CODE_UNKNOWN
-from pressurecooker.videos import guess_video_preset_by_resolution
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import authentication_classes
@@ -34,11 +32,8 @@ from contentcuration.api import get_hash
 from contentcuration.api import write_file_to_storage
 from contentcuration.models import ContentNode
 from contentcuration.models import File
-from contentcuration.models import FormatPreset
 from contentcuration.models import generate_object_storage_name
 from contentcuration.models import generate_storage_url
-from contentcuration.models import License
-from contentcuration.serializers import ContentNodeEditSerializer
 from contentcuration.serializers import FileSerializer
 from contentcuration.serializers import TaskSerializer
 from contentcuration.tasks import create_async_task
@@ -65,7 +60,7 @@ def get_upload_url(request):
 @authentication_classes((TokenAuthentication, SessionAuthentication))
 @permission_classes((IsAuthenticated,))
 def temp_file_upload(request):
-    filename = write_file_to_storage(request.FILES.values()[0])
+    filename = write_file_to_storage(request.FILES['file'])
     return HttpResponse(generate_storage_url(filename))
 
 
