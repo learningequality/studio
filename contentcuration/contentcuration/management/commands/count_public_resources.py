89540259a5c4e1c921fda6d7c5031f5ca7a00bb7
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-04-30 14:39:39 -0700

    Added script to count unique public resources

---
+++ contentcuration/contentcuration/management/commands/count_public_resources.py
@@ -0,0 +1,21 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from contentcuration.models import Channel
+from contentcuration.models import ContentNode
+
+logging.basicConfig()
+logger = logging.getLogger('command')
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        public_tree_ids = Channel.objects.filter(public=True, deleted=False).values_list('main_tree__tree_id', flat=True)
+        count = ContentNode.objects.filter(tree_id__in=public_tree_ids) \
+                                   .exclude(kind_id='topic') \
+                                   .values_list('content_id', flat=True) \
+                                   .distinct() \
+                                   .count()
+        logger.info("{} unique resources".format(count))
