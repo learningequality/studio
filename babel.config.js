d11c0c4794611b528c4c3921ef88ad43af1f73e1
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-05-22 10:03:20 -0700

    Fixed pdf generation

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+  ],
   plugins: ['@babel/plugin-proposal-object-rest-spread'],
 };
