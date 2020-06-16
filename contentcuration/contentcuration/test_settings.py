0a614b9be610e19a8ee9501d89a201eda785b865
Richard Tibbles <richard@learningequality.org>
2020-06-16 11:26:38 -0700

    Update django-webpack-loader. Add a dummy webpack loader class for tests.

--- contentcuration/contentcuration/test_settings.py
+++ contentcuration/contentcuration/test_settings.py
@@ -4,7 +4,7 @@ from .settings import *  # noqa
 
 DEBUG = True
 
-if RUNNING_TESTS:
+if RUNNING_TESTS:  # noqa
     # if we're running tests, run Celery tests synchronously so tests won't complete before the process
     # is finished.
     CELERY_TASK_ALWAYS_EAGER = True
@@ -12,15 +12,19 @@ if RUNNING_TESTS:
 ALLOWED_HOSTS = ["studio.local", "192.168.31.9", "127.0.0.1", "*"]
 
 ACCOUNT_ACTIVATION_DAYS = 7
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 SITE_ID = 2
-logging.basicConfig(level='INFO')
+logging.basicConfig(level="INFO")
 
 # Allow the debug() context processor to add variables to template context.
 # Include here the IPs from which a local dev server might be accessed. See
 # https://docs.djangoproject.com/en/2.0/ref/settings/#internal-ips
 INTERNAL_IPS = (
-    '127.0.0.1',
-    'studio.local',
-    '192.168.31.9',
+    "127.0.0.1",
+    "studio.local",
+    "192.168.31.9",
 )
+
+WEBPACK_LOADER["DEFAULT"][  # noqa
+    "LOADER_CLASS"
+] = "contentcuration.tests.webpack_loader.TestWebpackLoader"
