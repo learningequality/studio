f833e1368ad4d916cf2d8c3b45314e2ae48aed71
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-07-17 17:26:40 -0700

    Updated review page in import modal to use latest components

--- contentcuration/contentcuration/frontend/channelEdit/views/ImportFromChannels/ReviewSelectionsPage.vue
+++ contentcuration/contentcuration/frontend/channelEdit/views/ImportFromChannels/ReviewSelectionsPage.vue
@@ -1,86 +1,57 @@
 <template>
 
-  <div>
-    <VToolbar fixed dark color="primary" class="over-app-bar">
-      <VBtn icon :to="exitRoute">
-        <VIcon>arrow_back</VIcon>
-      </VBtn>
-      <VToolbarTitle>
-        {{ $tr('toolbarTitle') }}
-      </VToolbarTitle>
-    </VToolbar>
-
-    <VSheet
-      class="pa-5 ma-5"
-      elevation="2"
-    >
-      <h1>{{ $tr('reviewSelectionHeader') }}</h1>
-      <p v-if="selected.length === 0">
-        {{ $tr('noResourcesSelected') }}
-      </p>
-      <VList>
-        <template v-for="(node, index) in selected">
-          <VListTile :key="index" class="py-2 height-auto">
-            <VLayout row align-center wrap>
-              <VFlex shrink class="px-2 mr-2">
-                <ContentNodeIcon :kind="node.kind" />
-              </VFlex>
-              <VFlex md5 sm12>
-                <VListTileContent>
-                  <VListTileTitle>
-                    <RouterLink :to="node.backRoute">
-                      {{ node.title }}
-                    </RouterLink>
-                  </VListTileTitle>
-                  <VListTileSubTitle v-if="numberOfResources(node) !== undefined">
-                    {{ $tr('resourcesInTopic', { count: numberOfResources(node) }) }}
-                  </VListTileSubTitle>
-                </VListTileContent>
-              </VFlex>
-              <VFlex md4 sm12>
-                {{ node.channelName }}
-              </VFlex>
-              <VFlex grow class="text-sm-right">
-                <VBtn flat @click="handleClickRemove(node)">
-                  {{ $tr('removeAction') }}
-                </VBtn>
-              </VFlex>
-            </VLayout>
-          </VListTile>
-          <VDivider :key="'div' + index" />
-        </template>
-      </VList>
-    </VSheet>
-
-    <BottomToolBar>
-      <VLayout align-center justify-end>
-        <span class="mr-2">
-          {{ $tr('resourcesSelected', { count: selectedResourcesCount }) }}
-        </span>
-        <VBtn
-          :disabled="selected.length === 0"
-          color="primary"
-          @click="handleClickImport"
-        >
-          {{ $tr('importAction') }}
-        </VBtn>
-      </VLayout>
-    </BottomToolBar>
-  </div>
+  <VContainer fluid>
+    <h1>{{ $tr('reviewSelectionHeader') }}</h1>
+    <p v-if="selected.length === 0">
+      {{ $tr('noResourcesSelected') }}
+    </p>
+    <VList>
+      <template v-for="(node, index) in selected">
+        <VListTile :key="index" class="py-2 height-auto">
+          <VLayout row align-center wrap>
+            <VFlex shrink class="px-2 mr-2">
+              <ContentNodeIcon :kind="node.kind" />
+            </VFlex>
+            <VFlex md5 sm12>
+              <VListTileContent>
+                <VListTileTitle>
+                  <ActionLink
+                    class="notranslate subheading"
+                    :text="node.title"
+                    @click="$emit('preview', node)"
+                  />
+                </VListTileTitle>
+                <VListTileSubTitle v-if="node.kind === 'topic'">
+                  {{ $tr('resourcesInTopic', { count: numberOfResources(node) }) }}
+                </VListTileSubTitle>
+              </VListTileContent>
+            </VFlex>
+            <VFlex md4 sm12 class="notranslate">
+              {{ node.original_channel_name }}
+            </VFlex>
+            <VFlex grow class="text-sm-right">
+              <VBtn flat @click="handleClickRemove(node)">
+                {{ $tr('removeAction') }}
+              </VBtn>
+            </VFlex>
+          </VLayout>
+        </VListTile>
+        <VDivider :key="'div' + index" />
+      </template>
+    </VList>
+  </VContainer>
 
 </template>
 
 
 <script>
 
-  import BottomToolBar from 'shared/views/BottomToolBar';
   import ContentNodeIcon from 'shared/views/ContentNodeIcon';
 
   export default {
     name: 'ReviewSelectionsPage',
     inject: ['RouterNames'],
     components: {
-      BottomToolBar,
       ContentNodeIcon,
     },
     props: {
@@ -91,42 +62,11 @@
           return [];
         },
       },
-      selectedResourcesCount: {
-        type: Number,
-        default: 0,
-      },
-    },
-    computed: {
-      exitRoute() {
-        // TODO save last location
-        return {
-          name: this.RouterNames.IMPORT_FROM_CHANNELS_BROWSE,
-        };
-      },
     },
     methods: {
       handleClickRemove(node) {
         const newSelected = this.selected.filter(x => x.id !== node.id);
         this.$emit('update:selected', newSelected);
-      },
-      handleClickImport() {
-        const nodeIds = this.selected.map(({ id }) => id);
-        return this.$store
-          .dispatch('importFromChannels/duplicateNodesToTarget', {
-            nodeIds,
-            targetNodeId: this.$route.params.destNodeId,
-          })
-          .then(duplicationTask => {
-            this.$router.push({
-              name: this.RouterNames.TREE_VIEW,
-              params: {
-                nodeId: this.$route.params.destNodeId,
-              },
-              query: {
-                watchTask: duplicationTask.id,
-              },
-            });
-          });
       },
       numberOfResources(node) {
         if (node.kind !== 'topic') {
@@ -137,11 +77,8 @@
     },
     $trs: {
       reviewSelectionHeader: 'Review selections',
-      resourcesSelected: `{count, number} {count, plural, one {selection} other {selections}}`,
       resourcesInTopic: `{count, number} {count, plural, one {resource} other {resources}}`,
-      importAction: 'Import',
       removeAction: 'Remove',
-      toolbarTitle: 'Resource selection',
       noResourcesSelected: 'No resources selected',
     },
   };
