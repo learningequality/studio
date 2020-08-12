6a096112e5b205f9a8906bcb81d85c55b018fb4c
Richard Tibbles <richard@learningequality.org>
2020-08-12 13:42:44 -0700

    Update duplicate node task to no longer rely on deprecated sort_order property.

--- contentcuration/contentcuration/frontend/channelEdit/vuex/importFromChannels/actions.js
+++ contentcuration/contentcuration/frontend/channelEdit/vuex/importFromChannels/actions.js
@@ -3,30 +3,17 @@ import { NOVALUE, ChannelListTypes } from 'shared/constants';
 
 import { Channel, SavedSearch } from 'shared/data/resources';
 
-// Function that calls the get_nodes_by_ids_complete endpoint
-export function getCompleteContentNode(context, nodeId) {
-  return client.get(`/api/get_nodes_by_ids_complete/${nodeId}`).then(response => {
-    return response.data[0];
-  });
-}
-
 // Function that calls the duplicate_nodes endpoint to add new nodes to the channel/topic
 export function duplicateNodesToTarget(context, { nodeIds, targetNodeId }) {
-  // get targetNodeId metadata.max_sort_order
-  return getCompleteContentNode(context, targetNodeId).then(targetNode => {
-    const maxSortOrder = targetNode.metadata.max_sort_order || 0;
-    return client
-      .post(window.Urls.duplicate_nodes(), {
-        node_ids: nodeIds,
-        sort_order: maxSortOrder + 1,
-        target_parent: targetNodeId,
-        channel_id: context.rootState.currentChannel.currentChannelId,
-      })
-      .then(response => {
-        context.dispatch('task/startTask', { task: response.data }, { root: true });
-        return response.data;
-      });
-  });
+  return client
+    .post(window.Urls.duplicate_nodes(), {
+      node_ids: nodeIds,
+      target_parent: targetNodeId,
+    })
+    .then(response => {
+      context.dispatch('task/startTask', { task: response.data }, { root: true });
+      return response.data;
+    });
 }
 
 export function fetchResourceSearchResults(context, params) {
