32cd86df17c5dc9c647af3edc9ccee600bd73ae2
Jacob Pierce <w.jacobpierce@gmail.com>
2020-08-31 10:25:49 -0700

    Move check to copyAll action

--- contentcuration/contentcuration/frontend/channelEdit/views/CurrentTopicView.vue
+++ contentcuration/contentcuration/frontend/channelEdit/views/CurrentTopicView.vue
@@ -379,7 +379,7 @@
           actionCallback: () => changeTracker.revert(),
         });
 
-        const copyPromise = this.copyAll({ id__in, deep: true }).then(() => {
+        this.copyAll({ id__in, deep: true }).then(() => {
           const nodes = id__in.map(id => this.getContentNode(id));
           const hasResource = nodes.find(n => n.kind !== 'topic');
           const hasTopic = nodes.find(n => n.kind === 'topic');
@@ -398,15 +398,6 @@
             actionCallback: () => changeTracker.revert(),
           });
         });
-
-        // If clipboardNode comes back undefined, the clipboard hasn't been
-        // loaded yet - so we must load it before returning the above Promise.
-        const clipboardNode = this.getContentNode(window.user.clipboard_root_id);
-        if (!clipboardNode) {
-          return this.loadClipboardTree().then(() => copyPromise);
-        } else {
-          return copyPromise;
-        }
       }),
       duplicateNodes: withChangeTracker(function(id__in, changeTracker) {
         const count = id__in.length;
