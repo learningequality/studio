e8ab715ea95aadb7f7b5820dd70dc42e46a63056
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-09-09 16:35:19 -0700

    Removed unnecessary assessment_items fields

--- contentcuration/contentcuration/frontend/channelEdit/components/edit/__tests__/data.js
+++ contentcuration/contentcuration/frontend/channelEdit/components/edit/__tests__/data.js
@@ -41,7 +41,6 @@ export function generateNode(props = {}) {
     is_prerequisite_of: [],
     files: [{ id: 'file', preset: { id: 'preset' } }],
     metadata: { resource_size: 0 },
-    assessment_items: [],
     extra_fields: extra_fields,
     tags: [],
     ancestors: [],
