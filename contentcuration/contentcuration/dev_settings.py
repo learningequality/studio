8acfe5822ba89c39c91891d49ca350babe60be2a
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-09-29 14:16:12 -0700

    Add Arabic to allowed languages in settings

--- contentcuration/contentcuration/dev_settings.py
+++ contentcuration/contentcuration/dev_settings.py
@@ -3,8 +3,6 @@ from .not_production_settings import *  # noqa
 
 DEBUG = True
 
-LANGUAGES += (("ar", ugettext("Arabic")),)  # noqa
-
 ROOT_URLCONF = "contentcuration.dev_urls"
 
 INSTALLED_APPS += ("drf_yasg",)
