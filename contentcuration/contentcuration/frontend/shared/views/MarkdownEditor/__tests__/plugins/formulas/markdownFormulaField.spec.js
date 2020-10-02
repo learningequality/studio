48d787e744ec6d59eabe599d52519100d9454656
Micah Fitch <micah@learningequality.org>
2020-10-02 09:47:23 -0500

    test to make sure contenteditable gets set to 'false'

--- contentcuration/contentcuration/frontend/shared/views/MarkdownEditor/__tests__/plugins/formulas/markdownFormulaField.spec.js
+++ contentcuration/contentcuration/frontend/shared/views/MarkdownEditor/__tests__/plugins/formulas/markdownFormulaField.spec.js
@@ -10,35 +10,34 @@ jest.mock('./style.css', () => '');
 
 let formulaEl;
 
-describe('MarkdownEditor - extensions - formula', () => {
-  describe('MarkdownFormula custom element', () => {
-    beforeAll(() => {
-      document.body.innerHTML = `
+describe('MarkdownFormulaField custom element', () => {
+  beforeAll(() => {
+    document.body.innerHTML = `
         <span is="markdown-formula-field">x^y</span>
       `;
-      formulaEl = document.querySelector('span[is="markdown-formula-field"]');
-      registerMarkdownFormulaField();
-    });
+    formulaEl = document.querySelector('span[is="markdown-formula-field"]');
+    registerMarkdownFormulaField();
+  });
 
-    test('getting formula latex from the custom element', async done => {
-      await window.customElements.whenDefined('markdown-formula-field');
-      expect(formulaEl.getVueInstance().latex).toBe('x^y');
-      done();
-    });
+  test('getting formula latex from the custom element', async done => {
+    await window.customElements.whenDefined('markdown-formula-field');
+    expect(formulaEl.getVueInstance().latex).toBe('x^y');
+    done();
+  });
 
-    it('renders some MathQuill markup in a shadowRoot', async done => {
-      await window.customElements.whenDefined('markdown-formula-field');
-      let shadowRoot = formulaEl.shadowRoot;
-      expect(shadowRoot).toBeTruthy();
-      let varEls = shadowRoot.querySelectorAll('var');
-      expect(varEls[0].innerHTML).toBe('x');
-      expect(varEls[1].innerHTML).toBe('y');
-      done();
-    });
+  it('renders some MathQuill markup in a shadowRoot', async done => {
+    await window.customElements.whenDefined('markdown-formula-field');
+    let shadowRoot = formulaEl.shadowRoot;
+    expect(shadowRoot).toBeTruthy();
+    let varEls = shadowRoot.querySelectorAll('var');
+    expect(varEls[0].innerHTML).toBe('x');
+    expect(varEls[1].innerHTML).toBe('y');
+    done();
+  });
 
-    it('sets `contenteditable=false` on its host element', async done => {
-      await window.customElements.whenDefined('markdown-formula-field');
-      done();
-    });
+  it('sets `contenteditable=false` on its host element', async done => {
+    await window.customElements.whenDefined('markdown-formula-field');
+    expect(formulaEl.getAttribute('contenteditable')).toBe('false');
+    done();
   });
 });
--- contentcuration/contentcuration/frontend/shared/views/MarkdownEditor/__tests__/plugins/image-upload/markdownImageField.spec.js
+++ contentcuration/contentcuration/frontend/shared/views/MarkdownEditor/__tests__/plugins/image-upload/markdownImageField.spec.js
@@ -12,41 +12,39 @@ jest.mock('./style.css', () => '');
 let imageEl;
 const imageMd = '![](${☣ CONTENTSTORAGE}/checksum.jpg =100x200)';
 
-describe('MarkdownEditor - extensions - image', () => {
-  describe('MarkdownImageField custom element', () => {
-    beforeAll(() => {
-      document.body.innerHTML = `
+describe('MarkdownImageField custom element', () => {
+  beforeAll(() => {
+    document.body.innerHTML = `
         <span is="markdown-image-field">${imageMd}</span>
       `;
-      imageEl = document.querySelector('span[is="markdown-image-field"]');
-      registerMarkdownImageField();
-    });
+    imageEl = document.querySelector('span[is="markdown-image-field"]');
+    registerMarkdownImageField();
+  });
 
-    test('renders some image markdown as an `img` element', async done => {
-      await window.customElements.whenDefined('markdown-image-field');
-      const innerImgEl = imageEl.shadowRoot.querySelector('img');
-      expect(innerImgEl.getAttribute('src')).toBe('/content/storage/c/h/checksum.jpg');
+  test('renders some image markdown as an `img` element', async done => {
+    await window.customElements.whenDefined('markdown-image-field');
+    const innerImgEl = imageEl.shadowRoot.querySelector('img');
+    expect(innerImgEl.getAttribute('src')).toBe('/content/storage/c/h/checksum.jpg');
 
-      expect(innerImgEl.getAttribute('width')).toBe('100');
-      done();
-    });
+    expect(innerImgEl.getAttribute('width')).toBe('100');
+    done();
+  });
 
-    it('can update its markdown upon resizing', async done => {
-      await window.customElements.whenDefined('markdown-image-field');
-      const imageVueComponent = imageEl.getVueInstance();
-      imageVueComponent.image.width = 5000000;
-      imageVueComponent.image.height = 1;
+  it('can update its markdown upon resizing', async done => {
+    await window.customElements.whenDefined('markdown-image-field');
+    const imageVueComponent = imageEl.getVueInstance();
+    imageVueComponent.image.width = 5000000;
+    imageVueComponent.image.height = 1;
 
-      const expectedMd = '![](${☣ CONTENTSTORAGE}/checksum.jpg =5000000x1)';
-      imageVueComponent.exportParamsToMarkdown();
+    const expectedMd = '![](${☣ CONTENTSTORAGE}/checksum.jpg =5000000x1)';
+    imageVueComponent.exportParamsToMarkdown();
 
-      await Vue.nextTick();
-      await Vue.nextTick(); // wait another tick for prop to update
+    await Vue.nextTick();
+    await Vue.nextTick(); // wait another tick for prop to update
 
-      expect(imageEl.innerHTML).toBe(expectedMd);
-      expect(imageVueComponent.markdown).toBe(expectedMd);
+    expect(imageEl.innerHTML).toBe(expectedMd);
+    expect(imageVueComponent.markdown).toBe(expectedMd);
 
-      done();
-    });
+    done();
   });
 });
