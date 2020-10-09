9fbd82e30cd8cd2e0048032f760c5da38ebd0e55
Blaine Jester <blainesworld@gmail.com>
2020-10-09 11:19:10 -0700

    Fix main test

--- contentcuration/contentcuration/frontend/accounts/pages/__tests__/main.spec.js
+++ contentcuration/contentcuration/frontend/accounts/pages/__tests__/main.spec.js
@@ -3,9 +3,13 @@ import router from '../../router';
 import Main from '../Main';
 
 const login = jest.fn();
+const showSnackbar = jest.fn();
 
 function makeWrapper() {
-  let wrapper = mount(Main, { router });
+  let wrapper = mount(Main, {
+    router,
+    stubs: ['GlobalSnackbar'],
+  });
   wrapper.setMethods({
     login: data => {
       return new Promise(resolve => {
@@ -13,6 +17,7 @@ function makeWrapper() {
         resolve();
       });
     },
+    showSnackbar: (...args) => showSnackbar(...args),
   });
   wrapper.setData({
     username: 'test@test.com',
@@ -38,41 +43,63 @@ describe('main', () => {
   beforeEach(() => {
     wrapper = makeWrapper();
     login.mockReset();
+    showSnackbar.mockReset();
+  });
+  afterEach(() => {
+    if (wrapper) {
+      wrapper.destroy();
+    }
   });
   it('should trigger submit method when form is submitted', () => {
+    expect(showSnackbar).not.toHaveBeenCalled();
     const submit = jest.fn();
     wrapper.setMethods({ submit });
     wrapper.find({ ref: 'form' }).trigger('submit');
     expect(submit).toHaveBeenCalled();
   });
   it('should call login with username and password provided', () => {
+    expect(showSnackbar).not.toHaveBeenCalled();
     wrapper.vm.submit();
     expect(login).toHaveBeenCalled();
   });
   it('should fail if username is not provided', () => {
+    expect(showSnackbar).not.toHaveBeenCalled();
     wrapper.setData({ username: ' ' });
     wrapper.vm.submit();
     expect(login).not.toHaveBeenCalled();
   });
   it('should fail if password is not provided', () => {
+    expect(showSnackbar).not.toHaveBeenCalled();
     wrapper.setData({ password: '' });
     wrapper.vm.submit();
     expect(login).not.toHaveBeenCalled();
   });
   it('should set loginFailed if login fails', async () => {
+    expect(showSnackbar).not.toHaveBeenCalled();
     wrapper.setMethods({ login: makeFailedPromise() });
     await wrapper.vm.submit();
     expect(wrapper.vm.loginFailed).toBe(true);
   });
   it('should say account has not been activated if login returns 405', async () => {
+    expect(showSnackbar).not.toHaveBeenCalled();
     wrapper.setMethods({ login: makeFailedPromise() });
     await wrapper.vm.submit();
     expect(wrapper.vm.loginFailed).toBe(true);
   });
   it('should navigate to next url if next query param is set', async () => {
-    window.location.assign = jest.fn();
     const testUrl = '/testnext/';
-    router.push(`/account/?next=${testUrl}`);
+    const location = new URL(`http://studio.time/?next=${testUrl}`);
+
+    delete window.location;
+    window.location = location;
+    window.location.assign = jest.fn();
+
+    wrapper.destroy();
+    wrapper = makeWrapper();
+    await wrapper.vm.$nextTick();
+
+    expect(showSnackbar).toHaveBeenCalled();
+
     await wrapper.vm.submit();
     expect(window.location.assign.mock.calls[0][0]).toBe(testUrl);
   });
