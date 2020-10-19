d161944a8bf3d173a005f035dc7e103389bcc6cb
Aron Asor <gitkraken@aron.asorman.io>
2020-10-19 07:54:30 -0700

    ignore flake8 linting for production_settings.py

--- contentcuration/contentcuration/production_settings.py
+++ contentcuration/contentcuration/production_settings.py
@@ -1,3 +1,5 @@
+# flake8: noqa
+# ignore the entire file in general, since we do a lot of overrides here which break pep8 compat
 from __future__ import absolute_import
 
 from . import settings as base_settings
