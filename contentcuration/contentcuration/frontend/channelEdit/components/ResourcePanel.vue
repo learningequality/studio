02fd3de90491e055f5301201dd4e23dd83d721a0
Blaine Jester <blainesworld@gmail.com>
2020-11-05 12:09:42 -0800

    Add scroll emitter and option to hide navigation

--- contentcuration/contentcuration/frontend/channelEdit/components/ResourcePanel.vue
+++ contentcuration/contentcuration/frontend/channelEdit/components/ResourcePanel.vue
@@ -1,8 +1,8 @@
 <template>
 
-  <VLayout row wrap>
+  <VLayout row wrap @scroll="$emit('scroll', $event)">
     <VFlex xs12>
-      <VLayout row>
+      <VLayout v-if="!hideNavigation" row>
         <VFlex v-if="!loading && node">
           <div class="mb-1">
             <!-- Slot for elements like "Back" link -->
@@ -22,6 +22,7 @@
           <Icon>clear</Icon>
         </VBtn>
       </VLayout>
+      <ContentNodeIcon v-else :kind="node.kind" includeText />
     </VFlex>
     <LoadingText v-if="loading || !node" class="mt-4" />
     <VFlex v-else xs12 class="pb-5">
@@ -351,6 +352,10 @@
         type: String,
         required: true,
       },
+      hideNavigation: {
+        type: Boolean,
+        default: false,
+      },
     },
     data() {
       return {
@@ -505,7 +510,7 @@
       },
     },
     watch: {
-      // Listen to node id specifically to avoid recursvie call to watcher,
+      // Listen to node id specifically to avoid recursive call to watcher,
       // but still get updated properly if need to wait for node to be loaded
       'node.id'() {
         this.showAnswers = false;
