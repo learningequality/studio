eef461ae0dd6810ff3b8f7245baa89d95b9a9181
sairina <sairina.merinotsui@gmail.com>
2020-11-24 20:59:52 -0800

    makes update banner hidden for view-only channels

--- contentcuration/contentcuration/frontend/channelEdit/views/TreeView/index.vue
+++ contentcuration/contentcuration/frontend/channelEdit/views/TreeView/index.vue
@@ -1,7 +1,7 @@
 <template>
 
   <TreeViewBase @dropToClipboard="handleDropToClipboard">
-    <template v-if="hasStagingTree" #extension>
+    <template v-if="hasStagingTree && canManage" #extension>
       <Banner
         :value="true"
         border
@@ -197,6 +197,7 @@
         'stagingId',
         'rootId',
         'canEdit',
+        'canManage',
       ]),
       ...mapGetters('contentNode', ['getContentNode', 'getContentNodeAncestors']),
       ...mapGetters('draggable', ['activeDraggableRegionId']),
