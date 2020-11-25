5f136d984862f1139b344db14ed4d08e98fb9c18
Aron Asor <aron@noreply.github.com>
2020-11-25 06:04:34 -0800

    add bucket name to helm deploy script

--- k8s/helm-deploy.sh
+++ k8s/helm-deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-set -xe
+set -euo pipefail
 
 RELEASE_NAME=$1
 STUDIO_APP_IMAGE_NAME=$2
 STUDIO_NGINX_IMAGE_NAME=$3
+STUDIO_BUCKET_NAME=$4
 
 K8S_DIR=$(dirname $0)
 
@@ -12,12 +13,14 @@ function get_secret {
     gcloud beta secrets versions access --secret $1 latest
 }
 
+# helm template \
 helm upgrade --install \
      --namespace $RELEASE_NAME --create-namespace \
      --set studioApp.postmarkApiKey=$(get_secret postmark-api-key) \
      --set studioApp.releaseCommit=$(git rev-parse HEAD) \
      --set studioApp.imageName=$STUDIO_APP_IMAGE_NAME \
      --set studioNginx.imageName=$STUDIO_NGINX_IMAGE_NAME \
+     --set studioApp.gcs.bucketName=$4 \
      --set settings=contentcuration.production_settings \
      --set sentry.dsnKey=$(get_secret sentry-dsn-key) \
      --set redis.password=$(get_secret redis-password) \
