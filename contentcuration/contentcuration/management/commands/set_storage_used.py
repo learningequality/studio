3e278b61273a868c5f51cbeb4b72ac87c1944c92
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-12-02 15:03:07 -0800

    Added script for setting all user storage

---
+++ contentcuration/contentcuration/management/commands/set_storage_used.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+from contentcuration.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for user in User.objects.filter(disk_space_used=0):
+            user.set_storage_used()
