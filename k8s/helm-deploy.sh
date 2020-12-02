d7bea79051d3f0c6656830956b35718ea7d6e66b
Aron Asor <aron@noreply.github.com>
2020-12-02 10:45:22 -0800

    add more flags to the helm-deploy script

--- k8s/helm-deploy.sh
+++ k8s/helm-deploy.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -xeuo pipefail
 
 RELEASE_NAME=$1
 STUDIO_APP_IMAGE_NAME=$2
 STUDIO_NGINX_IMAGE_NAME=$3
 STUDIO_BUCKET_NAME=$4
+COMMIT_SHA=$5
+# not used yet, will integrate later
+PROJECT_ID=$6
+DATABASE_INSTANCE_NAME=$7
 
 K8S_DIR=$(dirname $0)
 
 function get_secret {
-    gcloud beta secrets versions access --secret $1 latest
+    gcloud secrets versions access --secret=$1 latest
 }
 
 # helm template \
 helm upgrade --install \
      --namespace $RELEASE_NAME --create-namespace \
      --set studioApp.postmarkApiKey=$(get_secret postmark-api-key) \
-     --set studioApp.releaseCommit=$(git rev-parse HEAD) \
+     --set studioApp.releaseCommit=$COMMIT_SHA \
      --set studioApp.imageName=$STUDIO_APP_IMAGE_NAME \
      --set studioNginx.imageName=$STUDIO_NGINX_IMAGE_NAME \
-     --set studioApp.gcs.bucketName=$4 \
+     --set studioApp.gcs.bucketName=$STUDIO_BUCKET_NAME \
      --set settings=contentcuration.production_settings \
      --set sentry.dsnKey=$(get_secret sentry-dsn-key) \
      --set redis.password=$(get_secret redis-password) \
