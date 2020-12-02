29e663bdd7a46ab5134ebdbb18a3703b1d5ba607
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-12-01 17:02:32 -0800

    Update storage after deploying staged channels

--- contentcuration/contentcuration/api.py
+++ contentcuration/contentcuration/api.py
@@ -94,6 +94,7 @@ def activate_channel(channel, user):
     channel.save()
 
     user.staged_files.all().delete()
+    user.set_space_used()
 
     change = generate_update_event(
         channel.id,
