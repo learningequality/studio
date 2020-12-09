6dd20d883b23875c5c37a983c52d279d102cd618
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-12-09 14:53:05 -0800

    Added force flag to storage calculation command

--- contentcuration/contentcuration/management/commands/set_storage_used.py
+++ contentcuration/contentcuration/management/commands/set_storage_used.py
@@ -1,9 +1,16 @@
+import progressbar
 from django.core.management.base import BaseCommand
 
 from contentcuration.models import User
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("--force", action="store_true", dest="force", default=False)
+
     def handle(self, *args, **options):
-        for user in User.objects.filter(disk_space_used=0):
+        users = User.objects.all() if options["force"] else User.objects.filter(disk_space_used=0)
+        bar = progressbar.ProgressBar(max_value=users.count())
+        for index, user in enumerate(users):
             user.set_space_used()
+            bar.update(index)
