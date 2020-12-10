3ad854b0f4dc316fe2c333a9f1d31d258f295b11
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-12-10 09:27:01 -0800

    Fixed management command

--- contentcuration/contentcuration/management/commands/generate_staged_diffs.py
+++ contentcuration/contentcuration/management/commands/generate_staged_diffs.py
@@ -3,9 +3,9 @@ import logging
 import progressbar
 from django.core.management.base import BaseCommand
 
-from contentcuration.api import get_staged_diff
-from contentcuration.api import get_staged_diff_if_available
 from contentcuration.models import Channel
+from contentcuration.utils.nodes import generate_diff
+from contentcuration.utils.nodes import get_diff
 
 logging.basicConfig()
 logger = logging.getLogger('command')
@@ -22,7 +22,7 @@ class Command(BaseCommand):
         channels_with_staged_changes = Channel.objects.exclude(staging_tree=None)
         bar = progressbar.ProgressBar(max_value=channels_with_staged_changes.count())
         for i, c in enumerate(channels_with_staged_changes):
-            if options["force"] or not get_staged_diff_if_available(c.pk):
-                get_staged_diff(c.pk)
+            if options["force"] or not get_diff(c.staging_tree, c.main_tree):
+                generate_diff(c.staging_tree.pk, c.main_tree.pk)
             bar.update(i)
         logger.info("\n\nDONE")
