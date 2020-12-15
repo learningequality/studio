5671abe2b205ba11a46025c78b26955cbb7680c5
sairina <sairina.merinotsui@gmail.com>
2020-12-15 14:40:03 -0800

    Update tests

--- contentcuration/contentcuration/frontend/settings/pages/__tests__/usingStudio.spec.js
+++ contentcuration/contentcuration/frontend/settings/pages/__tests__/usingStudio.spec.js
@@ -1,26 +1,51 @@
-import { mount } from '@vue/test-utils';
+import Vuex from 'vuex';
+import { mount, createLocalVue } from '@vue/test-utils';
+import { Store } from 'vuex';
 import UsingStudio from '../UsingStudio/index';
+import { policies } from 'shared/constants';
 
-function makeWrapper() {
-  return mount(UsingStudio);
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+function makeWrapper({ methods = {} } = {}) {
+  const store = new Store({
+    modules: {
+      policies: {
+        namespaced: true,
+        actions: {
+          openPolicy: jest.fn(),
+        },
+      },
+    }
+  });
+
+  return mount(UsingStudio, {
+    localVue,
+    store,
+    methods,
+    stubs: ['PolicyModals'],
+  });
 }
 
 describe('usingStudio tab', () => {
   let wrapper;
+  let openPolicy;
+
   beforeEach(() => {
-    wrapper = makeWrapper();
+    openPolicy = jest.fn();
+    wrapper = makeWrapper({ methods: { openPolicy } });
   });
 
   it('clicking privacy link should open policies modal', () => {
     wrapper.find('[data-test="policy-link"]').trigger('click');
-    expect(wrapper.vm.showPrivacyPolicy).toBe(true);
+    expect(openPolicy).toHaveBeenCalledWith(policies.PRIVACY);
   });
   it('clicking terms of service link should open tos modal', () => {
     wrapper.find('[data-test="tos-link"]').trigger('click');
-    expect(wrapper.vm.showTermsOfService).toBe(true);
+    expect(openPolicy).toHaveBeenCalledWith(policies.TERMS_OF_SERVICE);
   });
   it('clicking community standards link should open standards modal', () => {
     wrapper.find('[data-test="communitystandards-link"]').trigger('click');
-    expect(wrapper.vm.showCommunityStandards).toBe(true);
+    expect(openPolicy).toHaveBeenCalledWith(policies.COMMUNITY_STANDARDS);
   });
 });
--- contentcuration/contentcuration/frontend/shared/vuex/policies/plugin.js
+++ contentcuration/contentcuration/frontend/shared/vuex/policies/plugin.js
@@ -1,32 +1,48 @@
+/**
+ * Policies vuex plugin that connects the Vuex state with the `window.user` variable
+ *
+ * @param store
+ */
 export default function PoliciesPlugin(store) {
   let userData = window.user;
 
   store.subscribeAction(action => {
+    // An update to the policies, update `window.user` with new data
     if (action.type === 'policies/setPolicies') {
       userData.policies = {
-        ...(userData.policies || {}),
+        ...(userData['policies'] || {}),
         ...action.payload,
       };
     }
   });
 
+  // When `window.user` is defined
   if (userData) {
-    store.dispatch('policies/setPolicies', userData.policies);
+    // Add policies data immediately
+    if (data.hasOwnProperty('policies')) {
+      store.dispatch('policies/setPolicies', userData.policies);
+    }
   } else {
-    // `window.user` is not defined, so define it and hook into when it's set
-    // so we can trigger Vuex mutation to add policy data
+    // Here `window.user` is not defined, so define it and hook into when it's
+    // set so we can trigger Vuex mutation to add policy data
     Object.defineProperty(window, 'user', {
       get() {
         return userData;
       },
       set(data) {
-        // Commit mutation when user data is already set to avoid circular loop
+        // Commit mutation when window.user is already set to avoid circular loop
         if (userData) {
-          store.commit('policies/SET_POLICIES', data.policies);
+          if (data.hasOwnProperty('policies')) {
+            store.commit('policies/SET_POLICIES', data.policies);
+          }
           userData = data;
         } else {
           userData = data;
-          store.dispatch('policies/setPolicies', data.policies);
+          // Dispatch initial setPolicies when window.user is set for the first time
+          if (data.hasOwnProperty('policies')) {
+            // Will trigger subscribed action above
+            store.dispatch('policies/setPolicies', data.policies);
+          }
         }
       },
       enumerable: true,
