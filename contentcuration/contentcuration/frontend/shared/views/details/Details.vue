aa5766a6bdfd6158df01986bef910bc899dca80b
Jordan Yoshihara <jordanyoshihara@gmail.com>
2021-01-07 10:55:39 -0800

    Standardized channel thumbnail styling

--- contentcuration/contentcuration/frontend/shared/views/details/Details.vue
+++ contentcuration/contentcuration/frontend/shared/views/details/Details.vue
@@ -223,7 +223,7 @@
             align-center
             row
           >
-            <VFlex style="max-width: 200px">
+            <VFlex class="source-thumbnail">
               <Thumbnail :src="channel.thumbnail" />
             </VFlex>
             <VFlex v-if="libraryMode" class="font-weight-bold notranslate px-4 subheading">
@@ -486,6 +486,12 @@
       font-weight: bold;
       text-transform: none;
     }
+  }
+
+  .source-thumbnail {
+    min-width: 150px;
+    max-width: 150px;
+    border: 1px solid var(--v-grey-lighten3);
   }
 
   .sample-nodes .v-card__text {
