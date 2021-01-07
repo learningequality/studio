ef9c27c1424f4f0661cade4ae6991db8d771d5f3
Jordan Yoshihara <jordanyoshihara@gmail.com>
2021-01-07 13:36:11 -0800

    Actually detect if nodes have changed

--- contentcuration/contentcuration/frontend/channelEdit/views/TreeView/TreeViewBase.vue
+++ contentcuration/contentcuration/frontend/channelEdit/views/TreeView/TreeViewBase.vue
@@ -271,7 +271,10 @@
         return this.rootNode && this.rootNode.error_count;
       },
       isChanged() {
-        return true;
+        return (
+          this.rootNode &&
+          (this.rootNode.has_updated_descendants || this.rootNode.has_new_descendants)
+        );
       },
       isPublished() {
         return this.currentChannel && this.currentChannel.published;
