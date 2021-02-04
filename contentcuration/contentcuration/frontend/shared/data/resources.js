9bf7bc5d0cb8a85095a594cdbb34eee55eaa2041
Blaine Jester <blainesworld@gmail.com>
2021-02-04 11:21:44 -0800

    Review update

--- contentcuration/contentcuration/frontend/shared/data/resources.js
+++ contentcuration/contentcuration/frontend/shared/data/resources.js
@@ -1215,7 +1215,7 @@ export const ContentNode = new TreeResource({
     const values = [nodeId, channelId];
     return this.table.get({ '[node_id+channel_id]': values }).then(node => {
       if (!node) {
-        return this.requestCollection({ _node_id_channel_id___in: values.join(',') }).then(
+        return this.requestCollection({ _node_id_channel_id_: values }).then(
           nodes => nodes[0]
         );
       }
