611e84b866dbf75a089e9657b6a2e776d820ab20
Blaine Jester <blainesworld@gmail.com>
2021-02-12 12:45:25 -0800

    Add support for different types of listeners other than vuex commit

--- contentcuration/contentcuration/frontend/shared/data/index.js
+++ contentcuration/contentcuration/frontend/shared/data/index.js
@@ -1,5 +1,4 @@
 import Dexie from 'dexie';
-import isFunction from 'lodash/isFunction';
 import mapValues from 'lodash/mapValues';
 import { createLeaderElection } from './leaderElection';
 import channel from './broadcastChannel';
@@ -10,7 +9,7 @@ import {
   IGNORED_SOURCE,
   TABLE_NAMES,
 } from './constants';
-import db, { CLIENTID } from './db';
+import db from './db';
 import { INDEXEDDB_RESOURCES } from './registry';
 import { startSyncing, stopSyncing } from './serverSync';
 import * as resources from './resources';
@@ -18,8 +17,6 @@ import * as resources from './resources';
 // Re-export for ease of reference.
 export { CHANGE_TYPES, TABLE_NAMES } from './constants';
 export { API_RESOURCES, INDEXEDDB_RESOURCES } from './registry';
-
-const LISTENERS = {};
 
 export function setupSchema() {
   if (!Object.keys(resources).length) {
@@ -51,31 +48,6 @@ if (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined') {
   window.resetDB = resetDB;
 }
 
-function setupListeners() {
-  db.on('changes', function(changes) {
-    changes.forEach(function(change) {
-      // Don't invoke listeners if their client originated the change
-      if (CLIENTID !== change.source) {
-        const tableListeners = LISTENERS[change.table];
-        if (tableListeners) {
-          const changeListeners = tableListeners[change.type];
-          if (changeListeners) {
-            for (let listener of changeListeners.values()) {
-              // Always invoke the callback with the full object representation
-              // It is up to the callbacks to know how to parse this.
-              const obj = Object.assign(
-                { [db[change.table].schema.primKey.keyPath]: change.key },
-                change.obj || {}
-              );
-              listener(obj);
-            }
-          }
-        }
-      }
-    });
-  });
-}
-
 function runElection() {
   const elector = createLeaderElection(channel);
 
@@ -88,35 +60,5 @@ function runElection() {
 
 export function initializeDB() {
   setupSchema();
-  setupListeners();
-
   return db.open().then(runElection);
-}
-
-export function registerListener(table, change, callback) {
-  change = Number(change);
-  if (!Object.values(CHANGE_TYPES).includes(change)) {
-    throw RangeError(
-      `Change must be ${CHANGE_TYPES.CREATED}, ${CHANGE_TYPES.UPDATED}, or ${CHANGE_TYPES.DELETED}`
-    );
-  }
-  if (!isFunction(callback)) {
-    throw TypeError('Callback argument must be a function');
-  }
-  if (!LISTENERS[table]) {
-    LISTENERS[table] = {};
-  }
-  if (!LISTENERS[table][change]) {
-    LISTENERS[table][change] = new Map();
-  }
-  LISTENERS[table][change].set(callback, callback);
-}
-
-export function removeListener(table, change, callback) {
-  if (LISTENERS[table]) {
-    if (LISTENERS[table][change]) {
-      return LISTENERS[table][change].delete(callback);
-    }
-  }
-  return false;
 }
---
+++ contentcuration/contentcuration/frontend/shared/vuex/indexedDBPlugin/index.spec.js
@@ -0,0 +1,269 @@
+import { EventEmitter } from 'events';
+import uuidv4 from 'uuid/v4';
+import IndexedDBPlugin, {
+  commitListener,
+  dispatchListener,
+  Listener,
+  ListenerGroup,
+} from 'shared/vuex/indexedDBPlugin/index';
+import { CLIENTID } from 'shared/data/db';
+import { CHANGE_TYPES } from 'shared/data';
+
+describe('Listener', function() {
+  beforeEach(() => {
+    this.callback = jest.fn();
+    this.listener = new Listener(this.callback);
+  });
+
+  describe('.getEventName()', () => {
+    it('should return null when unbound', () => {
+      expect(this.listener.getEventName()).toEqual(null);
+    });
+    it("should return event name composed of table's name and change type", () => {
+      this.listener.tableName = 'testTable';
+      this.listener.changeType = CHANGE_TYPES.CREATED;
+      expect(this.listener.getEventName()).toEqual('testTable/1');
+    });
+  });
+
+  describe('.prefix(name)', () => {
+    it('should return the name without a namespacePrefix', () => {
+      expect(this.listener.prefix('someVuexName')).toEqual('someVuexName');
+    });
+    it('should prefix with namespacePrefix', () => {
+      this.listener.namespacePrefix = 'test';
+      expect(this.listener.prefix('someVuexName')).toEqual('test/someVuexName');
+    });
+  });
+
+  const bindTest = (namespacePrefix = null) => {
+    it('should return a new Listener', () => {
+      const l = this.listener.bind('testTable', CHANGE_TYPES.CREATED, namespacePrefix);
+      expect(l).not.toEqual(this.listener);
+      expect(l).toBeInstanceOf(Listener);
+    });
+
+    it('should validate the changeType', () => {
+      expect(() => {
+        this.listener.bind('testTable', -1, namespacePrefix);
+      }).toThrow(/^Change must be/);
+    });
+
+    it('should assign bind args on new instance', () => {
+      const l = this.listener.bind('testTable', CHANGE_TYPES.CREATED, namespacePrefix);
+
+      expect(this.listener.callback).toEqual(this.callback);
+      expect(this.listener.tableName).toEqual(null);
+      expect(this.listener.changeType).toEqual(null);
+      expect(this.listener.namespacePrefix).toEqual(null);
+
+      expect(l.callback).toEqual(this.callback);
+      expect(l.tableName).toEqual('testTable');
+      expect(l.changeType).toEqual(CHANGE_TYPES.CREATED);
+      expect(l.namespacePrefix).toEqual(namespacePrefix);
+    });
+  };
+
+  describe('.bind(tableName, changeType)', bindTest);
+  describe('.bind(tableName, changeType, namespacePrefix)', bindTest.bind({}, 'testNamespace'));
+});
+
+describe('ListenerGroup', function() {
+  beforeEach(() => {
+    this.listeners = [new Listener(jest.fn()), new Listener(jest.fn()), new Listener(jest.fn())];
+    this.group = new ListenerGroup(this.listeners);
+  });
+
+  const bindTest = (namespacePrefix = null) => {
+    it('should return a new Listener', () => {
+      const l = this.group.bind('testTable', CHANGE_TYPES.CREATED, namespacePrefix);
+      expect(l).not.toEqual(this.group);
+      expect(this.listeners.length).toEqual(l.listeners.length);
+      expect(l).toBeInstanceOf(ListenerGroup);
+    });
+
+    it('should assign bind args on new listeners', () => {
+      const l = this.group.bind('testTable', CHANGE_TYPES.CREATED, namespacePrefix);
+
+      for (let i = 0; i < this.listeners.length; i++) {
+        const oldListener = this.listeners[i];
+        const newListener = l.listeners[i];
+
+        expect(oldListener).not.toEqual(newListener);
+        expect(oldListener.callback).toEqual(newListener.callback);
+
+        expect(oldListener.tableName).toEqual(null);
+        expect(oldListener.changeType).toEqual(null);
+        expect(oldListener.namespacePrefix).toEqual(null);
+
+        expect(newListener.tableName).toEqual('testTable');
+        expect(newListener.changeType).toEqual(CHANGE_TYPES.CREATED);
+        expect(newListener.namespacePrefix).toEqual(namespacePrefix);
+      }
+    });
+  };
+
+  describe('.bind(tableName, changeType)', bindTest);
+  describe('.bind(tableName, changeType, namespacePrefix)', bindTest.bind({}, 'testNamespace'));
+});
+
+describe('commitListener', function() {
+  beforeEach(() => {
+    this.commit = jest.fn();
+    this.store = {
+      commit: this.commit,
+    };
+    this.obj = {};
+    this.listener = commitListener('testMutationName');
+  });
+
+  it('should return a Listener', () => {
+    expect(this.listener).toBeInstanceOf(Listener);
+  });
+
+  describe('returned Listener.callback', () => {
+    it('should trigger store.commit()', () => {
+      this.listener.callback(this.store, this.obj);
+      expect(this.commit).toHaveBeenCalledWith('testMutationName', this.obj);
+    });
+
+    it('should trigger store.commit() with prefix', () => {
+      const l = this.listener.bind('testTable', CHANGE_TYPES.CREATED, 'testPrefix');
+      l.callback(this.store, this.obj);
+      expect(this.commit).toHaveBeenCalledWith('testPrefix/testMutationName', this.obj);
+    });
+  });
+});
+
+describe('dispatchListener', function() {
+  beforeEach(() => {
+    this.dispatch = jest.fn();
+    this.store = {
+      dispatch: this.dispatch,
+    };
+    this.obj = {};
+    this.listener = dispatchListener('testMutationName');
+  });
+
+  it('should return a Listener', () => {
+    expect(this.listener).toBeInstanceOf(Listener);
+  });
+
+  describe('returned Listener.callback', () => {
+    it('should trigger store.dispatch()', () => {
+      this.listener.callback(this.store, this.obj);
+      expect(this.dispatch).toHaveBeenCalledWith('testMutationName', this.obj);
+    });
+
+    it('should trigger store.dispatch() with prefix', () => {
+      const l = this.listener.bind('testTable', CHANGE_TYPES.CREATED, 'testPrefix');
+      l.callback(this.store, this.obj);
+      expect(this.dispatch).toHaveBeenCalledWith('testPrefix/testMutationName', this.obj);
+    });
+  });
+});
+
+describe('IndexedDBPlugin', function() {
+  beforeEach(() => {
+    this.source = uuidv4();
+    this.db = {
+      events: new EventEmitter(),
+      on(...args) {
+        return this.events.on(...args);
+      },
+    };
+    this.dispatch = jest.fn();
+    this.store = {};
+    this.obj = {};
+    this.changes = [];
+    this.listeners = [];
+  });
+
+  it('should listen for events on `db`', () => {
+    expect(this.db.events.listenerCount('changes')).toEqual(0);
+    IndexedDBPlugin(this.db, this.listeners);
+    expect(this.db.events.listenerCount('changes')).toEqual(1);
+  });
+
+  it('should return a function that registers listeners', () => {
+    const listener = new Listener(jest.fn());
+    const register = jest.spyOn(listener, 'register').mockImplementation(() => {});
+    const result = IndexedDBPlugin(this.db, [listener]);
+    expect(result).toBeInstanceOf(Function);
+    result(this.store);
+    expect(register).toHaveBeenCalledWith(expect.any(EventEmitter), this.store);
+  });
+
+  it('should handle change events and trigger listeners', () => {
+    const testChange = (table, type, source = null, obj = {}) => {
+      this.db[table] = { schema: { primKey: { keyPath: 'testId' } } };
+      const change = {
+        key: uuidv4(),
+        table,
+        type,
+        source: source || this.source,
+        obj,
+      };
+      this.changes.push(change);
+      return change;
+    };
+
+    const testListener = (table, type, namespacePrefix = null) => {
+      const callback = jest.fn();
+      let callObj = null;
+      this.listeners.push(new Listener(callback).bind(table, type, namespacePrefix));
+      return {
+        addChange: (source, obj = {}) => {
+          const change = testChange.call(this, table, type, source, obj);
+          callObj = {
+            ...obj,
+            testId: change.key,
+          };
+        },
+        assertCalled: () => {
+          expect(callback).toHaveBeenCalledWith(this.store, callObj);
+        },
+        assertNotCalled: () => {
+          expect(callback).not.toHaveBeenCalledWith(this.store, callObj);
+        },
+      };
+    };
+
+    const listener1 = testListener('testTableA', CHANGE_TYPES.CREATED);
+    const listener2 = testListener('testTableA', CHANGE_TYPES.UPDATED);
+    const listener3 = testListener('testTableA', CHANGE_TYPES.DELETED);
+    const listener4 = testListener('testTableB', CHANGE_TYPES.UPDATED, 'testVuexNamespace');
+    const listener5 = testListener('testTableC', CHANGE_TYPES.CREATED, 'anotherVuexNamespace');
+    const listener6 = testListener('testTableC', CHANGE_TYPES.UPDATED, 'anotherVuexNamespace');
+    const listener7 = testListener('testTableZ', CHANGE_TYPES.CREATED);
+
+    listener1.addChange();
+    listener2.addChange();
+    listener3.addChange();
+    listener4.addChange();
+    listener5.addChange();
+    listener6.addChange();
+    listener7.addChange(CLIENTID);
+
+    const result = IndexedDBPlugin(this.db, this.listeners);
+    result(this.store);
+
+    listener1.assertNotCalled();
+    listener2.assertNotCalled();
+    listener3.assertNotCalled();
+    listener4.assertNotCalled();
+    listener5.assertNotCalled();
+    listener6.assertNotCalled();
+    listener7.assertNotCalled();
+
+    this.db.events.emit('changes', this.changes);
+
+    listener1.assertCalled();
+    listener2.assertCalled();
+    listener3.assertCalled();
+    listener4.assertCalled();
+    listener5.assertCalled();
+    listener6.assertCalled();
+    listener7.assertNotCalled(); // from source CLIENTID
+  });
+});
