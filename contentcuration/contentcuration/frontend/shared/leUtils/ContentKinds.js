53dbba7842176c9af91075f9f1b3c395f33c8ccd
sairina <sairina.merinotsui@gmail.com>
2021-02-19 15:16:45 -0800

    Complete le-utils upgrade

--- contentcuration/contentcuration/frontend/shared/leUtils/ContentKinds.js
+++ contentcuration/contentcuration/frontend/shared/leUtils/ContentKinds.js
@@ -1,13 +1,13 @@
 // Constant values for ContentKinds sorted by id
 const ContentKinds = new Set([
-  'audio',
-  'document',
-  'exercise',
-  'h5p',
-  'html5',
-  'slideshow',
-  'topic',
-  'video',
+  "audio",
+  "document",
+  "exercise",
+  "h5p",
+  "html5",
+  "slideshow",
+  "topic",
+  "video",
 ]);
 
 export default ContentKinds;
@@ -15,12 +15,12 @@ export default ContentKinds;
 export const ContentKindsList = Array.from(ContentKinds);
 
 export const ContentKindsNames = {
-  AUDIO: 'audio',
-  DOCUMENT: 'document',
-  EXERCISE: 'exercise',
-  H5P: 'h5p',
-  HTML5: 'html5',
-  SLIDESHOW: 'slideshow',
-  TOPIC: 'topic',
-  VIDEO: 'video',
+  AUDIO: "audio",
+  DOCUMENT: "document",
+  EXERCISE: "exercise",
+  H5P: "h5p",
+  HTML5: "html5",
+  SLIDESHOW: "slideshow",
+  TOPIC: "topic",
+  VIDEO: "video",
 };
