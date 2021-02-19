20f28f778c27aaa2a45d7ec1f0e6f99a2dd980cf
sairina <sairina.merinotsui@gmail.com>
2021-02-19 15:27:09 -0800

    Fix linting

--- contentcuration/contentcuration/frontend/shared/leUtils/Roles.js
+++ contentcuration/contentcuration/frontend/shared/leUtils/Roles.js
@@ -1,14 +1,11 @@
 // Constant values for Roles sorted by value
-const Roles = new Set([
-  "coach",
-  "learner",
-]);
+const Roles = new Set(['coach', 'learner']);
 
 export default Roles;
 
 export const RolesList = Array.from(Roles);
 
 export const RolesNames = {
-  COACH: "coach",
-  LEARNER: "learner",
+  COACH: 'coach',
+  LEARNER: 'learner',
 };
