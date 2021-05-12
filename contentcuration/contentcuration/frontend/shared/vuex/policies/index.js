6a232afdb0de3080365b76ab6ac581b17077a5dc
Michaela <michaela@learningequality.org>
2021-05-12 10:46:36 +0200

    Simplify policies store tests
    - export getters, mutations, and actions to allow
      testing them directly instead of the need to initialize
      the whole store, similarly to how we test getters etc.
      of our other store modules (this has also allowed
      deletion of mocks that are not related to this test
      suite)
    - use hardcoded sample data in tests to improve clarity
      and make orientation in tests faster when reading them
      as documentation
    - add more documentation strings to store functions
    - move the test for updating `window.user` object to
      plugin.spec.js to make sure that `window.user` will
      be updated every time `setPolicies` mutation is called,
      and not only when `setPolicies` is called from `acceptPolicy`
      action

--- contentcuration/contentcuration/frontend/shared/vuex/policies/index.js
+++ contentcuration/contentcuration/frontend/shared/vuex/policies/index.js
@@ -4,6 +4,127 @@ import client from 'shared/client';
 
 const ACCEPT_ORDER = [policies.TERMS_OF_SERVICE, policies.PRIVACY];
 
+export const getters = {
+  /**
+   * Generate an object with information about policy, its version,
+   * and current datetime as user acceptance date
+   *
+   * @returns Object in the format
+   * { policy_year_month_day: day/month/year hour:minute' }
+   * where key is a policy + its version, and value is the current UTC datetime
+   */
+  getPolicyAcceptedData() {
+    return policyName => {
+      // Get current date string
+      const date = new Date();
+      // this data is what we send to the server,
+      // and converting it to a string makes it lose
+      // timezone data. Ensure that we standardize on
+      // UTC before sending that to the server.
+      // part of the fix for issue #2508 and #2317.
+      const day = ('0' + date.getUTCDate()).slice(-2);
+      const month = ('0' + (date.getUTCMonth() + 1)).slice(-2);
+      const year = String(date.getUTCFullYear()).slice(-2);
+      const hour = ('0' + (date.getUTCHours() + 1)).slice(-2);
+      const minute = ('0' + (date.getUTCMinutes() + 1)).slice(-2);
+      const dateStr = `${day}/${month}/${year} ${hour}:${minute}`;
+
+      // Get policy string
+      const policyDate = policyDates[policyName];
+      return { [createPolicyKey(policyName, policyDate)]: dateStr };
+    };
+  },
+  /**
+   * @returns A list of policy constants (e.g. policies.PRIVACY)
+   * that have not been signed by the user
+   */
+  nonAcceptedPolicies(state) {
+    return policyKeys
+      .filter(key => !state.policies[key])
+      .map(key =>
+        key
+          .split('_')
+          .slice(0, -3)
+          .join('_')
+      );
+  },
+  /**
+   * @returns `true` if a policy hasn't been
+   * accepted yet. Otherwise `false`.
+   * Always returns `false` for logged out users.
+   */
+  isPolicyUnaccepted(state, getters, rootState, rootGetters) {
+    return function(policy) {
+      if (!rootGetters.loggedIn) {
+        return false;
+      }
+
+      return getters.unacceptedPolicies.includes(policy);
+    };
+  },
+  selectedPolicy(state) {
+    return state.selectedPolicy;
+  },
+  /**
+   * @returns There can be more unaccepted policies.
+   * This getter returns the first of them when sorted
+   * by predefined order (see ACCEPT_ORDER).
+   * Always returns `null` for logged out users.
+   */
+  firstUnacceptedPolicy(state, getters, rootState, rootGetters) {
+    // acceptance information is only relevant to logged in users
+    if (!rootGetters.loggedIn) {
+      return null;
+    }
+    const unacceptedPolicies = getters.unacceptedPolicies;
+    if (unacceptedPolicies.length) {
+      return ACCEPT_ORDER.find(policy => unacceptedPolicies.includes(policy));
+    }
+  },
+};
+
+export const mutations = {
+  SET_SELECTED_POLICY(state, policy) {
+    state.selectedPolicy = policy;
+  },
+  SET_POLICIES(state, policies) {
+    for (let policy in policies) {
+      Vue.set(state.policies, policy, policies[policy]);
+    }
+  },
+};
+
+export const actions = {
+  setPolicies(context, policies) {
+    context.commit('SET_POLICIES', policies);
+  },
+  openPolicy(context, policy) {
+    context.commit('SET_SELECTED_POLICY', policy);
+  },
+  closePolicy(context, policy) {
+    if (context.state.selectedPolicy === policy) {
+      context.commit('SET_SELECTED_POLICY', null);
+    }
+  },
+  /**
+   * Accept a policy
+   *
+   * @param {Object} policyAcceptedData Object with information about policy,
+   * its version, and user acceptance date in the format
+   * { policy_year_month_day: day/month/year hour:minute' }
+   * @returns {Promise}
+   */
+  acceptPolicy(context, policyAcceptedData) {
+    return client
+      .post(window.Urls.policy_update(), { policy: JSON.stringify(policyAcceptedData) })
+      .then(() => {
+        return context
+          .dispatch('setPolicies', policyAcceptedData)
+          .then(() => context.dispatch('closePolicy'));
+      });
+  },
+};
+
 export default {
   namespaced: true,
   state() {
@@ -12,95 +133,7 @@ export default {
       selectedPolicy: null,
     };
   },
-  getters: {
-    getPolicyAcceptedData() {
-      return policyName => {
-        // what should the format be?
-        // {policy_name}_{year}_{month}_{day}
-        // Get current date string
-        const date = new Date();
-        // this data is what we send to the server,
-        // and converting it to a string makes it lose
-        // timezone data. Ensure that we standardize on
-        // UTC before sending that to the server.
-        // part of the fix for issue #2508 and #2317.
-        const day = ('0' + date.getUTCDate()).slice(-2);
-        const month = ('0' + (date.getUTCMonth() + 1)).slice(-2);
-        const year = String(date.getUTCFullYear()).slice(-2);
-        const hour = ('0' + (date.getUTCHours() + 1)).slice(-2);
-        const minute = ('0' + (date.getUTCMinutes() + 1)).slice(-2);
-        const dateStr = `${day}/${month}/${year} ${hour}:${minute}`;
-
-        // Get policy string
-        const policyDate = policyDates[policyName];
-        return { [createPolicyKey(policyName, policyDate)]: dateStr };
-      };
-    },
-    // returns a list of policy constants (e.g. policies.PRIVACY)
-    // that have not been signed by the user.
-    nonAcceptedPolicies(state) {
-      return policyKeys
-        .filter(key => !state.policies[key])
-        .map(key =>
-          key
-            .split('_')
-            .slice(0, -3)
-            .join('_')
-        );
-    },
-    isPolicyUnaccepted(state, getters, rootState, rootGetters) {
-      return function(policy) {
-        if (!rootGetters.loggedIn) {
-          return false;
-        }
-
-        return getters.nonAcceptedPolicies.includes(policy);
-      };
-    },
-    selectedPolicy(state) {
-      return state.selectedPolicy;
-    },
-    firstUnacceptedPolicy(state, getters, rootState, rootGetters) {
-      // acceptance information is only relevant to logged in users
-      if (!rootGetters.loggedIn) {
-        return null;
-      }
-      const nonAcceptedPolicies = getters.nonAcceptedPolicies;
-      if (nonAcceptedPolicies.length) {
-        return ACCEPT_ORDER.find(policy => nonAcceptedPolicies.includes(policy));
-      }
-    },
-  },
-  actions: {
-    setPolicies(context, policies) {
-      context.commit('SET_POLICIES', policies);
-    },
-    openPolicy(context, policy) {
-      context.commit('SET_SELECTED_POLICY', policy);
-    },
-    closePolicy(context, policy) {
-      if (context.state.selectedPolicy === policy) {
-        context.commit('SET_SELECTED_POLICY', null);
-      }
-    },
-    acceptPolicy(context, policyAcceptedData) {
-      return client
-        .post(window.Urls.policy_update(), { policy: JSON.stringify(policyAcceptedData) })
-        .then(() => {
-          return context
-            .dispatch('setPolicies', policyAcceptedData)
-            .then(() => context.dispatch('closePolicy'));
-        });
-    },
-  },
-  mutations: {
-    SET_SELECTED_POLICY(state, policy) {
-      state.selectedPolicy = policy;
-    },
-    SET_POLICIES(state, policies) {
-      for (let policy in policies) {
-        Vue.set(state.policies, policy, policies[policy]);
-      }
-    },
-  },
+  getters,
+  actions,
+  mutations,
 };
--- contentcuration/contentcuration/frontend/shared/vuex/policies/index.spec.js
+++ contentcuration/contentcuration/frontend/shared/vuex/policies/index.spec.js
@@ -1,83 +1,109 @@
-import transform from 'lodash/transform';
-import storeFactory from 'shared/vuex/baseStore';
+import { actions, getters } from './index';
 import { policies, policyDates } from 'shared/constants';
 import client from 'shared/client';
 
 jest.mock('shared/client');
-jest.mock('shared/vuex/connectionPlugin');
 
 describe('policies store', () => {
-  let store;
-  beforeEach(() => {
-    window.user = {};
-    store = storeFactory();
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 
-  describe('nonAcceptedPolicies getter', () => {
-    it('should return empty array if all policies have been accepted', () => {
-      const allPolicies = transform(
-        policyDates,
-        (result, value, key) => {
-          const policyKey = `${key}_${value.getUTCFullYear()}_${value.getUTCMonth() +
-            1}_${value.getUTCDate()}`;
-          return (result[policyKey] = '01/01/2000 12:12');
-        },
-        {}
-      );
-      store.state.policies = {
-        policies: allPolicies,
-        selectedPolicy: null,
-      };
+  describe('getters', () => {
+    describe('nonAcceptedPolicies', () => {
+      it('should return empty array if all policies have been accepted', () => {
+        const state = {
+          policies: {
+            privacy_policy_2020_12_10: '01/01/2000 12:12',
+            terms_of_service_2020_12_10: '01/01/2000 12:12',
+            community_standards_2020_8_30: '01/01/2000 12:12',
+          },
+        };
 
-      expect(store.getters['policies/nonAcceptedPolicies']).toEqual([]);
-    });
-    it('should return array of all policies that still need to be accepted', () => {
-      expect(store.getters['policies/nonAcceptedPolicies']).toEqual(Object.values(policies));
-    });
-    it('should return array of policies that have been updated since they were last accepted', () => {
-      const testTOSPolicyData = {
-        [`${policies.TERMS_OF_SERVICE}_1900_2_2`]: '01/01/2000 12:12',
-      };
-      store.commit('policies/SET_POLICIES', testTOSPolicyData);
-      expect(store.getters['policies/nonAcceptedPolicies']).toContain(policies.TERMS_OF_SERVICE);
-    });
-  });
-  describe('getPolicyAcceptedData getter', () => {
-    it('should return data in the format {policy_year_month_date: day/month/year hour:minute', () => {
-      const testKey = policies.PRIVACY;
-      const date = policyDates[testKey];
-      const now = new Date();
-      const day = ('0' + now.getUTCDate()).slice(-2);
-      const month = ('0' + (now.getUTCMonth() + 1)).slice(-2);
-      const year = String(now.getUTCFullYear()).slice(-2);
-      const hour = ('0' + (now.getUTCHours() + 1)).slice(-2);
-      const minute = ('0' + (now.getUTCMinutes() + 1)).slice(-2);
+        expect(getters.nonAcceptedPolicies(state)).toEqual([]);
+      });
 
-      const expectedKey = `${testKey}_${date.getUTCFullYear()}_${date.getUTCMonth() +
-        1}_${date.getUTCDate()}`;
-      const expectedValue = `${day}/${month}/${year} ${hour}:${minute}`;
-      expect(store.getters['policies/getPolicyAcceptedData'](policies.PRIVACY)).toEqual({
-        [expectedKey]: expectedValue,
+      it('should return array of all policies that still need to be accepted', () => {
+        const state = {
+          policies: {
+            privacy_policy_2020_12_10: undefined,
+            terms_of_service_2020_12_10: undefined,
+            community_standards_2020_8_30: '01/01/2000 12:12',
+          },
+        };
+
+        expect(getters.nonAcceptedPolicies(state)).toEqual(['privacy_policy', 'terms_of_service']);
+      });
+
+      it('should return array of policies that have been updated since they were last accepted', () => {
+        const state = {
+          policies: {
+            terms_of_service_1900_2_2: '01/01/1901 12:12',
+            terms_of_service_2020_12_10: undefined,
+            privacy_policy_1900_2_2: '01/01/1901 12:12',
+            privacy_policy_2020_12_10: '01/01/2021 12:12',
+            community_standards_1900_2_2: '01/01/1901 12:12',
+            community_standards_2020_8_30: '01/01/2021 12:12',
+          },
+        };
+
+        expect(getters.nonAcceptedPolicies(state)).toEqual(['terms_of_service']);
       });
     });
-  });
-  describe('acceptPolicy action', () => {
-    const testData = { testpolicy: 'test' };
-    it('should call policy_update endpoint', () => {
-      client.post.mockResolvedValue(Promise.resolve());
-      return store.dispatch('policies/acceptPolicy', testData).then(() => {
-        expect(client.post.mock.calls[0][0]).toBe('policy_update');
-        expect(client.post.mock.calls[0][1]).toEqual({ policy: JSON.stringify(testData) });
-      });
-    });
-    it('should update window.user with the new data', () => {
-      window.user.policies = { policy: 'signed' };
-      client.post.mockResolvedValue(Promise.resolve());
-      return store.dispatch('policies/acceptPolicy', testData).then(() => {
-        expect(window.user.policies).toEqual({
-          policy: 'signed',
-          ...testData,
+
+    describe('getPolicyAcceptedData', () => {
+      it(`should return data in the format {policy_year_month_day: day/month/year hour:minute
+        where key is a policy and value is the current UTC datetime`, () => {
+        // an example of the expected object:
+        // { privacy_policy_2020_12_10: '30/04/21 16:45' }
+
+        const testKey = policies.PRIVACY;
+        const date = policyDates[testKey];
+        const now = new Date();
+        const day = ('0' + now.getUTCDate()).slice(-2);
+        const month = ('0' + (now.getUTCMonth() + 1)).slice(-2);
+        const year = String(now.getUTCFullYear()).slice(-2);
+        const hour = ('0' + (now.getUTCHours() + 1)).slice(-2);
+        const minute = ('0' + (now.getUTCMinutes() + 1)).slice(-2);
+
+        const expectedKey = `${testKey}_${date.getUTCFullYear()}_${date.getUTCMonth() +
+          1}_${date.getUTCDate()}`;
+        const expectedValue = `${day}/${month}/${year} ${hour}:${minute}`;
+
+        expect(getters.getPolicyAcceptedData()(policies.PRIVACY)).toEqual({
+          [expectedKey]: expectedValue,
         });
+      });
+    });
+  });
+
+  describe('actions', () => {
+    describe('acceptPolicy', () => {
+      let dispatch;
+
+      beforeEach(() => {
+        client.post.mockResolvedValue(Promise.resolve());
+        dispatch = jest.fn().mockResolvedValue();
+      });
+
+      it('should call policy_update endpoint', async () => {
+        const policyAcceptedData = { privacy_policy_2020_12_10: '12/05/2021 09:56' };
+        await actions.acceptPolicy({ getters, dispatch }, policyAcceptedData);
+
+        expect(client.post.mock.calls.length).toBe(1);
+        expect(client.post.mock.calls[0][0]).toBe('policy_update');
+        expect(client.post.mock.calls[0][1]).toEqual({
+          policy: JSON.stringify(policyAcceptedData),
+        });
+      });
+
+      it('should call setPolicies with the new data and closePolicy', async () => {
+        const policyAcceptedData = { privacy_policy_2020_12_10: '12/05/2021 09:56' };
+        await actions.acceptPolicy({ getters, dispatch }, policyAcceptedData);
+
+        expect(dispatch.mock.calls.length).toBe(2);
+        expect(dispatch.mock.calls[0]).toEqual(['setPolicies', policyAcceptedData]);
+        expect(dispatch.mock.calls[1]).toEqual(['closePolicy']);
       });
     });
   });
