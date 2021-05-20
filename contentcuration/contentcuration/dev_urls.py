6fd93921974e1e34489c2ab1eb1a6035828c6c6d
Richard Tibbles <richard@learningequality.org>
2021-05-19 17:31:23 -0700

    Update dev urls for Django 3.

--- contentcuration/contentcuration/dev_urls.py
+++ contentcuration/contentcuration/dev_urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
-from django.conf.urls import include
-from django.conf.urls import url
 from django.contrib import admin
 from django.http.response import HttpResponseRedirect
+from django.urls import include
+from django.urls import path
+from django.urls import re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -31,33 +32,33 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = urlpatterns + [
-    url(r"^__open-in-editor/", webpack_redirect_view),
-    url(r'^admin/', include(admin.site.urls)),
-    url(
+    re_path(r"^__open-in-editor/", webpack_redirect_view),
+    path('admin/', admin.site.urls),
+    re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
-    url(
+    re_path(
         r"^api_explorer/$",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    url(
+    re_path(
         r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
-    url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    url(
+    re_path(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    re_path(
         r"^" + settings.STORAGE_URL[1:] + "(?P<path>.*)$",
         file_views.debug_serve_file,
         name="debug_serve_file",
     ),
-    url(
+    re_path(
         r"^" + settings.CONTENT_DATABASE_URL[1:] + "(?P<path>.*)$",
         file_views.debug_serve_content_database_file,
         name="content_database_debug_serve_file",
     ),
-    url(
+    re_path(
         r"^" + settings.CSV_URL[1:] + "(?P<path>.*)$",
         file_views.debug_serve_file,
         name="csv_debug_serve_file",
@@ -68,4 +69,4 @@ if getattr(settings, "DEBUG_PANEL_ACTIVE", False):
 
     import debug_toolbar
 
-    urlpatterns = [url(r"^__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns = [re_path(r"^__debug__/", include(debug_toolbar.urls))] + urlpatterns
