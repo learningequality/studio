3ea43cf9ef3881d6e4a169a4f8b62e693a9c9387
sairina <sairina.merinotsui@gmail.com>
2021-09-01 21:48:59 -0700

    WIP

--- contentcuration/contentcuration/frontend/channelEdit/components/edit/CategoryDropdown.vue
+++ contentcuration/contentcuration/frontend/channelEdit/components/edit/CategoryDropdown.vue
@@ -2,22 +2,28 @@
 
   <div>
     <VCombobox
-      v-model="search"
-      :label="$tr('categoryLabel')"
+      v-model="value"
       :items="items"
+      :label="$tr('categoryLabel')"
       box
       chips
       multiple
       deletableChips
-      maxlength="30"
     >
-      <template v-slot:items="{ items }">
+      <template v-slot:no-data>
+        <VListTile v-if="categoryText && categoryText.trim()">
+          <VListTileContent>
+            <VListTileTitle>
+              {{ $tr('noCategoriesFoundText', { text: categoryText.trim() }) }}
+            </VListTileTitle>
+          </VListTileContent>
+        </VListTile>
+      </template>
+
+      <template v-slot:item="{ index, item }">
         <VTreeview
-          selectable
           :items="item"
-          :search="search"
-          :filter="filter"
-          :open.sync="open"
+          selectable
         />
       </template>
     </VCombobox>
@@ -33,64 +39,116 @@
     props: {},
     data() {
       return {
-        open: [16, 3],
-        search: null,
+        //   category: [],
+        //   items: [
+        //     [{
+        //       id: 16,
+        //       name: 'For school',
+        //       children: [
+        //         [{
+        //           id: 2,
+        //           name: 'Mathematics',
+        //           children: [
+        //             {
+        //               id: 201,
+        //               name: 'Arithmetic',
+        //             },
+        //             {
+        //               id: 202,
+        //               name: 'Algebra',
+        //             },
+        //             {
+        //               id: 203,
+        //               name: 'Geometry',
+        //             },
+        //             {
+        //               id: 204,
+        //               name: 'Calculus',
+        //             },
+        //             {
+        //               id: 205,
+        //               name: 'Statistics',
+        //             },
+        //           ],
+        //         }],
+        //         [{
+        //           id: 3,
+        //           name: 'Sciences',
+        //           children: [
+        //             {
+        //               id: 301,
+        //               name: 'Biology',
+        //             },
+        //             {
+        //               id: 302,
+        //               name: 'Chemistry',
+        //             },
+        //           ],
+        //         }],
+        //       ],
+        //     }],
+        //   ],
+        // };
         items: [
-          {
-            id: 16,
-            name: 'For school',
-            children: [
-              {
-                id: 2,
-                name: 'Mathematics',
-                children: [
-                  {
-                    id: 201,
-                    name: 'Arithmetic',
-                  },
-                  {
-                    id: 202,
-                    name: 'Algebra',
-                  },
-                  {
-                    id: 203,
-                    name: 'Geometry',
-                  },
-                  {
-                    id: 204,
-                    name: 'Calculus',
-                  },
-                  {
-                    id: 205,
-                    name: 'Statistics',
-                  },
-                ],
-              },
-              {
-                id: 3,
-                name: 'Sciences',
-                children: [
-                  {
-                    id: 301,
-                    name: 'Biology',
-                  },
-                  {
-                    id: 302,
-                    name: 'Chemistry',
-                  },
-                ],
-              },
-            ],
-          },
+          [
+            {
+              id: 1,
+              name: 'For school :',
+              children: [
+                {
+                  id: 2,
+                  name: 'Calendar : app',
+                },
+                {
+                  id: 3,
+                  name: 'Chrome : app',
+                },
+                {
+                  id: 4,
+                  name: 'Webstorm : app',
+                },
+              ],
+            },
+          ],
+          [
+            {
+              id: 5,
+              name: 'Documents :',
+              children: [
+                {
+                  id: 6,
+                  name: 'vuetify :',
+                  children: [
+                    {
+                      id: 7,
+                      name: 'src :',
+                      children: [
+                        {
+                          id: 8,
+                          name: 'index : ts',
+                        },
+                        {
+                          id: 9,
+                          name: 'bootstrap : ts',
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
         ],
+        value: [],
+        categoryText: '',
       };
     },
     computed: {
-      filter() {
-        return this.caseSensitive
-          ? (item, search, textKey) => item[textKey].indexOf(search) > -1
-          : undefined;
-      },
+      // filter() {
+      //   return this.caseSensitive
+      //     ? (item, search, textKey) => item[textKey].indexOf(search) > -1
+      //     : undefined;
+      // },
     },
     $trs: {
       categoryLabel: 'Category',
