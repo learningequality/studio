4777abba320801dec1d9cbe230bb751d43f8c2e4
Blaine Jester <blainesworld@gmail.com>
2022-03-22 08:39:33 -0700

    Update completion criteria validator after breaking out mastery schema

--- contentcuration/contentcuration/constants/completion_criteria.py
+++ contentcuration/contentcuration/constants/completion_criteria.py
@@ -1,7 +1,24 @@
-import jsonschema
 from django.core.exceptions import ValidationError
+from jsonschema import RefResolver
+from jsonschema.exceptions import best_match
+from jsonschema.validators import validator_for
 from le_utils.constants import completion_criteria
 from le_utils.constants import content_kinds
+from le_utils.constants import mastery_criteria
+
+
+def _build_validator():
+    """
+    Builds the validator, once, for the completion criteria schema and includes the external mastery criteria schema
+    :rtype: jsonschema.Draft7Validator|jsonschema.validators.Validator
+    """
+    cls = validator_for(completion_criteria.SCHEMA)
+    validator = cls(completion_criteria.SCHEMA)
+    validator.resolver.store.update(RefResolver.from_schema(mastery_criteria.SCHEMA).store)
+    return validator
+
+
+validator = _build_validator()
 
 
 def validate(data, kind=None):
@@ -14,10 +31,9 @@ def validate(data, kind=None):
     if isinstance(data, (dict,)) and not data:
         return
 
-    try:
-        jsonschema.validate(instance=data, schema=completion_criteria.SCHEMA)
-    except jsonschema.ValidationError as e:
-        raise ValidationError("Completion criteria does not conform to schema") from e
+    error = best_match(validator.iter_errors(data))
+    if error is not None:
+        raise ValidationError("Completion criteria does not conform to schema") from error
 
     model = data.get("model")
     if kind is None or model is None:
