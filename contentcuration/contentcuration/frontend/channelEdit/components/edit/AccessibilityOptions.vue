6933124b345f74c3c7018382f742ce8cf80364b7
sairina <sairina.merinotsui@gmail.com>
2022-03-30 16:29:41 -0700

    Fixed strings/translations for accessibility options

--- contentcuration/contentcuration/frontend/channelEdit/components/edit/AccessibilityOptions.vue
+++ contentcuration/contentcuration/frontend/channelEdit/components/edit/AccessibilityOptions.vue
@@ -27,9 +27,11 @@
 
 <script>
 
+  import { camelCase } from 'lodash';
   import { AccessibilityCategories, AccessibilityCategoriesMap } from 'shared/constants';
   import Checkbox from 'shared/views/form/Checkbox';
   import HelpTooltip from 'shared/views/HelpTooltip';
+  import { constantsTranslationMixin, metadataTranslationMixin } from 'shared/mixins';
 
   export default {
     name: 'AccessibilityOptions',
@@ -37,6 +39,7 @@
       Checkbox,
       HelpTooltip,
     },
+    mixins: [constantsTranslationMixin, metadataTranslationMixin],
     props: {
       kind: {
         type: String,
@@ -64,20 +67,20 @@
           .filter(key => AccessibilityCategoriesMap[this.kind].includes(key))
           .map(key => {
             return {
-              label: key,
+              label: this.translateMetadataString(camelCase(key)),
               value: AccessibilityCategories[key],
-              help: key,
+              help: camelCase(key),
             };
           });
       },
     },
     $trs: {
       /* eslint-disable kolibri/vue-no-unused-translations */
-      ALT_TEXT: `Alternative text is provided for visual content (e.g., via the HTML alt attribute).`,
-      AUDIO_DESCRIPTION: `Audio descriptions are available (e.g., via an HTML5 track element with kind="descriptions")`,
-      HIGH_CONTRAST: `Content meets the visual contrast threshold set out in WCAG Success Criteria 1.4.6`,
-      SIGN_LANGUAGE: `Synchronized sign language intepretation is available for audio and video content. The value may be extended by adding an ISO 639 sign language code. For example, /sgn-en-us for American Sign Language.`,
-      TAGGED_PDF: `The structures in a PDF have been tagged to improve the navigation of the content.`,
+      altText: `Alternative text is provided for visual content (e.g., via the HTML alt attribute).`,
+      audioDescription: `Audio descriptions are available (e.g., via an HTML5 track element with kind="descriptions")`,
+      highContrast: `Content meets the visual contrast threshold set out in WCAG Success Criteria 1.4.6`,
+      signLanguage: `Synchronized sign language intepretation is available for audio and video content. The value may be extended by adding an ISO 639 sign language code. For example, /sgn-en-us for American Sign Language.`,
+      taggedPdf: `The structures in a PDF have been tagged to improve the navigation of the content.`,
       /* eslint-enable kolibri/vue-no-unused-translations */
     },
   };
