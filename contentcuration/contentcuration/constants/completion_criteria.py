6952d7fc7732cee5d1e77e2d46267c27edd965ef
Blaine Jester <blainesworld@gmail.com>
2022-04-08 14:03:12 -0700

    Expand on error descriptions

--- contentcuration/contentcuration/constants/completion_criteria.py
+++ contentcuration/contentcuration/constants/completion_criteria.py
@@ -1,6 +1,5 @@
 from django.core.exceptions import ValidationError
 from jsonschema import RefResolver
-from jsonschema.exceptions import best_match
 from jsonschema.validators import validator_for
 from le_utils.constants import completion_criteria
 from le_utils.constants import content_kinds
@@ -31,9 +30,24 @@ def validate(data, kind=None):
     if isinstance(data, (dict,)) and not data:
         return
 
-    error = best_match(validator.iter_errors(data))
-    if error is not None:
-        raise ValidationError("Completion criteria does not conform to schema") from error
+    error_descriptions = []
+    # @see https://python-jsonschema.readthedocs.io/en/latest/errors/
+    for error in validator.iter_errors(data):
+        if error.cause:
+            # documentation says this will only be set on FormatChecker errors
+            error_descriptions.append(error.cause)
+        elif error.absolute_path:
+            # if there's a path to a field, we can give a specific error
+            json_path = ".".join(error.absolute_path)
+            error_descriptions.append(ValidationError("{} {}".format(json_path, error.message)))
+        else:
+            # without a path, likely top-level validation error, e.g. `anyOf` conditions
+            error_descriptions.append(ValidationError("object doesn't satisfy '{}' conditions".format(error.validator)))
+
+    if error_descriptions:
+        e = ValidationError("Completion criteria doesn't conform to schema")
+        e.error_list.extend(error_descriptions)
+        raise e
 
     model = data.get("model")
     if kind is None or model is None:
