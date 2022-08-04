ce36717bd434935d172b3069808b1ea4243944e7
Blaine Jester <blainesworld@gmail.com>
2022-08-04 15:07:38 -0700

    Remove get_title func no longer needed

--- contentcuration/contentcuration/viewsets/contentnode.py
+++ contentcuration/contentcuration/viewsets/contentnode.py
@@ -469,11 +469,6 @@ def retrieve_thumbail_src(item):
     return None
 
 
-def get_title(item):
-    # If it's the root, use the channel name (should be original channel name)
-    return item["title"] if item["parent_id"] else item["original_channel_name"]
-
-
 def consolidate_extra_fields(item):
     extra_fields = item.get("extra_fields")
     if item["kind"] == content_kinds.EXERCISE:
