d2d19210b9d43eb1e4478b45d65d1bb280c99b01
Blaine Jester <blainesworld@gmail.com>
2022-11-30 07:13:53 -0800

    Add helpers to celery app class for production intervention

--- contentcuration/contentcuration/utils/celery/app.py
+++ contentcuration/contentcuration/utils/celery/app.py
@@ -45,6 +45,46 @@ class CeleryApp(Celery):
 
         return decoded_tasks
 
+    def count_queued_tasks(self, queue_name="celery"):
+        """
+        :param queue_name: The queue name, defaults to the default "celery" queue
+        :return: int
+        """
+        with self.pool.acquire(block=True) as conn:
+            count = conn.default_channel.client.llen(queue_name)
+        return count
+
+    def _revoke_matching(self, tasks, task_name, matching_kwargs=None):
+        count = 0
+        for task in tasks:
+            if task['name'] == task_name and (matching_kwargs is None or task['kwargs'] == matching_kwargs):
+                print('Revoking task {}'.format(task))
+                count += 1
+                self.control.revoke(task['id'], terminate=True)
+        return count
+
+    def revoke_reserved_tasks_by_name(self, task_name, matching_kwargs=None):
+        """
+        :param task_name: The string name of the task
+        :param matching_kwargs: A dict of kwargs to match to the task
+        :return: The count of revoked tasks
+        """
+        count = 0
+        for worker_name, tasks in self.control.inspect().reserved().items():
+            count += self._revoke_matching(tasks, task_name, matching_kwargs)
+        return count
+
+    def terminate_active_tasks_by_name(self, task_name, matching_kwargs=None):
+        """
+        :param task_name: The string name of the task
+        :param matching_kwargs: A dict of kwargs to match to the task
+        :return: The count of terminated tasks
+        """
+        count = 0
+        for worker_name, tasks in self.control.inspect().active().items():
+            count += self._revoke_matching(tasks, task_name, matching_kwargs)
+        return count
+
     def decode_result(self, result, status=None):
         """
         Decodes the celery result, like the raw result from the database, using celery tools
