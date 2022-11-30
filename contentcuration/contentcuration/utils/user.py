b6e08bab724c095e9e9b42d728855a87dab011c9
Blaine Jester <blainesworld@gmail.com>
2022-11-30 07:12:49 -0800

    Only enqueue storage calculation task for non-admins

--- contentcuration/contentcuration/utils/user.py
+++ contentcuration/contentcuration/utils/user.py
@@ -16,6 +16,7 @@ def calculate_user_storage(user_id):
         if user_id is None:
             raise User.DoesNotExist
         user = User.objects.get(pk=user_id)
-        calculate_user_storage_task.fetch_or_enqueue(user, user_id=user_id)
+        if not user.is_admin:
+            calculate_user_storage_task.fetch_or_enqueue(user, user_id=user_id)
     except User.DoesNotExist:
         logging.error("Tried to calculate user storage for user with id {} but they do not exist".format(user_id))
