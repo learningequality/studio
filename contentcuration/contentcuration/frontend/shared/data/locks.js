fd90b2671f7fa988a1bf2d5be428ed481da28663
Blaine Jester <blainesworld@gmail.com>
2023-06-13 14:26:50 -0700

    Track client support for web locks API

--- contentcuration/contentcuration/frontend/shared/data/locks.js
+++ contentcuration/contentcuration/frontend/shared/data/locks.js
@@ -1,3 +1,5 @@
+import logging from 'shared/logging';
+
 /**
  * Acquire an exclusive lock on the given name using the browser's Web Locks API,
  * and then run the given async function.
@@ -18,6 +20,9 @@ export function acquireLock({ name, exclusive = false }, asyncFunction) {
       },
       asyncFunction
     );
+  } else {
+    // Track if clients aren't supporting the Web Locks API
+    logging.error(new Error('Web Locks API not supported by browser'));
   }
   // Studio's supported browsers should support Web Locks but don't outright fail otherwise
   return asyncFunction();
