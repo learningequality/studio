b31818c7d42ca6514421ea85291fb4fa482fc3f3
akash5100 <shivamvermashs@gmail.com>
2023-07-18 16:23:10 +0530

    Refactor dispatch actions

--- contentcuration/contentcuration/frontend/shared/data/resources.js
+++ contentcuration/contentcuration/frontend/shared/data/resources.js
@@ -1156,7 +1156,7 @@ export const CaptionCues = new Resource({
         return pageData ? pageData : data;
       });
     });
-    this._requests[queryString] = {
+    this._requests[generatedUrl] = {
       [LAST_FETCHED]: now,
       promise,
     };
