6b7c4560c08f23ed40adb27fc8315eca32f31f0a
akash5100 <shivamvermashs@gmail.com>
2023-07-18 16:41:43 +0530

    fix linting errors

--- contentcuration/contentcuration/frontend/channelEdit/vuex/caption/getters.js
+++ contentcuration/contentcuration/frontend/channelEdit/vuex/caption/getters.js
@@ -1,11 +1,3 @@
-// Get the caption for the given id?
-
-// used to access and compute derived state from the Vuex store
-
-// They allow you to retrieve and manipulate data from the state without modifying it directly
-
-// are accessed as properties in components using the mapGetters helper or directly from the store using the store.getters syntax.
-
 // export function getCaptionFiles(state) {
 //   return Object.values(state.captionFilesMap);
 // }
