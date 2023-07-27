099963a4badeab8710c699fdcab32b806346c580
akash5100 <shivamvermashs@gmail.com>
2023-07-27 22:46:37 +0530

    Stage changes before rebase

--- contentcuration/contentcuration/frontend/channelEdit/components/CaptionsTab/CaptionsTab.vue
+++ contentcuration/contentcuration/frontend/channelEdit/components/CaptionsTab/CaptionsTab.vue
@@ -20,7 +20,7 @@
 
 <script>
   import { mapState, mapActions, mapGetters } from 'vuex';
-  import { uuid4 } from 'shared/data/resources'
+  import { CAPTIONS_LANGUAGES as LANGUAGES } from './languages';
 
   export default {
     name: 'CaptionsTab',
@@ -35,22 +35,14 @@
       return {
         message: 'Caption Editor',
         selectedLanguage: 'default',
-        languages: {
-          "en": "en",
-          "hn": "hn"
-        }
+        languages: LANGUAGES,
       };
     },
     computed: {
       ...mapState('caption', ['captionFilesMap', 'captionCuesMap']),
       ...mapGetters('file', ['getContentNodeFiles']),
       noCaptions() {
-        return (
-          this.captionFilesMap[this.nodeId] && 
-          this.captionFilesMap[this.nodeId].length > 0 
-            ? false
-            : true
-          );
+        return !this.captionFilesMap[this.nodeId] || this.captionFilesMap[this.nodeId].length === 0;
       },
     },
     methods: {
@@ -61,16 +53,15 @@
       addCaption() {
         // TODO: select the `file` with longest duration as recommended by @bjester.
         // For now just create captionFile object with default language 'en'.
-        const captionFile = {
-          id: uuid4(),
-          file_id: this.getContentNodeFiles(this.nodeId)[0].id,
-          language: this.selectedLanguage === 'default' ? 'en': this.selectedLanguage
-        }
-        this.addCaptionFile({ nodeId: this.nodeId, captionFile: captionFile }); 
+        const fileId = this.getContentNodeFiles(this.nodeId)[0].id;
+        const language = this.selectedLanguage === 'default' ? 'en': this.selectedLanguage;
+
+        this.addCaptionFile({
+          file_id: fileId,
+          language,
+          nodeId: this.nodeId,
+        });
       },
-    },
-    created() {
-      console.log(this);
     }
   }
 
