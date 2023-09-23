9ce6cc637f26c622521c048f1be823fec45055aa
akash5100 <shivamvermashs@gmail.com>
2023-09-23 12:35:38 +0530

    Adds KIND check to upload Caption file

--- contentcuration/contentcuration/frontend/channelEdit/components/CaptionsEditor/CaptionsEditor.vue
+++ contentcuration/contentcuration/frontend/channelEdit/components/CaptionsEditor/CaptionsEditor.vue
@@ -7,17 +7,31 @@
 </template>
 
 <script>
+  import { mapState, mapActions } from 'vuex';
+  import FileUpload from '../../views/files/FileUpload.vue';
+
 
   export default {
+    name: 'ChannelEdit',
+    components: {
+      FileUpload
+    },
+    props: {},
     data() {
       return {
         message: 'Caption editor!',
       };
     },
-    mounted() {
-      this.$store.dispatch('captionFiles/loadCaptionFiles');
+    computed: {
+      ...mapState('caption', ['captionFilesMap', 'captionCuesMap']),
     },
-  };
+    methods: {},
+    created() {
+      // console.log(this.captionFilesMap);
+      // this.loadCaptionCues(Object.keys(this.captionFilesMap));
+    },
+    beforeRouteEnter(to, from, next) {}
+  }
 
 </script>
 
