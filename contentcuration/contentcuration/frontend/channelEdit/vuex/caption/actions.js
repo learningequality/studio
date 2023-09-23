fa6752266ee87476ec5a0fd338249529baa36b80
akash5100 <shivamvermashs@gmail.com>
2023-09-23 13:48:24 +0530

    Refactor actions dispatch and create Vuex state mapping

--- contentcuration/contentcuration/frontend/channelEdit/vuex/caption/actions.js
+++ contentcuration/contentcuration/frontend/channelEdit/vuex/caption/actions.js
@@ -73,3 +73,21 @@ export async function addCaptionFile({ state, commit }, { id, file_id, language,
     });
   }
 }
+
+export async function loadCaptions({ commit, rootGetters }, params) {
+    const AI_FEATURE_FLAG = rootGetters['currentChannel/isAIFeatureEnabled']
+    if(!AI_FEATURE_FLAG) return;
+
+    const FILE_TYPE = rootGetters['contentNode/getContentNode'](params.contentnode_id).kind;
+    if(FILE_TYPE === 'video' || FILE_TYPE === 'audio') {
+        const captionFiles = await loadCaptionFiles(commit, params);
+        // If there is no Caption File for this contentnode
+        // Don't request for the cues
+        if(captionFiles.length === 0) return;
+        // TODO: call loadCaptionCues -> to be done after I finish saving captionFiles in indexedDB When CTA is called. So I have captions saved in the backend.
+    }
+}
+
+export async function addCaptionFile({ commit }, { captionFile, nodeId }) {
+    commit('ADD_CAPTIONFILE', { captionFile, nodeId });
+}
