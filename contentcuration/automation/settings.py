b397f3de82af0e853eff5ef4511bc6d73f891576
akash5100 <shivamvermashs@gmail.com>
2023-09-26 18:08:28 +0530

    changes enum to dict

--- contentcuration/automation/settings.py
+++ contentcuration/automation/settings.py
@@ -1,22 +1,21 @@
-from enum import Enum
-
 from torch.cuda import is_available as is_gpu_available
 
 DEVICE = "cuda:0" if is_gpu_available() else "cpu"
 
 
 # [TRANSCRIPTION GENERATION]
-class WhisperModel(Enum):
-    TINY = "openai/whisper-tiny"
-    BASE = "openai/whisper-base"
-    SMALL = "openai/whisper-small"
-    MEDIUM = "openai/whisper-medium"
-    LARGE = "openai/whisper-large"
-    LARGEV2 = "openai/whisper-large-v2"
+WHISPER_MODELS = dict(
+    TINY="openai/whisper-tiny",
+    BASE="openai/whisper-base",
+    SMALL="openai/whisper-small",
+    MEDIUM="openai/whisper-medium",
+    LARGE="openai/whisper-large",
+    LARGEV2="openai/whisper-large-v2",
+)
 
 
-DEV_TRANSCRIPTION_MODEL = WhisperModel.TINY
-TRANSCRIPTION_MODEL = WhisperModel.TINY
+DEV_TRANSCRIPTION_MODEL = WHISPER_MODELS['TINY']
+TRANSCRIPTION_MODEL = WHISPER_MODELS['TINY']
 
 # https://huggingface.co/docs/transformers/v4.29.1/en/generation_strategies#customize-text-generation
 MAX_TOKEN_LENGTH = 448
