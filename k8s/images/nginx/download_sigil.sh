4e568fbe16097af10b9a1094f6fc8bdba05023a7
Aron Asor <hello@aron.asorman.io>
2023-11-21 22:07:58 -0800

    fix: on download_sigil.sh, make sure we exit if any command

--- k8s/images/nginx/download_sigil.sh
+++ k8s/images/nginx/download_sigil.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -eou pipefail
+
 export SIGIL_VERSION=0.10.1
 export OS=`sh -c "uname -s | tr '[:upper:]' '[:lower:]'"`
 export ARCH=`sh -c "uname -m | tr '[:upper:]' '[:lower:]' | sed 's/aarch64/arm64/'"`
