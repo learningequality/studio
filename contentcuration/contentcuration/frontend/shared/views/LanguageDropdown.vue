9bafc01b2393844db047b8645ad20c2a714990ce
Surya Shukla <official.surya3jan@gmail.com>
2023-12-17 08:18:23 +0530

    lint test

--- contentcuration/contentcuration/frontend/shared/views/LanguageDropdown.vue
+++ contentcuration/contentcuration/frontend/shared/views/LanguageDropdown.vue
@@ -106,7 +106,7 @@
     },
     methods: {
       languageText(item) {
-        const firstNativeName = item.native_name.split(',')[0].trim()
+        const firstNativeName = item.native_name.split(',')[0].trim();
         return this.$tr('languageItemText', { language: firstNativeName, code: item.id });
       },
     },
