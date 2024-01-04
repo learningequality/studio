bf6dfd6db609fe2e554e15868df837eca57efc7c
Alex Velez <velezllaquealex@gmail.com>
2024-01-04 17:02:44 -0500

    Update resources language

--- contentcuration/contentcuration/frontend/channelEdit/components/QuickEditModal/EditLanguageModal.vue
+++ contentcuration/contentcuration/frontend/channelEdit/components/QuickEditModal/EditLanguageModal.vue
@@ -5,10 +5,21 @@
     :submitText="$tr('saveAction')"
     :cancelText="$tr('cancelAction')"
     data-test="edit-langugage"
+    :submitDisabled="!selectedLanguage"
     @submit="handleSave"
     @cancel="close"
   >
-    <div>
+    <p>
+      {{ $tr('resourcesSelected', { count: nodeIds.length }) }}
+    </p>
+    <KTextbox
+      autofocus
+      v-model="searchQuery"
+      data-test="search-input"
+      :label="$tr('selectLanguage')"
+      style="margin-top: 0.5em"
+    />
+    <div ref="languages" class="languages-options">
       <KRadioButton
         v-for="language in languageOptions"
         :key="language.id"
@@ -24,8 +35,9 @@
 
 <script>
 
-  import { mapGetters } from 'vuex';
+  import { mapGetters, mapActions } from 'vuex';
   import { LanguagesList } from 'shared/leUtils/Languages';
+  import { ContentKindsNames } from 'shared/leUtils/ContentKinds';
 
   export default {
     name: 'EditLanguageModal',
@@ -38,21 +50,58 @@
     data() {
       return {
         selectedLanguage: '',
+        searchQuery: '',
+        updateDescendants: false,
       };
     },
     computed: {
-      ...mapGetters('contentNode', ['getContentNodeChildren']),
-      languageOptions() {
-        return LanguagesList;
+      ...mapGetters('contentNode', ['getContentNodes']),
+      nodes() {
+        return this.getContentNodes(this.nodeIds);
       },
+      isTopicSelected() {
+        return this.nodes.some(node => node.kind === ContentKindsNames.TOPIC);
+      },
+      isMultipleNodesLanguages() {
+        const languages = new Set(
+          this.nodes
+            .map(node => node.language)
+            .filter(Boolean)
+        );
+        return languages.size > 1;
+      },
+      languageOptions() {
+        const searchQuery = this.searchQuery.trim().toLowerCase();
+        if (!this.searchQuery) {
+          return LanguagesList;
+        }
+        const criteria = ['id', 'native_name', 'readable_name'];
+        return LanguagesList.filter(lang => (
+          criteria.some(key => (
+            lang[key]?.toLowerCase().includes(searchQuery)
+          ))
+        ));
+      },
+    },
+    created() {
+      const languages = [...new Set(
+        this.nodes.map(node => node.language)
+      )];
+      if (languages.length === 1) {
+        this.selectedLanguage = languages[0] || '';
+      }
     },
     mounted() {
-      console.log('Aa', this.nodeIds, this.getContentNodeChildren(this.nodeIds[0]));
+      if (this.selectedLanguage) {
+        // Search for the selected KRadioButton and scroll to it
+        const selectedRadio = this.$refs.languages.querySelector(
+          `input[value="${this.selectedLanguage}"]`
+        );
+        selectedRadio?.scrollIntoView?.();
+      }
     },
-    methods: {
-      validateLanguage() {
-        return true;
-      },
+    methods: {  
+      ...mapActions('contentNode', ['updateContentNode']),
       languageText(langObject) {
         return this.$tr('languageItemText', {
           language: langObject.native_name,
@@ -62,10 +111,28 @@
       close() {
         this.$emit('close');
       },
-      handleSave() {
-        if (!this.validateLanguage()) {
+      async handleSave() {
+        if (!this.selectedLanguage) {
           return;
         }
+
+        await Promise.all(
+          this.nodes.map(node => {
+            if (
+              this.updateDescendants &&
+              node.kind === ContentKindsNames.TOPIC
+            ) { // will update with the new function to update all descendants
+              return this.updateContentNode({
+                id: node.id,
+                language: this.selectedLanguage,
+              });
+            }
+            return this.updateContentNode({
+              id: node.id,
+              language: this.selectedLanguage,
+            });
+          })
+        );
 
         this.$store.dispatch(
           'showSnackbarSimple',
@@ -81,7 +148,16 @@
       cancelAction: 'Cancel',
       editedLanguage:
         'Edited language for {count, number, integer} {count, plural, one {resource} other {resources}}',
+      selectLanguage: 'Select / Type Language',
+      resourcesSelected: '{count, number, integer} {count, plural, one {resource} other {resources}} selected',
     },
   };
 
 </script>
+
+<style scoped>
+  .languages-options {
+    height: 350px;
+    overflow-y: auto;
+  }
+</style>
