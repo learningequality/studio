d970a54e1174dd8a87b683cdffd6cf157ab20be3
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2024-03-15 11:43:39 +0300

    add more test

--- contentcuration/contentcuration/frontend/channelList/views/KCard/__test__/KCard.spec.js
+++ contentcuration/contentcuration/frontend/channelList/views/KCard/__test__/KCard.spec.js
@@ -1,24 +1,55 @@
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import KCard from './../index.vue';
 
 
-describe('KCard', () => {
-    it('renders horizontal layout with small thumbnail', () => {
-      const wrapper = mount(KCard, {
-        propsData: {
-          title: 'Sample Title',
-          headingLevel: 2,
-          to: { path: '/some-link' },
-          layout: 'horizontal',
-          thumbnailDisplay: 'small',
-          thumbnailSrc: 'https://example.com/image.jpg',
-        },
-      }); 
-  
-      expect(wrapper.find('[data-test="k-card"]').exists()).toBe(true);
-      expect(wrapper.find('h2').text()).toBe('Sample Title'); 
-      expect(wrapper.findAll('[slot="aboveTitle"]').length).toBe(1); 
-      expect(wrapper.findAll('[slot="title"]').length).toBe(1);
-      expect(wrapper.findAll('[slot="belowTitle"]').length).toBe(1);
+describe('index.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(KCard,{
+      propsData: {
+        title: 'Sample Title',
+        headingLevel: 2,
+        to: { path: '/some-link' },
+        layout: 'horizontal',
+        thumbnailDisplay: 'small',
+        thumbnailSrc: 'https://example.com/image.jpg',
+      },
+      methods: {cardFocus:()=>{}},
     });
   });
+
+  it('renders title', () => {
+    expect(wrapper.find('h2').exists()).toBe(false);
+  });
+
+  it('renders data on focus', () => {
+    let emittedOnFocus;
+    wrapper.vm.$on('focus',(data)=>{
+      emittedOnFocus = data;
+    });
+    expect(emittedOnFocus).toBeUndefined();
+  });
+
+  it('renders data on hover', () => {
+    let emittedOnHover;
+    wrapper.vm.$on('hover',(data)=>{
+      emittedOnHover = data;
+    });
+    expect(emittedOnHover).toBeUndefined();
+  });
+
+  it('renders image', () => {
+    expect(wrapper.find('img').exists()).toBe(false);
+  });
+
+  it('renders slotted content', () => {
+    expect(wrapper.find('[slot="aboveTitle"]').exists()).toBe(false);
+    expect(wrapper.find('[slot="title"]').exists()).toBe(false);
+    expect(wrapper.find('[slot="belowTitle"]').exists()).toBe(false);
+  });
+  
+
+
+  
+});
