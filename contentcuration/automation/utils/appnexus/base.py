89e314e62bd55d954dbd5acc2695c6e00d64a0cf
Alex Velez <velezllaquealex@gmail.com>
2024-04-01 08:48:17 -0500

    Base implementation

--- contentcuration/automation/utils/appnexus/base.py
+++ contentcuration/automation/utils/appnexus/base.py
@@ -1,8 +1,11 @@
 import time
+import logging
 import requests
 from abc import ABC
 from abc import abstractmethod
 from builtins import NotImplementedError
+
+from . import errors
 
 
 class SessionWithMaxConnectionAge(requests.Session):
@@ -32,17 +35,105 @@ class BackendRequest(object):
 
 class BackendResponse(object):
     """ Class that should be inherited by specific backend for its responses"""
-    pass
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
 
 class Backend(ABC):
     """ An abstract base class for backend interfaces that also implements the singleton pattern """
     _instance = None
+    session = None
+    base_url = None
+    connect_endpoint = None
 
-    def __new__(class_, *args, **kwargs):
+    def __new__(class_, *args, url_prefix="", **kwargs):
         if not isinstance(class_._instance, class_):
             class_._instance = object.__new__(class_, *args, **kwargs)
+            class_._instance.url_prefix = url_prefix
         return class_._instance
+
+    def __init__(self):
+        self.session = SessionWithMaxConnectionAge()
+
+    def _construct_full_url(self, path):
+        """This method should combine base_url, url_prefix, and path in that order, removing any trailing slashes beforehand."""
+        url_array = []
+        if self.base_url:
+            url_array.append(self.base_url.rstrip("/"))
+        if self.url_prefix:
+            url_array.append(self.url_prefix.rstrip("/"))
+        if path:
+            url_array.append(path.lstrip("/"))
+        return "/".join(url_array)
+
+    def _make_request(self, path, **kwargs):
+        url = self._construct_full_url(path)
+        is_retry = kwargs.pop("is_retry", False)
+        try: 
+            return self.session.request(url, **kwargs)
+        except (
+            requests.exceptions.ConnectionError,
+            requests.exceptions.RequestException,
+        ) as e:
+            if is_retry:
+                logging.error(str(e))
+                raise errors.ConnectionError("Connection error occurred. Retried request and failed again.")
+            logging.warning(f"Connection error occurred. Retrying request to {url}")
+            return self.make_request(path, is_retry=True, **kwargs)
+        except (
+            requests.exceptions.SSLError,
+        ) as e:
+            logging.error(str(e))
+            raise errors.ConnectionError(f"Unable to connect to {url}")
+        except (
+            requests.exceptions.Timeout,
+            requests.exceptions.ConnectTimeout,
+            requests.exceptions.ReadTimeout,
+        ) as e:
+            logging.error(str(e))
+            raise errors.TimeoutError(f"Timeout occurred while connecting to {url}")
+        except (
+            requests.exceptions.TooManyRedirects,
+            requests.exceptions.HTTPError,
+        ) as e:
+            logging.error(str(e))
+            raise errors.HttpError(f"HTTP error occurred while connecting to {url}")
+        except (
+            requests.exceptions.URLRequired,
+            requests.exceptions.MissingSchema,
+            requests.exceptions.InvalidSchema,
+            requests.exceptions.InvalidURL,
+            requests.exceptions.InvalidHeader,
+            requests.exceptions.InvalidJSONError,
+        ) as e:
+            logging.error(str(e))
+            raise errors.InvalidRequest(f"Invalid request to {url}")
+        except (
+            requests.exceptions.ContentDecodingError,
+            requests.exceptions.ChunkedEncodingError,
+        ) as e:
+            logging.error(str(e))
+            raise errors.InvalidResponse(f"Invalid response from {url}")
+    
+    def connect(self):
+        """ Establishes a connection to the backend service. """
+        try:
+            response = self.make_request(self.connect_endpoint, method="GET")
+            if response.status_code != 200:
+                return False
+            return True
+        except Exception as e:
+            return False
+
+    def make_request(self, path, **kwargs):
+        response = self._make_request(path, **kwargs)
+        try:
+            info = response.json()
+            return BackendResponse(**info)
+        except ValueError as e:
+            logging.error(str(e))
+            raise errors.InvalidResponse("Invalid response from backend")
 
     @abstractmethod
     def connect(self) -> None:
---
+++ contentcuration/automation/utils/appnexus/errors.py
@@ -0,0 +1,15 @@
+
+class ConnectionError(Exception):
+  pass
+
+class TimeoutError(Exception):
+  pass
+
+class HttpError(Exception):
+  pass
+
+class InvalidRequest(Exception):
+  pass
+
+class InvalidResponse(Exception):
+  pass
