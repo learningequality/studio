99c14c937dd14133aae6e0dffabd7168bd122897
ozer550 <prathameshd419@gmail.com>
2024-04-22 13:29:57 +0530

    Add Flagged Viewset

--- contentcuration/contentcuration/viewsets/feedback.py
+++ contentcuration/contentcuration/viewsets/feedback.py
@@ -1,9 +1,23 @@
+from rest_framework import permissions
 from rest_framework import serializers
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from contentcuration.models import BaseFeedback
 from contentcuration.models import BaseFeedbackEvent
 from contentcuration.models import BaseFeedbackInteractionEvent
 from contentcuration.models import FlagFeedbackEvent
+
+
+class IsAdminForListAndDestroy(permissions.BasePermission):
+    def has_permission(self, request, view):
+        # only allow list and destroy of flagged content to admins
+        if view.action in ['list', 'destroy']:
+            try:
+                return request.user and request.user.is_admin
+            except AttributeError:
+                return False
+        return True
 
 
 class BaseFeedbackSerializer(serializers.ModelSerializer):
@@ -28,3 +42,9 @@ class FlagFeedbackEventSerializer(BaseFeedbackSerializer, BaseFeedbackEventSeria
     class Meta:
         model = FlagFeedbackEvent
         fields = BaseFeedbackSerializer.Meta.fields + BaseFeedbackEventSerializer.Meta.fields + BaseFeedbackInteractionEventSerializer.Meta.fields
+
+
+class FlagFeedbackEventViewSet(viewsets.ModelViewSet):
+    queryset = FlagFeedbackEvent.objects.all()
+    serializer_class = FlagFeedbackEventSerializer
+    permission_classes = [IsAuthenticated, IsAdminForListAndDestroy]
