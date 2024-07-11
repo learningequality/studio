b8fdb18fb4389b26c78cb5e8d7601334f2a9e606
Samson Akol <akolson2010@gmail.com>
2024-07-11 14:03:49 +0300

    Implements extract content urls

--- contentcuration/automation/models.py
+++ contentcuration/automation/models.py
@@ -1,3 +1,12 @@
-# from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.db.models import JSONField
+
+
+class RecommendationsCache(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    request = JSONField(default=dict, null=True)
+    response = JSONField(default=dict, null=True)
+    priority = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
