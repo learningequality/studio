e872f1c513bffb474d9cde0fedf71db9b01f2c28
Samson Akol <akolson2010@gmail.com>
2024-07-30 16:35:09 +0300

    Adds indexes for optimized data querying

--- contentcuration/automation/models.py
+++ contentcuration/automation/models.py
@@ -4,6 +4,11 @@ from django.db import models
 from kolibri_public.models import ContentNode
 
 
+REQUEST_HASH_INDEX_NAME = "request_hash_idx"
+CONTENTNODE_ID_INDEX_NAME = "contentnode_id_idx"
+REQUEST_CONTENTNODE_INDEX_NAME = "request_hash_contentnode_id_idx"
+
+
 class RecommendationsCache(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     request_hash = models.CharField(max_length=32, null=True)
@@ -20,3 +25,11 @@ class RecommendationsCache(models.Model):
 
     class Meta:
         unique_together = ('request_hash', 'contentnode_id')
+        indexes = [
+            models.Index(fields=['request_hash'], name=REQUEST_HASH_INDEX_NAME),
+            models.Index(fields=['contentnode_id'], name=CONTENTNODE_ID_INDEX_NAME),
+            models.Index(
+                fields=['request_hash', 'contentnode_id'],
+                name=REQUEST_CONTENTNODE_INDEX_NAME,
+            ),
+        ]
