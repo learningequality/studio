0def99e926facd3aa40a2f8a478b117f1e739369
Samson Akol <akolson2010@gmail.com>
2024-08-01 16:48:37 +0300

    Writes queries to get main_tree_id

--- contentcuration/automation/models.py
+++ contentcuration/automation/models.py
@@ -6,7 +6,6 @@ from kolibri_public.models import ContentNode
 
 REQUEST_HASH_INDEX_NAME = "request_hash_idx"
 CONTENTNODE_ID_INDEX_NAME = "contentnode_id_idx"
-REQUEST_CONTENTNODE_INDEX_NAME = "request_hash_contentnode_idx"
 
 
 class RecommendationsCache(models.Model):
@@ -28,8 +27,4 @@ class RecommendationsCache(models.Model):
         indexes = [
             models.Index(fields=['request_hash'], name=REQUEST_HASH_INDEX_NAME),
             models.Index(fields=['contentnode_id'], name=CONTENTNODE_ID_INDEX_NAME),
-            models.Index(
-                fields=['request_hash', 'contentnode_id'],
-                name=REQUEST_CONTENTNODE_INDEX_NAME,
-            ),
         ]
