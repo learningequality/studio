f3622e8420d0d2beb79db394c3552374b970e906
Alex Velez <velezllaquealex@gmail.com>
2025-03-28 10:04:12 -0500

    Use CTE to get user server rev

--- contentcuration/contentcuration/models.py
+++ contentcuration/contentcuration/models.py
@@ -415,6 +415,18 @@ class User(AbstractBaseUser, PermissionsMixin):
         if changed:
             self.save()
 
+    def get_server_rev(self):
+        changes_cte = With(
+            Change.objects.filter(user=self).values("server_rev", "applied"),
+        )
+        return (
+            changes_cte.queryset()
+            .with_cte(changes_cte)
+            .filter(applied=True)
+            .values_list("server_rev", flat=True)
+            .order_by("-server_rev").first()
+        ) or 0
+
     class Meta:
         verbose_name = "User"
         verbose_name_plural = "Users"
