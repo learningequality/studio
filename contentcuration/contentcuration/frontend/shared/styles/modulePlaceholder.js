13e27456f58d3937991cda4be94387c99185694c
Blaine Jester <blainesworld@gmail.com>
2025-04-29 14:15:19 -0700

    Remove module placeholder file

--- contentcuration/contentcuration/frontend/shared/styles/modulePlaceholder.js
+++
@@ -1,7 +0,0 @@
-// This file is used as a placeholder, following the same pattern established in
-// jest_config/globalMocks/fileMock.js
-
-// It is used to help use properly overwrite the vuetify stylus files using webpack
-// (see also webpack.config.js > webpack.NormalModuleReplacementPlugin())
-
-'module-file-stub';
