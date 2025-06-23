a7c375a9848e8d1c8d5cc1538062dca58051b5bf
Michaela Robosova <robosovam@gmail.com>
2025-06-23 08:20:29 +0200

    Cleanup test suite
    Remove dependencies on internal implementation.

--- contentcuration/contentcuration/frontend/settings/pages/__tests__/changePasswordForm.spec.js
+++ contentcuration/contentcuration/frontend/settings/pages/__tests__/changePasswordForm.spec.js
@@ -13,34 +13,81 @@ function makeWrapper() {
 }
 
 describe('changePasswordForm', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = makeWrapper();
+  it('should render the form', () => {
+    const wrapper = makeWrapper();
+    expect(wrapper.html()).toContain('Change password');
+    expect(wrapper.html()).toContain('New password');
+    expect(wrapper.html()).toContain('Confirm new password');
+    expect(wrapper.html()).toContain('Save changes');
+    expect(wrapper.html()).toContain('Cancel');
   });
 
-  it('validation should fail if the password is too short', async () => {
-    await wrapper.setData({ password: 'test' });
-    expect(wrapper.vm.errors.password).toBe(true);
+  describe('if a password is too short', () => {
+    let updateUserPassword;
+    let wrapper;
+
+    beforeAll(async () => {
+      wrapper = makeWrapper();
+      updateUserPassword = jest.spyOn(wrapper.vm, 'updateUserPassword');
+      const passwordInputs = wrapper.findAll('input[type="password"]');
+      passwordInputs.at(0).setValue('pw');
+      wrapper.findComponent({ name: 'KModal' }).vm.$emit('submit');
+      await wrapper.vm.$nextTick();
+    });
+
+    it('should show a correct error message', () => {
+      expect(wrapper.html()).toContain('Password should be at least 8 characters long');
+    });
+
+    it('should not call updateUserPassword', () => {
+      expect(updateUserPassword).not.toHaveBeenCalled();
+    });
   });
 
-  it('validation should fail if the passwords do not match', async () => {
-    await wrapper.setData({ password: 'testtest', confirmation: 'testtest2' });
-    expect(wrapper.vm.errors.confirmation).toBe(true);
+  describe(`if passwords don't match`, () => {
+    let updateUserPassword;
+    let wrapper;
+
+    beforeAll(async () => {
+      wrapper = makeWrapper();
+      updateUserPassword = jest.spyOn(wrapper.vm, 'updateUserPassword');
+      const passwordInputs = wrapper.findAll('input[type="password"]');
+      passwordInputs.at(0).setValue('password1');
+      passwordInputs.at(1).setValue('password2');
+      wrapper.findComponent({ name: 'KModal' }).vm.$emit('submit');
+      await wrapper.vm.$nextTick();
+    });
+
+    it('should show a correct error message', () => {
+      expect(wrapper.html()).toContain(`Passwords don't match`);
+    });
+
+    it('should not call updateUserPassword', () => {
+      expect(updateUserPassword).not.toHaveBeenCalled();
+    });
   });
 
-  it('failed validation should not call updateUserPassword', async () => {
-    const updateUserPassword = jest.spyOn(wrapper.vm, 'updateUserPassword');
-    updateUserPassword.mockImplementation(() => Promise.resolve());
-    await wrapper.vm.submit();
-    expect(updateUserPassword).not.toHaveBeenCalled();
-  });
+  describe('if passwords match and are valid', () => {
+    let updateUserPassword;
+    let wrapper;
 
-  it('clicking submit should call updateUserPassword', async () => {
-    const updateUserPassword = jest.spyOn(wrapper.vm, 'updateUserPassword');
-    updateUserPassword.mockImplementation(() => Promise.resolve());
-    await wrapper.setData({ password: 'tester123', confirmation: 'tester123' });
-    await wrapper.vm.submit();
-    expect(updateUserPassword).toHaveBeenCalled();
+    beforeAll(async () => {
+      wrapper = makeWrapper();
+      updateUserPassword = jest.spyOn(wrapper.vm, 'updateUserPassword');
+      const passwordInputs = wrapper.findAll('input[type="password"]');
+      passwordInputs.at(0).setValue('password123');
+      passwordInputs.at(1).setValue('password123');
+      wrapper.findComponent({ name: 'KModal' }).vm.$emit('submit');
+      await wrapper.vm.$nextTick();
+    });
+
+    it('should not show any error messages', () => {
+      expect(wrapper.html()).not.toContain('Password should be at least 8 characters long');
+      expect(wrapper.html()).not.toContain(`Passwords don't match`);
+    });
+
+    it('should call updateUserPassword', () => {
+      expect(updateUserPassword).toHaveBeenCalled();
+    });
   });
 });
