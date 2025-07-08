b54f9a1450259a2b5711c777bcf5b4517b5a79c2
Aadarsh <aadarshmahk@gmail.com>
2025-07-08 10:24:36 +0530

    feat: create StudioOfflineAlert component using Kolibri Design System

---
+++ contentcuration/contentcuration/frontend/shared/views/StudioOfflineAlert.vue
@@ -0,0 +1,84 @@
+<template>
+  <KTransition kind="component-vertical-slide-out-in">
+    <div v-if="offline && !libraryMode" class="studio-offline-alert"
+      :style="computedstyle"
+      data-test="studio-offline-alert"
+    >
+      <div class="alert-content">
+        <KIcon
+          icon="disconnected"
+          class="mx-3"
+        />
+        <span class="notranslate">{{ $tr('offlineText') }}</span>
+      </div>
+    </div>
+  </KTransition>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import useKLiveRegion from 'kolibri-design-system/lib/composables/useKLiveRegion';
+
+export default {
+  name: 'StudioOfflineAlert',
+  props: {
+    offset: {
+      type: Number,
+      default: 0,
+    },
+  },
+  setup() {
+    const { sendPoliteMessage } = useKLiveRegion();
+    return { sendPoliteMessage };
+  },
+  computed: {
+    ...mapState({
+      offline: state => !state.connection.online,
+    }),
+    libraryMode() {
+      return window.libraryMode;
+    },
+    computedstyle() {
+      return {
+        position: 'fixed',
+        top: `${this.offset}px`,
+        left: '0',
+        right: '0',
+        backgroundColor: this.$themeTokens.surface,
+        zIndex: 300,
+      };
+    },
+  },
+  watch: {
+    offline(newVal) {
+      if (newVal) {
+        this.sendPoliteMessage(this.$tr('offlineText'));
+      } else {
+        this.sendPoliteMessage(this.$tr('onlineText'));
+      }
+    },
+  },
+  $trs: {
+    offlineText: 'You seem to be offline. Your changes will be saved once your connection is back.',
+    onlineText: 'You are back online.',
+  },
+};
+</script>
+
+<style scoped>
+.studio-offline-alert {
+  width: 100%;
+  display: flex;
+  padding: 12px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.alert-content {
+  display: flex;
+  align-items: center;
+}
+
+.alert-content span {
+  vertical-align: bottom;
+}
+</style>
