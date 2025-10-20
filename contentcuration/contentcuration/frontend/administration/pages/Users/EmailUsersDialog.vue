90bdd6207b7a8928f1c429607766d7ad7a32e944
Prashant-thakur77 <prashant101007@gmail.com>
2025-10-20 23:36:49 +0530

    added Generate form mixin Functionality

--- contentcuration/contentcuration/frontend/administration/pages/Users/EmailUsersDialog.vue
+++ contentcuration/contentcuration/frontend/administration/pages/Users/EmailUsersDialog.vue
@@ -1,145 +1,141 @@
 <template>
 
-  <VDialog
-    v-model="show"
+  <KModal
+    v-if="show"
+    :size="600"
+    :title="'Send Email'"
+    :submitText="$tr('sendButton')"
+    :cancelText="$tr('cancelButton')"
+    data-test="email-dialog"
     width="600"
-    max-width="100vw"
-    persistent
+    maxWidth="100vw"
+    @submit="submit"
+    @cancel="cancel"
   >
-    <VCard class="px-2 py-3">
-      <VCardTitle class="font-weight-bold pb-0 title"> Send email </VCardTitle>
-      <VForm
-        ref="form"
-        lazy-validation
-        @submit.prevent="emailHandler"
-      >
-        <VCardText class="pb-4 pt-3">
-          <div class="align-top layout-row mb-2">
-            <div class="flex-shrink pa-2">From:</div>
-            <div class="flex-grow">
-              <VChip small>
-                {{ senderEmail }}
-              </VChip>
-            </div>
+    <form
+      ref="form"
+      @submit.prevent="submit"
+    >
+      <VCardText class="pb-4 pt-3">
+        <StudioBanner
+          v-if="Boolean(errorCount())"
+          error
+          class="studio-banner"
+        >
+          {{ errorText() }}
+        </StudioBanner>
+
+        <div class="align-top layout-row mb-2">
+          <div class="flex-shrink pa-2">From:</div>
+          <div class="flex-grow">
+            <VChip small>
+              {{ senderEmail }}
+            </VChip>
           </div>
-          <div class="align-top layout-row">
-            <div class="flex-shrink pa-2">To:</div>
-            <div
-              class="flex-grow"
-              data-test="to-line"
+        </div>
+        <div class="align-top layout-row">
+          <div class="flex-shrink pa-2">To:</div>
+          <div
+            class="flex-grow"
+            data-test="to-line"
+          >
+            <ExpandableList
+              v-if="initialRecipients"
+              :items="users"
+              :max="4"
+              inline
+              :delimit="false"
             >
-              <ExpandableList
-                v-if="initialRecipients"
-                :items="users"
-                :max="4"
-                inline
-                :delimit="false"
-              >
-                <template #item="{ item }">
-                  <div class="chip-tooltip-container">
-                    <KTooltip
-                      :refs="getRefs()"
-                      :reference="`tooltip-${item.id}`"
-                      placement="bottom"
-                    >
-                      <span>{{ item.name }} &lt;{{ item.email }}&gt;</span>
-                    </KTooltip>
-                    <VChip
-                      :ref="`tooltip-${item.id}`"
-                      small
-                      :close="recipients.length > 1"
-                      data-test="remove"
-                      @input="remove(item.id)"
-                    >
-                      <div style="max-width: 72px">
-                        <div class="text-truncate">
-                          {{ item.name }}
-                        </div>
+              <template #item="{ item }">
+                <div class="chip-tooltip-container">
+                  <KTooltip
+                    :refs="getRefs()"
+                    :reference="`tooltip-${item.id}`"
+                    placement="bottom"
+                  >
+                    <span>{{ item.name }} &lt;{{ item.email }}&gt;</span>
+                  </KTooltip>
+                  <VChip
+                    :ref="`tooltip-${item.id}`"
+                    small
+                    :close="recipients.length > 1"
+                    data-test="remove"
+                    @input="remove(item.id)"
+                  >
+                    <div style="max-width: 72px">
+                      <div class="text-truncate">
+                        {{ item.name }}
                       </div>
-                    </VChip>
-                  </div>
-                </template>
-              </ExpandableList>
-              <VChip
-                v-else-if="usersFilterFetchQueryParams"
-                small
-              >
-                {{ searchString }}
-              </VChip>
-            </div>
-          </div>
-          <KTextbox
-            v-model="subject"
-            class="mt-4"
-            :label="'Subject line'"
-            :required="true"
-            :invalid="!subject.trim()"
-            :invalidText="requiredRules(subject)[0]"
-            :showLabel="true"
-            :appearanceOverrides="{ maxWidth: '100%', width: '100%' }"
-          />
-          <div class="caption grey--text">Add placeholder to message</div>
-          <div class="mb-1">
-            <VBtn
-              v-for="placeholder in placeholders"
-              :key="`placeholder-${placeholder.label}`"
+                    </div>
+                  </VChip>
+                </div>
+              </template>
+            </ExpandableList>
+            <VChip
+              v-else-if="usersFilterFetchQueryParams"
               small
-              round
-              depressed
-              color="grey lighten-4"
-              style="text-transform: none"
-              @click="addPlaceholder(placeholder.placeholder)"
             >
-              {{ placeholder.label }}
-            </VBtn>
+              {{ searchString }}
+            </VChip>
           </div>
-          <div class="email-textarea">
-            <KTextbox
-              ref="message"
-              v-model="message"
-              :label="'Email body'"
-              :required="true"
-              :invalid="!message.trim()"
-              :invalidText="requiredRules(message)[0]"
-              :showLabel="true"
-              :appearanceOverrides="{ maxWidth: '100%', width: '100%' }"
-              :floatingLabel="false"
-              :textArea="true"
-            />
-          </div>
-        </VCardText>
-        <VCardActions data-test="buttons">
-          <div class="spacer"></div>
-          <KButtonGroup class="button-group">
-            <KButton
-              data-test="cancel"
-              appearance="flat-button"
-              :text="$tr('cancelButton')"
-              @click="cancel"
-            />
-            <KButton
-              data-test="send"
-              appearance="raised-button"
-              :primary="true"
-              :text="$tr('sendButton')"
-              type="submit"
-            />
-          </KButtonGroup>
-        </VCardActions>
-      </VForm>
-      <KModal
-        v-if="showWarning"
-        :title="$tr('draftWarningTitle')"
-        :submitText="$tr('discardDraftButton')"
-        :cancelText="$tr('keepOpenButton')"
-        data-test="confirm"
-        @submit="close"
-        @cancel="showWarning = false"
-      >
-        <p>{{ $tr('draftWarningText') }}</p>
-      </KModal>
-    </VCard>
-  </VDialog>
+        </div>
+        <KTextbox
+          v-model="subject"
+          class="mt-4"
+          :label="'Subject line'"
+          :required="true"
+          :invalid="errors.subject"
+          :showInvalidText="errors.subject"
+          :invalidText="$tr('fieldRequiredText')"
+          :showLabel="true"
+          :appearanceOverrides="getAppearanceOverrides(errors.subject)"
+        />
+        <div class="caption grey--text">Add placeholder to message</div>
+        <div class="mb-1">
+          <VBtn
+            v-for="placeholder in placeholders"
+            :key="`placeholder-${placeholder.label}`"
+            small
+            round
+            depressed
+            color="grey lighten-4"
+            style="text-transform: none"
+            @click="addPlaceholder(placeholder.placeholder)"
+          >
+            {{ placeholder.label }}
+          </VBtn>
+        </div>
+        <div class="email-textarea">
+          <KTextbox
+            ref="message"
+            v-model="message"
+            :label="'Email body'"
+            :required="true"
+            :invalid="errors.message"
+            :showInvalidText="errors.message"
+            :invalidText="$tr('fieldRequiredText')"
+            :showLabel="true"
+            :appearanceOverrides="getAppearanceOverrides(errors.message)"
+            :floatingLabel="false"
+            :textArea="true"
+          />
+        </div>
+      </VCardText>
+    </form>
+
+    <!-- Warning modal for draft -->
+    <KModal
+      v-if="showWarning"
+      :title="$tr('draftWarningTitle')"
+      :submitText="$tr('discardDraftButton')"
+      :cancelText="$tr('keepOpenButton')"
+      data-test="confirm"
+      @submit="close"
+      @cancel="showWarning = false"
+    >
+      <p>{{ $tr('draftWarningText') }}</p>
+    </KModal>
+  </KModal>
 
 </template>
 
@@ -148,12 +144,21 @@
 
   import { mapActions, mapGetters } from 'vuex';
   import ExpandableList from 'shared/views/ExpandableList';
+  import { generateFormMixin } from 'shared/mixins';
+  import StudioBanner from 'shared/views/StudioBanner';
+
+  const formMixin = generateFormMixin({
+    subject: { required: true },
+    message: { required: true },
+  });
 
   export default {
     name: 'EmailUsersDialog',
     components: {
       ExpandableList,
+      StudioBanner,
     },
+    mixins: [formMixin],
     props: {
       value: {
         type: Boolean,
@@ -183,8 +188,6 @@
     },
     data() {
       return {
-        subject: '',
-        message: '',
         showWarning: false,
         recipients: [],
       };
@@ -201,12 +204,6 @@
       },
       users() {
         return this.getUsers(this.recipients);
-      },
-      requiredRules() {
-        return value => {
-          const isValid = Boolean(value && value.trim());
-          return isValid ? [] : ['Field is required'];
-        };
       },
       senderEmail() {
         return window.senderEmail;
@@ -263,29 +260,7 @@
         this.subject = '';
         this.message = '';
         this.showWarning = false;
-      },
-      emailHandler() {
-        const isSubjectValid = this.requiredRules(this.subject).length === 0;
-        const isMessageValid = this.requiredRules(this.message).length === 0;
-
-        if (isSubjectValid && isMessageValid) {
-          const query = this.initialRecipients
-            ? { ids: this.recipients.join(',') }
-            : this.usersFilterFetchQueryParams;
-
-          this.sendEmail({
-            query,
-            subject: this.subject,
-            message: this.message,
-          })
-            .then(() => {
-              this.close();
-              this.$store.dispatch('showSnackbarSimple', 'Email sent');
-            })
-            .catch(() => {
-              this.$store.dispatch('showSnackbarSimple', 'Email failed to send');
-            });
-        }
+        this.reset(); // Reset form validation state
       },
       addPlaceholder(placeholder) {
         this.message += ` ${placeholder}`;
@@ -294,9 +269,48 @@
       remove(id) {
         this.recipients = this.recipients.filter(u => u !== id);
       },
-
       getRefs() {
         return this.$refs;
+      },
+      getAppearanceOverrides(isInvalid) {
+        const baseStyles = { maxWidth: '100%', width: '100%' };
+        if (isInvalid) {
+          return {
+            ...baseStyles,
+            focusBorderColor: this.$themeTokens.error,
+            focusBoxShadow: `0 0 0 2px ${this.$themeTokens.error}33`,
+          };
+        }
+        return baseStyles;
+      },
+
+      // Form mixin methods
+      // eslint-disable-next-line kolibri/vue-no-unused-methods, vue/no-unused-properties
+      onValidationFailed() {
+        // Scroll to error banner
+        if (this.$refs.form && this.$refs.form.scrollIntoView) {
+          this.$refs.form.scrollIntoView({ behavior: 'smooth' });
+        }
+      },
+
+      // eslint-disable-next-line kolibri/vue-no-unused-methods, vue/no-unused-properties
+      onSubmit() {
+        const query = this.initialRecipients
+          ? { ids: this.recipients.join(',') }
+          : this.usersFilterFetchQueryParams;
+
+        this.sendEmail({
+          query,
+          subject: this.subject,
+          message: this.message,
+        })
+          .then(() => {
+            this.close();
+            this.$store.dispatch('showSnackbarSimple', 'Email sent');
+          })
+          .catch(() => {
+            this.$store.dispatch('showSnackbarSimple', 'Email failed to send');
+          });
       },
     },
     $trs: {
@@ -307,6 +321,7 @@
       keepOpenButton: 'Keep open',
       cancelButton: 'Cancel',
       sendButton: 'Send email',
+      fieldRequiredText: 'Field is required',
     },
   };
 
@@ -317,6 +332,11 @@
 
   ::v-deep .v-chip__close {
     padding-top: 4px;
+  }
+
+  ::v-deep textarea {
+    height: 120px;
+    min-height: 120px;
   }
 
   /* Or if you want them completely inline without any breaks: */
@@ -365,6 +385,11 @@
 
   .button-group {
     white-space: nowrap;
+  }
+
+  .studio-banner {
+    margin-top: 8px;
+    margin-bottom: 8px;
   }
 
 </style>
