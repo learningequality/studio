c6f00169a28c87ecf0e74ab3b291100d3e33aaae
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-10-22 23:38:46 +0530

    adding unit tests for StudioCopyToken using VTL
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- contentcuration/contentcuration/frontend/settings/pages/__tests__/studioCopyToken.spec.js
+++ contentcuration/contentcuration/frontend/settings/pages/__tests__/studioCopyToken.spec.js
@@ -1,36 +1,45 @@
-import { mount } from '@vue/test-utils';
+import { render, fireEvent, screen } from '@testing-library/vue';
+import VueRouter from 'vue-router';
 import StudioCopyToken from '../Account/StudioCopyToken.vue';
 
 function makeWrapper(props = {}) {
-  return mount(StudioCopyToken, {
-    propsData: {
-      token: 'testtoken',
-      ...props,
-    },
-    mocks: {
-      $store: { dispatch: jest.fn() },
-      $tr: key => key,
-    },
-  });
+  const mockStore = {
+    dispatch: jest.fn(),
+  };
+
+  return {
+    ...render(StudioCopyToken, {
+      props: {
+        token: 'testtoken',
+        ...props,
+      },
+      routes: new VueRouter({}),
+      mocks: {
+        $store: mockStore,
+        $tr: key => key,
+      },
+    }),
+    mockStore,
+  };
 }
 
 describe('StudioCopyToken', () => {
   it('displays hyphenated token by default', () => {
-    const wrapper = makeWrapper();
-    const input = wrapper.findComponent({ ref: 'tokenInput' });
-    expect(input.props('value')).toBe('testt-oken');
+    makeWrapper();
+    const input = screen.getByDisplayValue('testt-oken');
+    expect(input).toBeInTheDocument();
   });
 
   it('displays token without hyphen if hyphenate is false', () => {
-    const wrapper = makeWrapper({ hyphenate: false });
-    const input = wrapper.findComponent({ ref: 'tokenInput' });
-    expect(input.props('value')).toBe('testtoken');
+    makeWrapper({ hyphenate: false });
+    const input = screen.getByDisplayValue('testtoken');
+    expect(input).toBeInTheDocument();
   });
 
   it('shows loader when loading is true', () => {
-    const wrapper = makeWrapper({ loading: true });
-    expect(wrapper.findComponent({ name: 'KCircularLoader' }).exists()).toBe(true);
-    expect(wrapper.findComponent({ ref: 'tokenInput' }).exists()).toBe(false);
+    const { container } = makeWrapper({ loading: true });
+    expect(container.querySelector('.ui-progress-circular')).toBeInTheDocument();
+    expect(screen.queryByDisplayValue(/test/)).not.toBeInTheDocument();
   });
 
   it('should fire a copy operation on button click', async () => {
@@ -38,8 +47,9 @@ describe('StudioCopyToken', () => {
     Object.assign(navigator, {
       clipboard: { writeText },
     });
-    const wrapper = makeWrapper();
-    await wrapper.find('.copy-button').trigger('click');
+    makeWrapper();
+    const button = screen.getByRole('button');
+    await fireEvent.click(button);
     expect(navigator.clipboard.writeText).toHaveBeenCalledWith('testt-oken');
   });
 
@@ -48,9 +58,10 @@ describe('StudioCopyToken', () => {
     Object.assign(navigator, {
       clipboard: { writeText },
     });
-    const wrapper = makeWrapper();
-    await wrapper.find('.copy-button').trigger('click');
-    expect(wrapper.vm.$store.dispatch).toHaveBeenCalledWith('showSnackbarSimple', 'tokenCopied');
+    const { mockStore } = makeWrapper();
+    const button = screen.getByRole('button');
+    await fireEvent.click(button);
+    expect(mockStore.dispatch).toHaveBeenCalledWith('showSnackbarSimple', 'tokenCopied');
   });
 
   it('dispatches snackbar on failed copy', async () => {
@@ -58,20 +69,22 @@ describe('StudioCopyToken', () => {
     Object.assign(navigator, {
       clipboard: { writeText },
     });
-    const wrapper = makeWrapper();
-    await wrapper.find('.copy-button').trigger('click');
-    expect(wrapper.vm.$store.dispatch).toHaveBeenCalledWith(
-      'showSnackbarSimple',
-      'tokenCopyFailed',
-    );
+    const { mockStore } = makeWrapper();
+    const button = screen.getByRole('button');
+    await fireEvent.click(button);
+    expect(mockStore.dispatch).toHaveBeenCalledWith('showSnackbarSimple', 'tokenCopyFailed');
   });
 
   it('dispatches snackbar if token is empty', async () => {
-    const wrapper = makeWrapper({ token: '   ' });
-    await wrapper.find('.copy-button').trigger('click');
-    expect(wrapper.vm.$store.dispatch).toHaveBeenCalledWith(
-      'showSnackbarSimple',
-      'tokenCopyFailed',
-    );
+    const { mockStore } = makeWrapper({ token: '   ' });
+    const button = screen.getByRole('button');
+    await fireEvent.click(button);
+    expect(mockStore.dispatch).toHaveBeenCalledWith('showSnackbarSimple', 'tokenCopyFailed');
+  });
+
+  it('renders the copy button in the innerAfter slot', () => {
+    const { container } = makeWrapper();
+    const button = container.querySelector('.inner-after .copy-button');
+    expect(button).toBeInTheDocument();
   });
 });
