1bc4f7f3e2e470ebb4647a4f073e683ce79b5b42
Prashant-thakur77 <prashant101007@gmail.com>
2025-10-22 22:32:12 +0530

    Made StudioLargeLoader

---
+++ contentcuration/contentcuration/frontend/shared/views/StudioLargeLoader.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div class="studio-large-loader">
+    <KCircularLoader :size="70" />
+  </div>
+
+</template>
+
+
+<script>
+
+  import KCircularLoader from 'kolibri-design-system/lib/loaders/KCircularLoader.vue';
+
+  export default {
+    name: 'StudioLargeLoader',
+    components: { KCircularLoader },
+  };
+
+</script>
+
+
+<style lang="scss" scoped>
+
+  .studio-large-loader {
+    padding: 24px;
+  }
+
+</style>
