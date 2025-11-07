c2ca87e1f51b696323828119e404d61b4aaff9bf
Prashant-thakur77 <prashant101007@gmail.com>
2025-11-08 02:07:26 +0530

    Added button instead of kbutton

--- contentcuration/contentcuration/frontend/shared/views/StudioChip.vue
+++ contentcuration/contentcuration/frontend/shared/views/StudioChip.vue
@@ -12,15 +12,25 @@
         </slot>
       </div>
 
-      <KIconButton
+      <button
         v-if="close"
         class="studio-chip__close"
-        appearance="flat-button"
-        :ariaLabel="removeLabel"
-        icon="close"
+        :style="closeButtonStyles"
+        :class="
+          $computedClass({
+            ':hover': { backgroundColor: $themePalette.grey.v_700 },
+          })
+        "
+        :aria-label="removeLabel"
         data-test="remove-chip"
         @click.stop="handleClose"
-      />
+      >
+        <KIcon
+          icon="delete"
+          class="studio-chip__icon"
+          :color="iconColor"
+        />
+      </button>
     </div>
   </div>
 
@@ -44,10 +54,17 @@
     computed: {
       chipStyles() {
         const baseColor = this.$themePalette.grey.v_200;
-
         return {
           backgroundColor: baseColor,
         };
+      },
+      closeButtonStyles() {
+        return {
+          backgroundColor: this.$themePalette.grey.v_300,
+        };
+      },
+      iconColor() {
+        return 'grey';
       },
       removeLabel() {
         return `Remove ${this.text}`;
@@ -72,7 +89,7 @@
     min-height: 24px;
     padding: 2px 8px;
     margin: 2px;
-    font-size: 12px;
+    font-size: 13px;
     white-space: nowrap;
     cursor: default;
     user-select: none;
@@ -85,6 +102,34 @@
       align-items: center;
       height: 100%;
     }
+
+    &__text {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  .studio-chip__close {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 16px;
+    height: 16px;
+    padding: 0;
+    margin-top: 2px;
+    margin-left: 2px;
+    cursor: pointer;
+    border-radius: 50%;
+    transition: background-color 0.2s ease;
+  }
+
+  .studio-chip__icon {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    font-size: 18px;
+    transform: translate(-50%, -50%);
   }
 
 </style>
