3baa8f8d18ca6822ad2a367aeca70fb3f2b5973d
Prashant-thakur77 <prashant101007@gmail.com>
2025-11-14 11:05:28 +0530

    added ktextbox

--- contentcuration/contentcuration/frontend/channelList/views/Channel/CatalogFilters.vue
+++ contentcuration/contentcuration/frontend/channelList/views/Channel/CatalogFilters.vue
@@ -1,114 +1,40 @@
 <template>
 
   <div>
-    <!-- Mobile filter button -->
     <KButton
-      v-if="!windowIsLarge"
+      v-if="$vuetify.breakpoint.xsOnly"
       class="drawer-btn"
-      :text="$tr('FilterText')"
+      :text="$tr('searchText')"
       appearance="raised-button"
-      @click.stop="mobileSidePanelIsOpen = true"
+      @click.stop="drawer = true"
     />
-
     <CatalogFilterBar />
-
-    <!-- Desktop/Large Screen Side Panel (non-modal) -->
-    <aside
-      v-if="windowIsLarge"
-      class="side-panel-container"
-      :class="{ 'is-rtl': isRTL }"
+    <VNavigationDrawer
+      v-model="drawer"
+      :permanent="$vuetify.breakpoint.smAndUp"
+      app
+      disable-route-watcher
+      :clipped="$vuetify.breakpoint.smAndUp"
+      :right="isRTL"
     >
-      <div class="filters">
-        <!-- Keyword search -->
-        <KTextbox
-          v-model="keywordInput"
-          :label="$tr('searchLabel')"
-          :clearable="true"
-          data-test="keywords"
-          autofocus
-          @input="setKeywords"
-        />
+      <VContainer class="filters pa-3">
+        <VToolbar
+          v-if="$vuetify.breakpoint.xsOnly"
+          color="transparent"
+          flat
+          dense
+        >
+          <VSpacer />
+          <VBtn
+            icon
+            flat
+            style="text-align: right"
+            @click="drawer = false"
+          >
+            <Icon icon="clear" />
+          </VBtn>
+        </VToolbar>
 
-        <!-- Language -->
-        <LanguageFilter v-model="languages" />
-
-        <!-- License -->
-        <MultiSelect
-          v-if="!libraryMode"
-          v-model="licenses"
-          :items="licenseOptions"
-          :label="$tr('licenseLabel')"
-        />
-
-        <!-- Formats -->
-        <MultiSelect
-          v-model="kinds"
-          :items="kindOptions"
-          :label="$tr('formatLabel')"
-        />
-
-        <!-- Starred -->
-        <KCheckbox
-          v-if="loggedIn"
-          v-model="bookmark"
-          :label="$tr('starredLabel')"
-        />
-
-        <!-- Includes -->
-        <div class="subheading">
-          {{ $tr('includesLabel') }}
-        </div>
-
-        <div class="checkbox-with-tooltip">
-          <KCheckbox
-            v-model="coach"
-            :label="$tr('coachLabel')"
-          />
-          <HelpTooltip
-            :text="$tr('coachDescription')"
-            maxWidth="250px"
-          />
-        </div>
-
-        <KCheckbox
-          v-model="subtitles"
-          :label="$tr('subtitlesLabel')"
-        />
-
-        <KRouterLink
-          class="qa-link"
-          :to="faqLink"
-          :text="$tr('frequentlyAskedQuestionsLink')"
-          appearance="basic-link"
-          iconAfter="openNewTab"
-          target="_blank"
-        />
-
-        <!-- Footer with KImg -->
-        <div class="side-panel-footer">
-          <KImg
-            :src="leLogoSrc"
-            altText="Learning Equality logo"
-            :aspectRatio="getLogoAspectRatio"
-            scaleType="contain"
-            :appearanceOverrides="logoStyles"
-          />
-          <KExternalLink
-            href="https://learningequality.org/"
-            :text="$tr('copyright', { year: new Date().getFullYear() })"
-            openInNewTab
-          />
-        </div>
-      </div>
-    </aside>
-
-    <!-- Mobile Side Panel Modal -->
-    <SidePanelModal
-      v-else-if="mobileSidePanelIsOpen"
-      alignment="left"
-      @closePanel="mobileSidePanelIsOpen = false"
-    >
-      <div class="filters mobile-filters">
         <!-- Keyword search -->
         <KTextbox
           v-model="keywordInput"
@@ -119,9 +45,12 @@
         />
 
         <!-- Language -->
-        <LanguageFilter v-model="languages" />
+        <LanguageFilter
+          v-model="languages"
+          :menu-props="menuProps"
+        />
 
-        <!-- License -->
+        <!-- License (attach to self to keep in notranslate class) -->
         <MultiSelect
           v-if="!libraryMode"
           v-model="licenses"
@@ -129,7 +58,7 @@
           :label="$tr('licenseLabel')"
         />
 
-        <!-- Formats -->
+        <!-- Formats (attach to self to keep in notranslate class) -->
         <MultiSelect
           v-model="kinds"
           :items="kindOptions"
@@ -137,7 +66,7 @@
         />
 
         <!-- Starred -->
-        <KCheckbox
+        <Checkbox
           v-if="loggedIn"
           v-model="bookmark"
           :label="$tr('starredLabel')"
@@ -148,22 +77,23 @@
           {{ $tr('includesLabel') }}
         </div>
 
-        <div class="checkbox-with-tooltip">
-          <KCheckbox
+        <div :style="{ display: 'flex', alignItems: 'center' }">
+          <Checkbox
             v-model="coach"
+            aria-describedby="tooltip-coach"
             :label="$tr('coachLabel')"
           />
           <HelpTooltip
             :text="$tr('coachDescription')"
             maxWidth="250px"
+            tooltipId="tooltip-coach"
           />
         </div>
 
-        <KCheckbox
+        <Checkbox
           v-model="subtitles"
           :label="$tr('subtitlesLabel')"
         />
-
         <KRouterLink
           class="qa-link"
           :to="faqLink"
@@ -172,15 +102,19 @@
           iconAfter="openNewTab"
           target="_blank"
         />
-
-        <!-- Footer with KImg for mobile -->
-        <div class="side-panel-footer">
-          <KImg
-            :src="leLogoSrc"
-            altText="Learning Equality logo"
-            :aspectRatio="getLogoAspectRatio"
-            scaleType="contain"
-            :appearanceOverrides="mobileLogoStyles"
+      </VContainer>
+      <VFooter
+        class="pb-3 pt-2 px-4"
+        color="transparent"
+        height="100"
+      >
+        <div>
+          <VImg
+            height="60"
+            width="90"
+            class="mb-1 mr-2"
+            contain
+            :src="require('shared/images/le-logo.svg')"
           />
           <KExternalLink
             href="https://learningequality.org/"
@@ -188,8 +122,8 @@
             openInNewTab
           />
         </div>
-      </div>
-    </SidePanelModal>
+      </VFooter>
+    </VNavigationDrawer>
   </div>
 
 </template>
@@ -199,17 +133,15 @@
 
   import { mapGetters } from 'vuex';
   import debounce from 'lodash/debounce';
-  import useKResponsiveWindow from 'kolibri-design-system/lib/composables/useKResponsiveWindow';
   import { RouteNames } from '../../constants';
   import CatalogFilterBar from './CatalogFilterBar';
   import { catalogFilterMixin } from './mixins';
   import LanguageFilter from './components/LanguageFilter';
   import MultiSelect from 'shared/views/form/MultiSelect';
   import { constantsTranslationMixin } from 'shared/mixins';
+  import Checkbox from 'shared/views/form/Checkbox';
   import HelpTooltip from 'shared/views/HelpTooltip';
   import { ContentKindsNames } from 'shared/leUtils/ContentKinds';
-
-  import SidePanelModal from 'shared/views/SidePanelModal';
 
   const excludedKinds = new Set([ContentKindsNames.TOPIC, ContentKindsNames.H5P]);
 
@@ -217,23 +149,16 @@
     name: 'CatalogFilters',
     components: {
       LanguageFilter,
+      Checkbox,
       HelpTooltip,
       MultiSelect,
       CatalogFilterBar,
-      SidePanelModal,
     },
     mixins: [constantsTranslationMixin, catalogFilterMixin],
-    setup() {
-      const { windowIsLarge } = useKResponsiveWindow();
-      return {
-        windowIsLarge,
-      };
-    },
     data() {
       return {
-        mobileSidePanelIsOpen: false,
+        drawer: false,
         keywordInput: '',
-        leLogoSrc: require('shared/images/le-logo.svg'),
       };
     },
     computed: {
@@ -246,6 +171,9 @@
       },
       faqLink() {
         return { name: RouteNames.CATALOG_FAQ };
+      },
+      menuProps() {
+        return { offsetY: true, maxHeight: 270 };
       },
       kindOptions() {
         return (window.publicKinds || [])
@@ -270,24 +198,6 @@
       setKeywords() {
         return debounce(this.updateKeywords, 500);
       },
-      // LE logo aspect ratio (90x60 = 3:2)
-      getLogoAspectRatio() {
-        return '3:2';
-      },
-      logoStyles() {
-        return {
-          width: '90px',
-          height: '60px',
-          margin: '0 auto 8px auto',
-        };
-      },
-      mobileLogoStyles() {
-        return {
-          width: '80px',
-          height: '53px',
-          margin: '0 auto 8px auto',
-        };
-      },
     },
     watch: {
       keywords() {
@@ -310,7 +220,7 @@
       licenseLabel: 'Licenses',
       formatLabel: 'Formats',
       includesLabel: 'Display only channels with',
-      FilterText: 'Filter',
+      searchText: 'Search',
       coachDescription: 'Resources for coaches are only visible to coaches in Kolibri',
       frequentlyAskedQuestionsLink: 'Frequently asked questions',
       copyright: '© {year} Learning Equality',
@@ -322,40 +232,12 @@
 
 <style lang="scss" scoped>
 
-  .side-panel-container {
-    width: 300px;
-    min-width: 300px;
-    height: calc(100vh - 64px); // Adjust based on app bar height
-    overflow-y: auto;
-    background-color: white;
-    border-right: 1px solid #e0e0e0;
-
-    &.is-rtl {
-      border-left: 1px solid #e0e0e0;
-    }
+  .v-input--checkbox {
+    margin: 0;
   }
 
-  .filters {
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    height: 100%;
-    padding: 16px;
-  }
-
-  .mobile-filters {
-    height: auto;
-    padding-bottom: 80px; // Extra space for mobile
-  }
-
-  .mobile-header {
-    padding: 16px;
-
-    h2 {
-      margin: 0;
-      font-size: 1.25rem;
-      font-weight: 600;
-    }
+  ::v-deep .v-messages {
+    display: none;
   }
 
   .subheading {
@@ -365,10 +247,10 @@
     color: gray;
   }
 
-  .checkbox-with-tooltip {
-    display: flex;
-    gap: 8px;
-    align-items: center;
+  .filters {
+    width: 100%;
+    height: calc(100% - 100px);
+    overflow: auto;
   }
 
   .qa-link {
@@ -377,18 +259,6 @@
 
   .drawer-btn {
     margin-top: 10px;
-  }
-
-  .side-panel-footer {
-    padding-top: 24px;
-    margin-top: auto;
-    text-align: center;
-    border-top: 1px solid #e0e0e0;
-  }
-
-  // Remove Vuetify-specific styles
-  ::v-deep .v-messages {
-    display: none;
   }
 
 </style>
