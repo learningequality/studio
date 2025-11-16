1d35ffee1f8b22fd4bd398f1bedc6d0392341db6
Prashant-thakur77 <prashant101007@gmail.com>
2025-11-17 01:31:27 +0530

    Updated tests

--- contentcuration/contentcuration/frontend/channelList/views/Channel/__tests__/catalogFilters.spec.js
+++ contentcuration/contentcuration/frontend/channelList/views/Channel/__tests__/catalogFilters.spec.js
@@ -2,6 +2,7 @@ import { mount } from '@vue/test-utils';
 import { factory } from '../../../store';
 import router from '../../../router';
 import CatalogFilters from '../CatalogFilters';
+import CatalogFilterPanelContent from '../CatalogFilterPanelContent';
 
 const store = factory();
 
@@ -24,25 +25,44 @@ describe('catalogFilters', () => {
   });
 
   describe('keywords', () => {
-    it('should call setKeywords when keywords change', () => {
-      const setKeywordsMock = jest.fn();
-      const setKeywords = () => {
-        return () => {
-          setKeywordsMock();
-        };
-      };
-      wrapper = makeWrapper({ setKeywords });
-      const keywords = wrapper.find('[data-test="keywords"]');
-      keywords.element.value = 'test';
-      keywords.trigger('input');
-      expect(setKeywordsMock).toHaveBeenCalled();
-    });
-    it('keywordInput should stay in sync with query param', () => {
-      const keywords = 'testing new keyword';
-      router.push({ query: { keywords } });
-      wrapper.vm.$nextTick(() => {
-        expect(wrapper.vm.keywordInput).toBe(keywords);
+    it('should update keywords data when updateKeywords is called', async () => {
+      wrapper = mount(CatalogFilterPanelContent, {
+        sync: false,
+        router,
+        store,
       });
+
+      await wrapper.setData({ keywordInput: 'initial value' });
+
+      wrapper.vm.updateKeywords();
+
+      expect(wrapper.vm.keywords).toBe('initial value');
+    });
+
+    it('keywordInput should sync with route query parameters on mount', async () => {
+      const keywords = 'route keywords';
+      await router.push({ query: { keywords } });
+
+      wrapper = mount(CatalogFilterPanelContent, {
+        sync: false,
+        router,
+        store,
+      });
+
+      expect(wrapper.vm.keywordInput).toBe(keywords);
+    });
+
+    it('keywordInput should update when route query parameters change', async () => {
+      wrapper = mount(CatalogFilterPanelContent, {
+        sync: false,
+        router,
+        store,
+      });
+
+      const keywords = 'new route keywords';
+      await router.push({ query: { keywords } });
+
+      expect(wrapper.vm.keywordInput).toBe(keywords);
     });
   });
 });
