bf8287e31e1ec5750c87ac5825d9c583aea6c3bc
Prashant-thakur77 <prashant101007@gmail.com>
2025-11-18 23:00:39 +0530

    Hovericon made color configurable

--- contentcuration/contentcuration/frontend/shared/views/StudioChip.vue
+++ contentcuration/contentcuration/frontend/shared/views/StudioChip.vue
@@ -21,15 +21,12 @@
         data-test="remove-chip"
         @click.stop="handleClose"
       >
-        <KIcon
-          icon="delete"
-          :color="$themePalette.grey.v_400"
-          class="close-icon default-icon"
-        />
-        <KIcon
-          icon="delete"
-          :color="$themePalette.grey.v_900"
-          class="close-icon hover-icon"
+        <HoverIcon
+          outlineIcon="delete"
+          filledIcon="delete"
+          :outlineColor="$themePalette.grey.v_400"
+          :filledColor="$themePalette.grey.v_900"
+          class="icon"
         />
       </button>
     </div>
@@ -40,8 +37,13 @@
 
 <script>
 
+  import HoverIcon from './HoverIcon.vue';
+
   export default {
     name: 'StudioChip',
+    components: {
+      HoverIcon,
+    },
     props: {
       text: {
         type: String,
@@ -101,6 +103,7 @@
     display: flex;
     gap: 4px;
     align-items: center;
+    justify-content: center;
     height: 100%;
   }
 
@@ -110,45 +113,21 @@
   }
 
   .close-button {
-    position: relative;
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 22px;
-    height: 22px;
-    padding: 0;
-    margin: 0;
     cursor: pointer;
     border-radius: 50%;
   }
 
-  .close-icon {
-    font-size: 18px;
-  }
-
-  .default-icon {
-    top: 0;
-    opacity: 1;
-  }
-
-  .hover-icon {
-    position: absolute;
-    top: 1px;
-    opacity: 0;
-  }
-
-  .close-button:hover {
-    .default-icon {
-      opacity: 0;
-    }
-
-    .hover-icon {
-      opacity: 1;
-    }
-  }
-
   .studio-chip.has-close-button {
     padding: 2px 6px 2px 12px;
+  }
+
+  .icon {
+    width: 24px;
+    height: 24px;
+    font-size: 18px;
   }
 
 </style>
