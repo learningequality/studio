0c1a2de2aeebdebbba4c0be864f0f9b338c9b033
Prashant-thakur77 <prashant101007@gmail.com>
2026-01-16 22:51:50 +0530

    Updated Catalogfiltercontent to handle padding wihout sidepanelmodal

--- contentcuration/contentcuration/frontend/channelList/views/Channel/CatalogFilters.vue
+++ contentcuration/contentcuration/frontend/channelList/views/Channel/CatalogFilters.vue
@@ -21,11 +21,7 @@
       alignment="left"
       @closePanel="closeSidePanel"
     >
-      <div
-        style="padding: 24px 32px 16px;"
-      >
-        <CatalogFilterPanelContent />
-      </div>
+      <CatalogFilterPanelContent />
       
     </SidePanelModal>
   </div>
