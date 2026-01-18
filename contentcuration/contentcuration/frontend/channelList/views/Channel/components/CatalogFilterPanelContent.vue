adb8767f0aebc0e989463d0a71562a4072a1d7aa
Prashant-thakur77 <prashant101007@gmail.com>
2026-01-18 17:12:33 +0530

    precommit

--- contentcuration/contentcuration/frontend/channelList/views/Channel/components/CatalogFilterPanelContent.vue
+++ contentcuration/contentcuration/frontend/channelList/views/Channel/components/CatalogFilterPanelContent.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div 
+  <div
     class="filter-panel-content"
     :style="contentStyles"
   >
@@ -111,7 +111,6 @@
   import Checkbox from 'shared/views/form/Checkbox';
   import HelpTooltip from 'shared/views/HelpTooltip';
   import { ContentKindsNames } from 'shared/leUtils/ContentKinds';
-  
 
   const excludedKinds = new Set([ContentKindsNames.TOPIC, ContentKindsNames.H5P]);
 
