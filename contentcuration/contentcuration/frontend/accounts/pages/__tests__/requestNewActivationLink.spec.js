becb30b7cbce1607ef72d3f1e276eccaa2eb1eb0
Michaela Robosova <robosovam@gmail.com>
2026-02-02 20:52:33 +0100

    Remove comment

--- contentcuration/contentcuration/frontend/accounts/pages/__tests__/requestNewActivationLink.spec.js
+++ contentcuration/contentcuration/frontend/accounts/pages/__tests__/requestNewActivationLink.spec.js
@@ -29,12 +29,10 @@ function renderComponent() {
       {
         path: '/main',
         name: 'Main',
-        // component: { render: h => h('div') }, // dummy
       },
       {
         path: '/activation-link-resent',
         name: 'ActivationLinkReSent',
-        // component: { render: h => h('div') }, // dummy
       },
     ],
   });
