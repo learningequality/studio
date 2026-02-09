60b7dfb6a68983591b9fa06b28eea231a453a387
Michaela Robosova <robosovam@gmail.com>
2026-02-09 16:10:10 +0100

    Remove visuallyhidden style definition
    This is newly provided by KDS so not needed in Studio.

--- contentcuration/contentcuration/frontend/channelList/views/Channel/CatalogFAQ.vue
+++ contentcuration/contentcuration/frontend/channelList/views/Channel/CatalogFAQ.vue
@@ -455,15 +455,4 @@
     @extend %dropshadow-1dp;
   }
 
-  .visuallyhidden {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    margin: -1px;
-    overflow: hidden;
-    clip: rect(0 0 0 0);
-    border: 0;
-  }
-
 </style>
