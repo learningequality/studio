4665226d8a63445a328fead4f883cff8ce9f44e9
Michaela Robosova <michaela@learningequality.org>
2026-03-06 16:42:05 +0100

    Use KCard
    in My Channels, Starred channels, View-only channels,
    and Content library.

--- contentcuration/contentcuration/frontend/channelList/router.js
+++ contentcuration/contentcuration/frontend/channelList/router.js
@@ -1,5 +1,7 @@
 import VueRouter from 'vue-router';
-import ChannelList from './views/Channel/ChannelList';
+import StudioMyChannels from './views/Channel/StudioMyChannels';
+import StudioStarredChannels from './views/Channel/StudioStarredChannels';
+import StudioViewOnlyChannels from './views/Channel/StudioViewOnlyChannels';
 import StudioCollectionsTable from './views/ChannelSet/StudioCollectionsTable';
 import ChannelSetModal from './views/ChannelSet/ChannelSetModal';
 import CatalogList from './views/Channel/CatalogList';
@@ -7,17 +9,14 @@ import { RouteNames } from './constants';
 import CatalogFAQ from './views/Channel/CatalogFAQ';
 import ChannelModal from 'shared/views/channel/ChannelModal';
 import ChannelDetailsModal from 'shared/views/channel/ChannelDetailsModal';
-import { ChannelListTypes } from 'shared/constants';
 
 const router = new VueRouter({
   routes: [
     {
       name: RouteNames.CHANNELS_EDITABLE,
       path: '/my-channels',
-      component: ChannelList,
-      props: { listType: ChannelListTypes.EDITABLE },
+      component: StudioMyChannels,
     },
-
     {
       name: RouteNames.CHANNEL_SETS,
       path: '/collections',
@@ -38,14 +37,12 @@ const router = new VueRouter({
     {
       name: RouteNames.CHANNELS_STARRED,
       path: '/starred',
-      component: ChannelList,
-      props: { listType: ChannelListTypes.STARRED },
+      component: StudioStarredChannels,
     },
     {
       name: RouteNames.CHANNELS_VIEW_ONLY,
       path: '/view-only',
-      component: ChannelList,
-      props: { listType: ChannelListTypes.VIEW_ONLY },
+      component: StudioViewOnlyChannels,
     },
     {
       name: RouteNames.CHANNEL_DETAILS,
