b70676f1de6f408d61b2c94c333f29e25de09326
Prashant-thakur77 <prashant101007@gmail.com>
2026-04-13 23:19:31 +0530

    Resolved some indentaion

--- contentcuration/contentcuration/frontend/channelEdit/components/sidePanels/PublishSidePanel.vue
+++ contentcuration/contentcuration/frontend/channelEdit/components/sidePanels/PublishSidePanel.vue
@@ -12,10 +12,8 @@
       </template>
 
       <template #default>
-        <div
-          style="padding: 24px 32px 16px;"
-        >
-        <component :is="showDraftMode ? 'KRadioButtonGroup' : 'div'">
+        <div style="padding: 24px 32px 16px">
+          <component :is="showDraftMode ? 'KRadioButtonGroup' : 'div'">
             <KRadioButton
               v-if="showDraftMode"
               :label="modeLive$()"
